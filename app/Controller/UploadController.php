72e1ad91e98cd9f8acc866906893c667d22181b8
liuzhaoren@gmail.com <liuzhaoren@gmail.com@1aee191c-eff3-477b-8fa7-d95ec15a78bd>
2014-09-18 14:31:30 +0000

    add new UploadHander

---
+++ app/Controller/UploadController.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: liuzhr
+ * Date: 9/17/14
+ * Time: 10:48 PM
+ */
+class UploadController extends AppController {
+    var $name = 'Uploadfiles';
+    var $helpers = array(
+        'Html'
+    );
+    var $components = array(
+        'Session',
+        'SwfUpload'
+    );
+
+    function upload() {
+
+        App::import('Vendor', 'UploadHandler', array('file' => 'file.upload/UploadHandler.php'));
+        $options = array(
+            'upload_dir' => 'Your upload directory',
+            'accept_file_types' => '/\.(gif|jpe?g|png)$/i'
+        );
+
+        $upload_handler = new UploadHandler($options);
+    }
+
+}
