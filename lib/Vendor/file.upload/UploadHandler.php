9978454c576b0f057a8c75265093f8497cc0ab4d
liuzhaoren@gmail.com <liuzhaoren@gmail.com@1aee191c-eff3-477b-8fa7-d95ec15a78bd>
2014-09-21 14:18:22 +0000

    fix upload bug

--- lib/Vendor/file.upload/UploadHandler.php
+++ lib/Vendor/file.upload/UploadHandler.php
@@ -1063,56 +1063,57 @@ class UploadHandler extends Object
         $file->type = $type;
         if ($this->validate($uploaded_file, $file, $error, $index)) {
             $this->handle_form_data($file, $index);
-            $upload_dir = $this->get_upload_path();
-            if (!is_dir($upload_dir)) {
-                mkdir($upload_dir, $this->options['mkdir_mode'], true);
-            }
-            $file_path = $this->get_upload_path($file->name);
-            $append_file = $content_range && is_file($file_path) &&
-                $file->size > $this->get_file_size($file_path);
-            if ($uploaded_file && is_uploaded_file($uploaded_file)) {
-                // multipart/formdata uploads (POST method uploads)
-                if ($append_file) {
-                    file_put_contents(
-                        $file_path,
-                        fopen($uploaded_file, 'r'),
-                        FILE_APPEND
-                    );
-                } else {
-                    if(defined('SAE_MYSQL_DB')){
-                        $stor = new SaeStorage();
-                        $download_url = $stor->upload(SAE_STORAGE_UPLOAD_DOMAIN_NAME , $file_path , $uploaded_file);
-                        $this->log('handle_file_upload: final file='. $download_url .', $file-path='. $file_path .', $uploaded_file='. $uploaded_file);
+
+            if(defined('SAE_MYSQL_DB')){
+                $file_path = $file->name;
+                $stor = new SaeStorage();
+                $download_url = $stor->upload(SAE_STORAGE_UPLOAD_DOMAIN_NAME , $file_path , $uploaded_file);
+                $this->log('handle_file_upload: final file='. $download_url .', $file-path='. $file_path .', $uploaded_file='. $uploaded_file);
+
+                $file->url = $download_url;
+            } else {
+
+                $upload_dir = $this->get_upload_path();
+                if (!is_dir($upload_dir)) {
+                    mkdir($upload_dir, $this->options['mkdir_mode'], true);
+                }
+                $file_path = $this->get_upload_path($file->name);
+                $append_file = $content_range && is_file($file_path) &&
+                    $file->size > $this->get_file_size($file_path);
+                if ($uploaded_file && is_uploaded_file($uploaded_file)) {
+                    // multipart/formdata uploads (POST method uploads)
+                    if ($append_file) {
+                        file_put_contents(
+                            $file_path,
+                            fopen($uploaded_file, 'r'),
+                            FILE_APPEND
+                        );
                     } else {
                         move_uploaded_file($uploaded_file, $file_path);
                     }
-                }
-            } else {
-                // Non-multipart uploads (PUT method support)
-                file_put_contents(
-                    $file_path,
-                    fopen('php://input', 'r'),
-                    $append_file ? FILE_APPEND : 0
-                );
-            }
-            $file_size = $this->get_file_size($file_path, $append_file);
-            if ($file_size === $file->size) {
-                if (isset($download_url) && $download_url) {
-                    $file->url = $download_url;
                 } else {
+                    // Non-multipart uploads (PUT method support)
+                    file_put_contents(
+                        $file_path,
+                        fopen('php://input', 'r'),
+                        $append_file ? FILE_APPEND : 0
+                    );
+                }
+                $file_size = $this->get_file_size($file_path, $append_file);
+                if ($file_size === $file->size) {
                     $file->url = $this->get_download_url($file->name);
+                    if ($this->is_valid_image_file($file_path)) {
+                        $this->handle_image_file($file_path, $file);
+                    }
+                } else {
+                    $file->size = $file_size;
+                    if (!$content_range && $this->options['discard_aborted_uploads']) {
+                        unlink($file_path);
+                        $file->error = $this->get_error_message('abort');
+                    }
                 }
-                if ($this->is_valid_image_file($file_path)) {
-                    $this->handle_image_file($file_path, $file);
-                }
-            } else {
-                $file->size = $file_size;
-                if (!$content_range && $this->options['discard_aborted_uploads']) {
-                    unlink($file_path);
-                    $file->error = $this->get_error_message('abort');
-                }
+                $this->set_additional_file_properties($file);
             }
-            $this->set_additional_file_properties($file);
         }
         return $file;
     }
