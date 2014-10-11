a4b08ee07e9a5185c9e84a37e65387da4e9732dd
liuzhaoren@gmail.com <liuzhaoren@gmail.com@1aee191c-eff3-477b-8fa7-d95ec15a78bd>
2014-10-11 11:41:03 +0000

    change time to 10s

--- webroot/js/front_orders.js
+++ webroot/js/front_orders.js
@@ -196,7 +196,7 @@ function orders_receive_3g(order_id){
             $('.order_item_action_'+order_id).html('<a class="btn-sm btn-primary" href="/orders/detail/'+order_id+'">详细</a>');
             $('.order-status-'+order_id).html('已收货');
             $('#orders-wait_receive').find('.order_item_'+order_id).remove();
-        }, 2000);
+        }, 10000);
     });
 }
 
