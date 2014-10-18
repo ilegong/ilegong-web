36e16db505ec5c7c9afbd672ca30cf8a7cc443f8
liuzhaoren@gmail.com <liuzhaoren@gmail.com@1aee191c-eff3-477b-8fa7-d95ec15a78bd>
2014-10-18 06:29:20 +0000

    fix minor typo

--- app/Controller/Apple201410Controller.php
+++ app/Controller/Apple201410Controller.php
@@ -46,7 +46,7 @@ class Apple201410Controller extends AppController {
         } else {
 
             $awardInfo = $this->AwardInfo->getAwardInfoByUidAndType($this->currentUser['id'], KEY_APPLE_201410);
-            if (!$awardInfo) {
+            if (empty($awardInfo)) {
                 $awardInfo = array('AwardInfo' => array('uid' => $this->currentUser['id'], 'type' => KEY_APPLE_201410, 'times' => 99, 'got' => 0));
                 $this->AwardInfo->save($awardInfo);
                 $awardInfo = $awardInfo['AwardInfo'];
