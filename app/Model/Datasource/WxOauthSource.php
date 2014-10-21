2c6ce2b1ac0d43c67c974e4de897a8bf5f430e5c
liuzhaoren@gmail.com <liuzhaoren@gmail.com@1aee191c-eff3-477b-8fa7-d95ec15a78bd>
2014-10-21 05:15:23 +0000

    add log for login

--- app/Model/Datasource/WxOauthSource.php
+++ app/Model/Datasource/WxOauthSource.php
@@ -92,7 +92,7 @@ class WxOauthSource extends DataSource {
         } else {
             throw new CakeException("not supported query type(" . $queryData['method'] . ")");
         }
-        $this->log("method:".$queryData['method'].", json:". $json);
+        $this->log("method:".$queryData['method'].", request:".json_encode($queryData)." json:". $json);
         $res = json_decode($json, true);
         if (is_null($res)) {
             $error = json_last_error();
