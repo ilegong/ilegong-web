342e04d027be7b1bd8ffdaeff5255ff9b565fb13
liuzhaoren@gmail.com <liuzhaoren@gmail.com@1aee191c-eff3-477b-8fa7-d95ec15a78bd>
2014-10-27 14:00:34 +0000

    fix float display bug

--- webroot/js/common.js
+++ webroot/js/common.js
@@ -891,5 +891,19 @@ var utils = {
 
     is_weixin: function(){
         return (typeof '_pys_in_weixin' != 'undefined' && _pys_in_weixin);
+    },
+
+    toFixed: function (value, precision) {
+        var precision = precision || 0,
+            power = Math.pow(10, precision),
+            absValue = Math.abs(Math.round(value * power)),
+            result = (value < 0 ? '-' : '') + String(Math.floor(absValue / power));
+
+        if (precision > 0) {
+            var fraction = String(absValue % power),
+                padding = new Array(Math.max(precision - fraction.length, 0) + 1).join('0');
+            result += '.' + padding + fraction;
+        }
+        return result;
     }
 };
