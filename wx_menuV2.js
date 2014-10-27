8400c543b1ce401146eb915b9c5441c178e32d25
liuzhaoren@gmail.com <liuzhaoren@gmail.com@1aee191c-eff3-477b-8fa7-d95ec15a78bd>
2014-10-27 15:48:04 +0000

    update menu

--- wx_menuV2.js
+++ wx_menuV2.js
@@ -5,10 +5,18 @@
         "name":"购买特产",
         "url":"http://pys.b-wmobile.com/users/wx_menu_point?referer_key=CLICK_URL_TECHAN&from=service"
     },
-    {
-        "type":"view",
-        "name":"苹果游戏",
-        "url":"http://pys.b-wmobile.com/apple_201410/award.html"
+    {  "name" : "苹果游戏",
+        "sub_button" : [
+            {
+                "type":"view",
+                "name":"苹果游戏",
+                "url":"http://pys.b-wmobile.com/apple_201410/award.html"
+            },            {
+                "type":"view",
+                "name":"购买苹果",
+                "url":"http://pys.b-wmobile.com/products/20141009/xiao_juan_jia_xian_yang_hong_fu_shi_yu_shou_20xiang.html?from=game_home"
+            }
+        ]
     },
     {
         "name":"个人中心",
