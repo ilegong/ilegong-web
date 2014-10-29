2ea5f0659484a2ce82b5c33f14f9c4f9d4d24239
liuzhaoren@gmail.com <liuzhaoren@gmail.com@1aee191c-eff3-477b-8fa7-d95ec15a78bd>
2014-10-29 07:44:42 +0000

    fix log saving bug

--- app/Controller/AliPayController.php
+++ app/Controller/AliPayController.php
@@ -33,30 +33,16 @@ class AliPayController extends AppController {
             $this->log("Zhifubao: $out_trade_no, $trade_no, $trade_status, request:".json_encode($_REQUEST));
 
             if($trade_status == 'TRADE_FINISHED') {
-                //判断该笔订单是否在商户网站中已经做过处理
-                //如果没有做过处理，根据订单号（out_trade_no）在商户网站的订单系统中查到该笔订单的详细，并执行商户的业务程序
-                //如果有做过处理，不执行商户的业务程序
-
                 //注意：
                 //该种交易状态只在两种情况下出现
                 //1、开通了普通即时到账，买家付款成功后。
                 //2、开通了高级即时到账，从该笔交易成功时间算起，过了签约时的可退款时限（如：三个月以内可退款、一年以内可退款等）后。
 
-                //调试用，写文本函数记录程序运行情况是否正常
-                //logResult("这里写入想要调试的代码变量值，或其他运行的结果记录");
-                $this->saveNotifyIfNotSaved($out_trade_no, $trade_no, $_POST);
+                $this->saveNotifyIfNotSaved($out_trade_no, $trade_no, $_POST['buyer_id'], $_POST['buyer_email']);
             }
             else if ($trade_status == 'TRADE_SUCCESS') {
                 //判断该笔订单是否在商户网站中已经做过处理
-                //如果没有做过处理，根据订单号（out_trade_no）在商户网站的订单系统中查到该笔订单的详细，并执行商户的业务程序
-                //如果有做过处理，不执行商户的业务程序
-
-                //注意：
-                //该种交易状态只在一种情况下出现——开通了高级即时到账，买家付款成功后。
-
-                //调试用，写文本函数记录程序运行情况是否正常
-                //logResult("这里写入想要调试的代码变量值，或其他运行的结果记录");
-                $this->saveNotifyIfNotSaved($out_trade_no, $trade_no, $_GET);
+                $this->saveNotifyIfNotSaved($out_trade_no, $trade_no, $_POST['buyer_id'], $_POST['buyer_email']);
 
             }  else {
                 $this->log("verify notify not handling: for $out_trade_no, $trade_status");
@@ -79,19 +65,12 @@ class AliPayController extends AppController {
             $trade_no = $_GET['trade_no'];
             $trade_status = $_GET['trade_status'];
             if($trade_status == 'TRADE_FINISHED' || $trade_status == 'TRADE_SUCCESS') {
-                //判断该笔订单是否在商户网站中已经做过处理
-                //如果没有做过处理，根据订单号（out_trade_no）在商户网站的订单系统中查到该笔订单的详细，并执行商户的业务程序
-                //如果有做过处理，不执行商户的业务程序
-                if ($this->WxPayment->notifyCounted($out_trade_no) > 0) {
-                    $this->log("Zhifubao: Aready done, so skipped");
-
+                list($status, $order) = $this->saveNotifyIfNotSaved($out_trade_no, $trade_no, $_GET['buyer_id'], $_GET['buyer_email']);
+                if ($status == PAYNOTIFY_STATUS_SKIPPED) {
                     $this->redirect('/orders/mine');
-                    //TODO: 需要显示信息。
                     echo "您的订单支付在处理中，立即将要返回我的订单列表查看状态";
-                    //TODO: 拿到订单，给出订单支付完成界面
-
+                    //TODO: 拿到订单，给出订单支付完成界面/未完成界面
                 } else {
-                    list($status, $order) = $this->WxPayment->saveNotifyAndUpdateStatus($out_trade_no, $trade_no, TRADE_ALI_TYPE, true);
                     if(!empty($order)) {
                         $this->redirect('/orders/detail/'.$order['Order']['id']);
                     }
@@ -111,22 +90,22 @@ class AliPayController extends AppController {
     /**
      * @param $out_trade_no
      * @param $trade_no
-     * @param $paras array parameters array
+     * @param $total_fee
+     * @param $buyer_email
+     * @internal param $total_fee_in_cent
      * @return array status and Order object
      */
-    protected function saveNotifyIfNotSaved($out_trade_no, $trade_no, &$paras) {
-        $buyer_id = $paras['buyer_id'];
-        $total_fee = $paras['total_fee'] * 100;
-        $buyer_email = $paras['buyer_email'];
-        $arr = array("buyer_id" => $buyer_id, "exterface" => $paras['exterface'], "is_success" => $paras["is_success"], "payment_type" => $paras['payment_type'], "trade_status" => $paras['trade_status']);
+    protected function saveNotifyIfNotSaved($out_trade_no, $trade_no, $total_fee, $buyer_email) {
+        $total_fee_in_cent = $total_fee * 100;
+        $arr = array("buyer_id" => $_REQUEST['buyer_id'], "exterface" => $_REQUEST['exterface'], "is_success" => $_REQUEST["is_success"], "payment_type" => $_REQUEST['payment_type'], "trade_status" => $_REQUEST['trade_status']);
         $attach = json_encode($arr);
 
         if ($this->WxPayment->notifyCounted($out_trade_no) > 0) {
-            $this->log("Zhifubao: Aready done, so skipped");
+            $this->log("Zhifubao: Aready done, so skipped: ".$out_trade_no);
             return PAYNOTIFY_STATUS_SKIPPED;
         } else {
             list($status, $order) = $this->WxPayment->saveNotifyAndUpdateStatus($out_trade_no, $trade_no, TRADE_ALI_TYPE, true, $buyer_email, 0,
-                $total_fee, false, '', '', $attach, '');
+                $total_fee_in_cent, false, '', '', $attach, '');
 
             if ($status == PAYNOTIFY_STATUS_ORDER_UPDATED) {
                 $this->loadModel('Oauthbind');
