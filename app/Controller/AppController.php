8580c8daae583e5d738afc40d325351bb1d52861
liuzhaoren@gmail.com <liuzhaoren@gmail.com@1aee191c-eff3-477b-8fa7-d95ec15a78bd>
2014-11-10 03:17:54 +0000

    log ip

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -681,10 +681,10 @@ class AppController extends Controller {
         $clientIp = $this->request->clientIp();
         $newUser = empty($trackLogs);
         if ($newUser) {
-            $toUpdate = array('TrackLog' => array('type' => $track_type, 'last_ip' => $clientIp, 'from' => $current_uid, 'to' => $friendUid, 'award_time' => date(FORMAT_DATETIME)));
+            $toUpdate = array('TrackLog' => array('type' => $track_type, 'last_ip' => '\''.$clientIp.'\'', 'from' => $current_uid, 'to' => $friendUid, 'award_time' => date(FORMAT_DATETIME)));
             $this->TrackLog->save($toUpdate);
         } else {
-            $this->TrackLog->updateAll(array('latest_click_time' => '\''.date(FORMAT_DATETIME).'\'', 'last_ip' => $clientIp), array('id' => $trackLogs['TrackLog']['id']));
+            $this->TrackLog->updateAll(array('latest_click_time' => '\''.date(FORMAT_DATETIME).'\'', 'last_ip' => '\''.$clientIp.'\''), array('id' => $trackLogs['TrackLog']['id']));
         }
         return $newUser;
     }
