bbad1bad49b6e13e19c3651256bf8b219b65a62f
liuzhaoren@gmail.com <liuzhaoren@gmail.com@1aee191c-eff3-477b-8fa7-d95ec15a78bd>
2014-11-11 14:24:32 +0000

    add new limited buy

--- app/Model/ShipPromotion.php
+++ app/Model/ShipPromotion.php
@@ -73,7 +73,8 @@ class ShipPromotion extends AppModel {
         '228' => array(100, 13, 1),
         '229' => array(80, 13, 1),
         PRODUCT_ID_CAKE => array(288, 74, 0),
-        '240' => array(306, 78, 1 )
+        '240' => array(306, 78, 1 ),
+        '259' => array(350, 78, 1 )
     );
 
     /**
