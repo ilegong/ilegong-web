8ade448cb70651867d3672029592ff8a979e5221
liuzhaoren@gmail.com <liuzhaoren@gmail.com@1aee191c-eff3-477b-8fa7-d95ec15a78bd>
2014-11-12 11:12:30 +0000

    adjust style

--- webroot/js/apple_game.js
+++ webroot/js/apple_game.js
@@ -184,7 +184,7 @@ $(document).ready(function(){
     var coupon_count = data.coupon_count;
     $appleGotCnt.text(apple_count-exchange_apple_count);
     $riceGotCnt.text((apple_count-exchange_apple_count)*10);
-    utils.alert("恭喜，兑换"+coupon_count+"张粮票成功，马上点击右侧<span class='apple_numbers'>查看我的优惠券</span>查看!");
+    utils.alert("恭喜，兑换"+coupon_count+"张粮票成功，<a href='/users/my_coupons.html' class='apple_medium_links'>查看我的优惠券</a>!");
     }  else {
     utils.alert("呜呜，兑换失败，请稍后重试。");
     }
