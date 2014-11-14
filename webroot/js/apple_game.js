f4f35f477cb183cd4d0f70bdcebef710e94378f6
liuzhaoren@gmail.com <liuzhaoren@gmail.com@1aee191c-eff3-477b-8fa7-d95ec15a78bd>
2014-11-14 06:46:18 +0000

    add hint for high got users

--- webroot/js/apple_game.js
+++ webroot/js/apple_game.js
@@ -91,7 +91,11 @@ $(document).ready(function(){
     if ($got > 0) {
     utils.alert('恭喜你！你摇掉了<span class="apple_numbers">' + $got + '</span>个'+game_obj_name+'！<br/><small>(2秒后自动消失)</small>', function(){}, timeout, close_callback);
     } else {
-    utils.alert('你力气太小啦！只晃掉了几片树叶！<br/><small>(2秒后自动消失)</small>', function(){}, timeout, close_callback);
+        var msg = '你力气太小啦！只晃掉了几片树叶！<br/><small>(2秒后自动消失)</small>';
+        if ($total > 95) {
+            msg += '<br/>大奖按天投放，如果超过四十次树叶，请明天早点来抢哦！'
+        }
+        utils.alert(msg, function(){}, timeout, close_callback);
     }
     updateViewState(times, total);
     }
