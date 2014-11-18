db6456e839018d3a98061f645af4c118f8b1d372
liuzhaoren@gmail.com <liuzhaoren@gmail.com@1aee191c-eff3-477b-8fa7-d95ec15a78bd>
2014-11-18 09:06:33 +0000

    edit paid message if has a special sharing offer

--- app/Controller/Component/WeixinComponent.php
+++ app/Controller/Component/WeixinComponent.php
@@ -111,20 +111,30 @@ class WeixinComponent extends Component
         return false;
     }
 
-    public function send_order_paid_message($open_id, $price, $good_info, $ship_info, $order_no, $brand_id = null)
+    public function send_order_paid_message($open_id, $price, $good_info, $ship_info, $order_no, $order = null)
     {
+        $so = ClassRegistry::init('ShareOffer');
+        $offer = $so->query_gen_offer($order, $order['Order']['creator']);
+
+        $number = 0;
+        $name = '';
+        if(!empty($offer)) {
+            $number = $offer['number'];
+            $name = $offer['name'];
+        }
+
         $post_data = array(
             "touser" => $open_id,
             "template_id" => $this->wx_message_template_ids["ORDER_PAID"],
             "url" => $this->get_order_query_url($order_no),
             "topcolor" => "#FF0000",
             "data" => array(
-                "first" => array("value" => "亲，您的订单已完成付款，商家将即时为您发货。"),
+                "first" => array("value" => "亲，您的订单已完成付款，商家将即时为您发货。". ($number > 0 && !empty($name)) ? "恭喜您获得".$name."红包，马上领取" : ""),
                 "orderProductPrice" => array("value" => $price),
                 "orderProductName" => array("value" => $good_info),
                 "orderAddress" => array("value" => empty($ship_info)?'':$ship_info),
                 "orderName" => array("value" => $order_no),
-                "remark" => array("value" => "点击详情，查询订单。", "color" => "#FF8800")
+                "remark" => array("value" => "点击详情，查询订单".($number > 0 ? "/领取红包":"")."。", "color" => "#FF8800")
             )
         );
         return $this->send_weixin_message($post_data);
@@ -286,7 +296,7 @@ class WeixinComponent extends Component
 
 
             }  else {
-                $this->send_order_paid_message($open_id, $price, $good_info, $ship_info, $order_id, $order['Order']['brand_id']);
+                $this->send_order_paid_message($open_id, $price, $good_info, $ship_info, $order_id, $order);
             }
 
         }
--- app/Controller/OrdersController.php
+++ app/Controller/OrdersController.php
@@ -368,7 +368,8 @@ class OrdersController extends AppController{
         $products = $product_new;
         unset($product_new);
 
-        $toShare = $this->to_share_offer($orderinfo, $this->currentUser['id']);
+        $shareOffer = ClassRegistry::init('ShareOffer');
+        $toShare = $shareOffer->query_gen_offer($orderinfo, $this->currentUser['id']);
         $canComment = $this->can_comment($status);
 
         $this->set(compact('toShare', 'canComment', 'no_more_money', 'order_id', 'order'));
@@ -1043,110 +1044,6 @@ class OrdersController extends AppController{
             && $status != ORDER_STATUS_WAITING_PAY
 //            && $status != ORDER_STATUS_SHIPPED
             ;
-    }
-
-    /**
-     * Get a share offer to sharing for current order && user
-     * @param $order
-     * @param $uid
-     * @return object ShareOffer object
-     */
-    private function to_share_offer($order, $uid) {
-        $status = $order['Order']['status'];
-        $brandId = $order['Order']['brand_id'];
-        $payTime = $order['Order']['pay_time'];
-        if ( ($status == ORDER_STATUS_DONE
-            || $status == ORDER_STATUS_PAID
-            || $status == ORDER_STATUS_RECEIVED
-            || $status == ORDER_STATUS_SHIPPED)
-           && !empty($payTime)
-           && !empty($brandId)
-           && $order['Order']['total_all_price'] > 0
-            ) {
-            $soModel = ClassRegistry::init('ShareOffer');
-            $so = $soModel->findByBrandId($brandId, true, $payTime);
-            if (!empty($so)) {
-                $usModel = ClassRegistry::init('SharedOffer');
-                $userShared = $usModel->find_user_shared_offer($uid, $order['Order']['id'], $so['ShareOffer']['id']);
-                if (empty($userShared)){
-                    $toShareNum = round( ($so['ShareOffer']['ratio_percent'] * $order['Order']['total_all_price'] * 100)/100, 0, PHP_ROUND_HALF_DOWN);
-                    if ($toShareNum <= 0) {
-                        return null;
-                    }
-
-                    //订单Id和UID有唯一索引，避免用户重复点击
-                    $usModel->create();
-                    if($usModel->save(array(
-                        'share_offer_id' => $so['ShareOffer']['id'],
-                        'total_number' => $toShareNum,
-                        'start' =>  date(FORMAT_DATETIME, mktime()), //use db time
-                        'uid' => $order['Order']['creator'],
-                        'order_id' => $order['Order']['id'],
-                    ))) {
-
-                        $split_num = $so['ShareOffer']['split_num'];
-
-                        $avg = round($toShareNum/$split_num, 0, PHP_ROUND_HALF_UP);
-                        $amount_left = $toShareNum;
-
-                        $shared_id = $usModel->getLastInsertID();
-                        $mSharedSlice = ClassRegistry::init('SharedSlice');
-
-                        //只有总体多于1毛，平均大于1分，才能分成多个红包
-                        if ($split_num > 1 && $avg >= 1) {
-                            for ($i = 0; $i < $split_num; $i++) {
-                                if ($i < $split_num) {
-                                    //减一，避免只有两份的情况下一次全部分完
-                                    //HALF_UP, 避免0分钱红包
-                                    $num = round(rand($avg / 2, $avg * 2 - 1), 0, PHP_ROUND_HALF_UP);
-                                    if ($num > $amount_left) {
-                                        $num = $amount_left;
-                                    }
-                                    $amount_left -= $num;
-                                } else {
-                                    $num = $amount_left;
-                                }
-
-                                if ($num <= 0) {
-                                    continue;
-                                }
-
-                                $mSharedSlice->create();
-                                $mSharedSlice->save(array(
-                                    'shared_offer_id' => $shared_id,
-                                    'number' => $num,
-                                    'accept_time' => null,
-                                ));
-                            }
-                        } else {
-                            $mSharedSlice->create();
-                            $mSharedSlice->save(array(
-                                'shared_offer_id' => $shared_id,
-                                'number' => $amount_left,
-                                'accept_time' => null,
-                            ));
-                        }
-
-                        return array(
-                            'name' => $so['ShareOffer']['name'],
-                            'number' => $toShareNum,
-                            'id' => $shared_id,
-                        );
-
-                    } else {
-                        return null;
-                    }
-                }
-                else {
-                    return array(
-                        'name' => $so['ShareOffer']['name'],
-                        'number' => $userShared['SharedOffer']['total_number'],
-                        'id' => $userShared['SharedOffer']['id'],
-                    );
-                }
-            }
-        }
-        return null;
     }
 
     /**
