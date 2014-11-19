94818c0c64a381827aff25f4285fc698a86abbed
liuzhaoren@gmail.com <liuzhaoren@gmail.com@1aee191c-eff3-477b-8fa7-d95ec15a78bd>
2014-11-19 05:19:43 +0000

    reduce left slice

--- app/Controller/SharingController.php
+++ app/Controller/SharingController.php
@@ -104,6 +104,7 @@ class SharingController extends AppController{
                                 $this->CouponItem->addCoupon($uid, $couponId, $uid, 'shared_offer'.$shared_offer_id);
                                 $this->Weixin->send_coupon_received_message($uid, 1, "在".$sharedOffer['ShareOffer']['name']."店购买时有效","有效期至".friendlyDateFromStr($valid_end, 'full'));
                             }
+                            $left_slice -= 1;
                         }
                         $slice['SharedSlice']['accept_user'] = $uid;
                         $slice['SharedSlice']['accept_time'] = $now;
