0eb8cdb5837f81835f70f6804d7d7ae0b0c60ca5
liuzhaoren@gmail.com <liuzhaoren@gmail.com@1aee191c-eff3-477b-8fa7-d95ec15a78bd>
2014-11-19 03:50:06 +0000

    fix coupon template message

--- app/Controller/Component/WeixinComponent.php
+++ app/Controller/Component/WeixinComponent.php
@@ -129,12 +129,12 @@ class WeixinComponent extends Component
             "url" => $this->get_order_query_url($order_no),
             "topcolor" => "#FF0000",
             "data" => array(
-                "first" => array("value" => "亲，您的订单已完成付款，商家将即时为您发货。". ($number > 0 && !empty($name)) ? "恭喜您获得".$name."红包，马上领取" : ""),
+                "first" => array("value" => "亲，您的订单已完成付款，商家将即时为您发货。". (($number > 0 && !empty($name)) ? "恭喜您获得".$name."红包，点击领取" : "")),
                 "orderProductPrice" => array("value" => $price),
                 "orderProductName" => array("value" => $good_info),
                 "orderAddress" => array("value" => empty($ship_info)?'':$ship_info),
                 "orderName" => array("value" => $order_no),
-                "remark" => array("value" => "点击详情，查询订单".($number > 0 ? "/领取红包":"")."。", "color" => "#FF8800")
+                "remark" => array("value" => "点击查看订单详情".($number > 0 ? "/领取红包":"")."。", "color" => "#FF8800")
             )
         );
         return $this->send_weixin_message($post_data);
