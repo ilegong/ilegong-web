fc298f5d69a122daf25fc5636bcf40e1ce978736
liuzhaoren@gmail.com <liuzhaoren@gmail.com@1aee191c-eff3-477b-8fa7-d95ec15a78bd>
2014-11-26 11:45:27 +0000

    apple game: remove incorrect hint tips

--- webroot/js/apple_game.js
+++ webroot/js/apple_game.js
@@ -105,9 +105,6 @@ $(document).ready(function(){
     } else {
         timeout = 5000;
         var msg = '你力气太小啦！只晃掉了几片树叶！<br/><small>(5秒后自动消失)</small>';
-        if (total >= 90) {
-            msg += '<br/>大奖每天限量投放，越来越难，也可以考虑其他优惠券！<a href="#get_coupons" class="btn-sm btn-warning">查看</a>';
-        }
         utils.alert(msg, function(){}, timeout, close_callback);
     }
     updateViewState(times, total);
