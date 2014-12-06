350974f0bc3e5b18c83d225d60d36eee2196601b
liuzhaoren@gmail.com <liuzhaoren@gmail.com@1aee191c-eff3-477b-8fa7-d95ec15a78bd>
2014-12-06 16:29:09 +0000

    store management: try to fix bug in edit image

--- webroot/js/ckeditor/front-config.js
+++ webroot/js/ckeditor/front-config.js
@@ -53,4 +53,43 @@ CKEDITOR.editorConfig = function( config )
 	
 };
 
+CKEDITOR.on('instanceReady', function (ev) {
+    ev.editor.dataProcessor.htmlFilter.addRules(
+        {
+            elements:
+            {
+                $: function (element) {
+                    // Remove width and height style rules from images
+                    if (element.name == 'img') {
+                        var style = element.attributes.style;
+
+                        if (style) {
+                            // Get the width from the style.
+                            var match = /(?:^|\s)width\s*:\s*(\d+)px/i.exec(style),
+                                width = match && match[1];
+
+                            // Get the height from the style.
+                            match = /(?:^|\s)height\s*:\s*(\d+)px/i.exec(style);
+                            var height = match && match[1];
+
+                            if (width) {
+                                element.attributes.style = element.attributes.style.replace(/(?:^|\s)width\s*:\s*(\d+)px;?/i, '');
+                            }
+
+                            if (height) {
+                                element.attributes.style = element.attributes.style.replace(/(?:^|\s)height\s*:\s*(\d+)px;?/i, '');
+                            }
+                        }
+                    }
+
+                    if (!element.attributes.style)
+                        delete element.attributes.style;
+
+                    return element;
+                }
+            }
+        });
+});
+
+
 
