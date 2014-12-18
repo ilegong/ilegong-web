b03d9752a819c65c84d9492fc7850a27c3bb1b68
lipengyu@bupt.edu.cn <lipengyu@bupt.edu.cn@1aee191c-eff3-477b-8fa7-d95ec15a78bd>
2014-12-18 07:46:04 +0000

    revert OrderShichi.php

--- app/Model/OrderShichi.php
+++ app/Model/OrderShichi.php
@@ -1,12 +1,19 @@
 <?php
 /**
- * Created by IntelliJ IDEA.
- * User: liuzhr
- * Date: 12/17/14
- * Time: 9:20 AM
+ * Created by PhpStorm.
+ * User: algdev
+ * Date: 14/12/16
+ * Time: 下午12:25
  */
 
 class OrderShichi extends AppModel {
+
+
+    public function find_my_shichi_order_byId($orderId, $uid) {
+        return $this->find('first', array(
+            'conditions' => array('id' => $orderId, 'creator' => $uid),
+        ));
+    }
 
     public function bought_by_curr_user($tryId, $currUid) {
         return $this->find('count', array(
@@ -14,4 +21,60 @@ class OrderShichi extends AppModel {
         ));
     }
 
-} 
+
+    /*
+     * @param $uid
+     * @param $order_status  int|array
+     * @return array orders, order_carts and mapped brands
+     */
+
+    public function get_user_shichi_orders($uid,$order_status=null) {
+
+        $cond = array('creator' => $uid);
+        if ($order_status !== null) {
+            $cond['status'] = $order_status;
+        }
+        $orders = $this->find('all', array(
+            'order' => 'id desc',
+            'conditions' => $cond,
+        ));
+        $order_ids = array();
+        $brandIds = array();
+        foreach ($orders as $o) {
+            $order_ids[] = $o['OrderShichi']['order_id'];
+            $brandIds[] = $o['OrderShichi']['brand_id'];
+        }
+
+        $order_carts = array();
+        if (!empty($order_ids)) {
+            $cartM = ClassRegistry::init('Cart');
+            $Carts = $cartM->find('all', array(
+                'conditions' => array(
+                    'order_id' => $order_ids,
+                    'creator' => $uid
+//                    'status' => CART_ITEM_STATUS_BALANCED,
+                )));
+
+            foreach ($Carts as $c) {
+                $order_id = $c['Cart']['order_id'];
+                if (!isset($order_carts[$order_id])) $order_carts[$order_id] = array();
+                $order_carts[$order_id][] = $c;
+            }
+        }
+
+        $mappedBrands = array();
+        if (!empty($brandIds)) {
+            $brandM = ClassRegistry::init('Brand');
+            $brands = $brandM->find('all', array(
+                'conditions' => array('id' => $brandIds),
+                'fields' => array('id', 'name', 'created', 'slug', 'coverimg')
+            ));
+
+            foreach ($brands as $brand) {
+                $mappedBrands[$brand['Brand']['id']] = $brand;
+            }
+        }
+        return array($orders, $order_carts, $mappedBrands);
+    }
+
+}
