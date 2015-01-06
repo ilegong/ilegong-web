a48f22e77f52578fd9c7a7a0a9fc03a97e331dd7
shi_chao_peng@yeah.net <shi_chao_peng@yeah.net@1aee191c-eff3-477b-8fa7-d95ec15a78bd>
2015-01-06 04:16:23 +0000

    修改登陆样式和提示IE升级内容

--- webroot/js/iealert/iealert.js
+++ webroot/js/iealert/iealert.js
@@ -65,7 +65,7 @@ function initialize($obj, support, title, text){
 
 	$.fn.iealert = function(options){
 		var defaults = { 
-			support: "ie8",  // ie8 (ie6,ie7,ie8), ie7 (ie6,ie7), ie6 (ie6)
+			support: "ie7",  // ie8 (ie6,ie7,ie8), ie7 (ie6,ie7), ie6 (ie6)
 			title: "\u4F60\u77E5\u9053\u4F60\u7684Internet Explorer\u662F\u8FC7\u65F6\u4E86\u5417?", // title text
 			text: "\u4E3A\u4E86\u5F97\u5230\u6211\u4EEC\u7F51\u7AD9\u6700\u597D\u7684\u4F53\u9A8C\u6548\u679C,\u6211\u4EEC\u5EFA\u8BAE\u60A8\u5347\u7EA7\u5230\u6700\u65B0\u7248\u672C\u7684Internet Explorer\u6216\u9009\u62E9\u53E6\u4E00\u4E2Aweb\u6D4F\u89C8\u5668.\u4E00\u4E2A\u5217\u8868\u6700\u6D41\u884C\u7684web\u6D4F\u89C8\u5668\u5728\u4E0B\u9762\u53EF\u4EE5\u627E\u5230.<br /><br /><h1 id='goon' style='font-size:20px;cursor:pointer;'>>>>\u7EE7\u7EED\u8BBF\u95EE</h1>"
 		};
