cb4ded836f4e3883964a2a5f5919caf0c54052fe
liuzhaoren@gmail.com <liuzhaoren@gmail.com@1aee191c-eff3-477b-8fa7-d95ec15a78bd>
2015-01-16 13:42:23 +0000

    fix wx_menuV2.js

--- wx_menuV2.js
+++ wx_menuV2.js
@@ -1,42 +1,56 @@
 {
     "button":[
     {
-        "type":"view",
-        "name":"购买特产",
-        "url":"http://www.pyshuo.com/users/wx_menu_point?referer_key=CLICK_URL_TECHAN&from=service"
+        "name":"朋友推荐",
+        "sub_button":[
+            {"type": "view",
+                "name": "厨娘帮手",
+                "url": "http://www.pyshuo.com/subscripts/chu_niang_bang_shou.html"
+            },
+            {"type": "view",
+                "name": "美食故事",
+                "url": "http://www.pyshuo.com/subscripts/mei_shi_gu_shi.html"
+            }
+        ]
     },
     {
-        "type":"view",
-        "name":"摇一摇",
-        "url":"http://www.pyshuo.com/t/ag.html"
+        "name":"试吃团",
+        "sub_button":[
+            {"type": "view",
+                "name": "每期报道",
+                "url": "http://www.pyshuo.com/shichituan.html"
+            },
+            {"type": "view",
+                "name": "吃货聚会",
+                "url": "http://www.pyshuo.com/subscripts/chi_huo_ju_hui.html"
+            },
+            {"type": "view",
+                "name": "加入试吃",
+                "url": "http://www.pyshuo.com/users/wx_menu_point?referer_key=CLICK_URL_SHICHI_APPLY&do_wx_auth_if_not_log_in=1"
+            },
+            {"type": "view",
+                "name": "吃货集中营",
+                "url": "http://wsq.qq.com/reflow/177650290"
+            }
+        ]
     },
     {
-        "name":"个人中心",
+        "name":"线上活动",
         "sub_button":[
             {
                 "type":"view",
-                "name":"我的订单",
-                "url":"http://www.pyshuo.com/users/wx_menu_point?referer_key=CLICK_URL_MINE&do_wx_auth_if_not_log_in=1&from=service"
+                "name":"关于我们",
+                "url":"http://www.pyshuo.com/subscripts/about_us.html"
             },
             {
                 "type":"view",
-                "name":"微社区",
-                "url":"http://wsq.qq.com/reflow/177650290"
+                "name":"投稿有奖",
+                "url":"http://mp.weixin.qq.com/s?__biz=MjM5NzQ3NTkxNA==&mid=203733076&idx=1&sn=e9954ecd208dd60fb470ed6733aff026#rd"
             },
             {
                 "type":"view",
-                "name":"加入试吃",
-                "url":"http://www.pyshuo.com/users/wx_menu_point?referer_key=CLICK_URL_SHICHI_APPLY&do_wx_auth_if_not_log_in=1&from=service"
-            },
-            {
-                "type":"view",
-                "name":"售后服务",
-                "url":"http://www.pyshuo.com/users/wx_menu_point?referer_key=CLICK_URL_SALE_AFTER_SAIL&from=service"
-            },
-            {
-                "type":"click",
-                "name":"关注服务号",
-                "key":"CLICK_GUANZHU_FUWUHAO"
+                "name":"狂点调戏",
+                "url":"http://mp.weixin.qq.com/s?__biz=MjM5MjY5ODAyOA==&mid=200769784&idx=1&sn=8cce5a47e8a6123028169065877446b9#rd"
             }
         ]
     }
