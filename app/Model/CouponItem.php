2613a470ea5175791c0d4e933d277428d4a5e7c3
liangdyun@163.com <liangdyun@163.com@1aee191c-eff3-477b-8fa7-d95ec15a78bd>
2015-01-20 10:37:26 +0000

    fix a bug : nianhuo

--- app/Model/CouponItem.php
+++ app/Model/CouponItem.php
@@ -139,10 +139,10 @@ class CouponItem extends AppModel {
 
     public function add_spring_festival_coupon($userId, $pid) {
         $coupon_types = $this->read_spring_festivals($pid);
-        $couponType = $coupon_types[$pid];
+        $couponType = $coupon_types[0]['Coupon']['id'];
         if (!empty($couponType)) {
             $got_items = $this->find_got_spring_festival_coupons($userId, $pid);
-            if(empty($got_items)){
+            if(empty($got_items[$pid])){
                 $this->addCoupon($userId, $couponType, $userId, 'spring_festival');
                 return true;
             } else {
