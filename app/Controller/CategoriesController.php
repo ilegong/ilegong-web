0e7789a2eabd9606ed8b6056d755189881b21d57
shi_chao_peng@yeah.net <shi_chao_peng@yeah.net@1aee191c-eff3-477b-8fa7-d95ec15a78bd>
2015-01-21 01:57:06 +0000

    fix autom get coupon vug

--- app/Controller/CategoriesController.php
+++ app/Controller/CategoriesController.php
@@ -640,7 +640,6 @@ class CategoriesController extends AppController {
         $uid = $this->Session->read('Auth.User.id');
         $this->set('uid',$uid);
         $temp_pid = $this->Session->read('coupon-id');
-        $this->Session->delete('coupon-id');
         $this->set('pid',$temp_pid);
     }
     public function mobile_get_spring_coupon(){
