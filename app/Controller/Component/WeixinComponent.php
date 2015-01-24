604b24880be565da9f422ebfada5dc890504c28f
lipengyu@bupt.edu.cn <lipengyu@bupt.edu.cn@1aee191c-eff3-477b-8fa7-d95ec15a78bd>
2015-01-24 02:43:28 +0000

    fix send_message when orders were paid

--- app/Controller/Component/WeixinComponent.php
+++ app/Controller/Component/WeixinComponent.php
@@ -487,7 +487,11 @@ class WeixinComponent extends Component
 
         $good_num = $Cart->find('count',array('conditions' => array('order_id' => $order['Order']['id'],'creator' => $order['Order']['creator'])));
         $good_infomation = explode(';',$good_info);
+        if ($good_num == 1){
+        $msg = '用户'.$order_consinessname.'刚刚购买了'.$good_infomation[0].'等'.$good_num.'件商品，订单金额'.$price.'元，请您发货。订单号'.$order_id.'，关注服务号接收更详细信息。';
+        }else {
         $msg = '用户'.$order_consinessname.'刚刚购买了'.$good_infomation[0].'、'.$good_infomation[1].'等'.$good_num.'件商品，订单金额'.$price.'元，请您发货。订单号'.$order_id.'，关注服务号接收更详细信息。';
+        }
 
         $this->log('brand_creator:'.json_encode($brand_creator).'bussiness_info'.json_encode($bussiness_info).'bussiness_mobilephone'.json_encode($bussiness_mobilephone).'msg'.$msg);
         message_send($msg, $bussiness_mobilephone);
