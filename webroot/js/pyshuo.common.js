02f8ae6e2ffe8939c593245f1c759823736e6cdf
shi_chao_peng@yeah.net <shi_chao_peng@yeah.net@1aee191c-eff3-477b-8fa7-d95ec15a78bd>
2015-01-27 13:31:57 +0000

    fix comment tip

--- webroot/js/pyshuo.common.js
+++ webroot/js/pyshuo.common.js
@@ -27,15 +27,9 @@ Pyshuo.ui.utils={
             me.$tipInfoPanel = $('<div class="comment_tip_layer radius10" style="width:60%; left:50%; top:30%; margin-left:-30%; display: none;"></div>');
             $('body').append(me.$tipInfoPanel);
         }
-        fadeInTime = fadeInTime?fadeInTime:500;
-        fadeOutTime = fadeOutTime?fadeOutTime:1000;
-        callBack = callBack?callBack:function(){
-        };
-        me.$tipInfoPanel.text(msg).fadeIn(fadeInTime)
-        setTimeout(
-            function(){
-                me.$tipInfoPanel.fadeOut(fadeOutTime,callBack);
-            },1500);
-
+        fadeInTime = fadeInTime?fadeInTime:1000;
+        fadeOutTime = fadeOutTime?fadeOutTime:3000;
+        callBack = callBack?callBack:function(){};
+        me.$tipInfoPanel.text(msg).fadeIn(fadeInTime).fadeOut(fadeOutTime,callBack);
     }
 };
