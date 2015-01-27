cf54c79aa5cbad940b492943da9303477c11c260
liuzhaoren@gmail.com <liuzhaoren@gmail.com@1aee191c-eff3-477b-8fa7-d95ec15a78bd>
2015-01-27 11:43:58 +0000

    bugfix

--- app/Model/UserPrice.php
+++ app/Model/UserPrice.php
@@ -9,6 +9,14 @@
 class UserPrice extends AppModel {
 
     public function add($product_id, $customized_price, $uid, $cart_id) {
+        $found = $this->find('first', array(
+            'conditions' => array('product_id' => $product_id, 'uid' => $uid)
+        ));
+
+        if (!empty($found)) {
+            $this->id = $found['UserPrice']['id'];
+        }
+
         $this->save(array('product_id' => $product_id, 'customized_price' => $customized_price, 'uid' => $uid, 'cart_id' => $cart_id));
     }
 
