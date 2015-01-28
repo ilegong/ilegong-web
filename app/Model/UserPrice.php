62a5de0b1b5e9496646c8eff04545ad8065c068a
liuzhaoren@gmail.com <liuzhaoren@gmail.com@1aee191c-eff3-477b-8fa7-d95ec15a78bd>
2015-01-28 01:14:32 +0000

    add log for saving user price

--- app/Model/UserPrice.php
+++ app/Model/UserPrice.php
@@ -13,11 +13,15 @@ class UserPrice extends AppModel {
             'conditions' => array('product_id' => $product_id, 'uid' => $uid)
         ));
 
+        $action = 'new';
         if (!empty($found)) {
             $this->id = $found['UserPrice']['id'];
+            $action = 'update';
         }
 
         $this->save(array('product_id' => $product_id, 'customized_price' => $customized_price, 'uid' => $uid, 'cart_id' => $cart_id));
+
+        $this->log('add UserPrice:update/new?'.$action.', product_id='.$product_id.', customized_price='.$customized_price.', uid='.$uid.', cart_id='.$cart_id);
     }
 
 } 
