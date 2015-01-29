bab81f28c38c93a89473a5930e0ccc4978accabb
lipengyu@bupt.edu.cn <lipengyu@bupt.edu.cn@1aee191c-eff3-477b-8fa7-d95ec15a78bd>
2015-01-29 13:07:50 +0000

    modify title for guess_price

--- app/Controller/ProductsController.php
+++ app/Controller/ProductsController.php
@@ -439,7 +439,7 @@ class ProductsController extends AppController{
         $cartM = ClassRegistry::init('Cart');
         $total_sold = total_sold(PRODUCT_ID_JD_HS_NZT, array('start' => '2015-01-28 00:00:00', 'end' => '2014-01-29 00:00:00'), $cartM);
 
-        $this->pageTitle = '你敢出价，我就敢卖！';
+        $this->pageTitle = '你说多少钱，就卖多少钱！';
         $bannerItems = array(
             array('img' => "/img/guess_price/banner01.jpg"),
             array('img' => "/img/guess_price/banner02.jpg"),
