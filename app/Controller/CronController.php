4a782fff9b297532aa65a8cde2dbcd85d102d603
shi_chao_peng@yeah.net <shi_chao_peng@yeah.net@1aee191c-eff3-477b-8fa7-d95ec15a78bd>
2015-02-04 05:14:16 +0000

    log ship task url

--- app/Controller/CronController.php
+++ app/Controller/CronController.php
@@ -103,7 +103,7 @@ class CronController extends AppController
                         $this->log('push ship info '.$orderId.' wx send error on date '.$date);
                     }
                 }else{
-                    $this->log('push ship info '.$orderId.' can not fetch ship info on date '.$date);
+                    $this->log('push ship info '.$orderId.' can not fetch ship info on date '.$date.' url is '.$url);
                 }
             }
         }
