3b99d54aac7884a8e789415f30a451964afcb16c
liuzhaoren@gmail.com <liuzhaoren@gmail.com@1aee191c-eff3-477b-8fa7-d95ec15a78bd>
2015-02-04 07:13:33 +0000

    bugfix

--- app/Controller/GameJiujiuController.php
+++ app/Controller/GameJiujiuController.php
@@ -284,7 +284,7 @@ class GameJiujiuController extends AppController
                 if ($in_special_city) {
                     $rnd = mt_rand(0, 7);
                     $hourlyCnt = $this->CouponItem->couponCountHourly(self::COUPON_JIUJIU_FIRST, time());
-                    $this->log("exchange_coupon_first: special city=" . $in_special_city . ", rnd=" . $rnd .", hourlyCnt=".$hourlyCnt);
+                    $this->log("exchange_coupon_first: special city=" . $in_special_city . ", rnd=" . $rnd .", hourlyCnt=".$hourlyCnt.', hour_limit='.$this->hours_limit());
                     if ($rnd == 3) {
                         if (false /*$hourlyCnt < $this->hours_limit()*/) {
                             $coupon_count = 1;
