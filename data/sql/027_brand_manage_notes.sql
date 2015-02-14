938efee11f7df6208dd901223b7a838c757c8bba
liuzhaoren@gmail.com <liuzhaoren@gmail.com@1aee191c-eff3-477b-8fa7-d95ec15a78bd>
2015-02-14 15:57:41 +0000

    try to add manage notes

---
+++ data/sql/027_brand_manage_notes.sql
@@ -0,0 +1,4 @@
+alter table cake_brands add column manage_notes varchar(1000) default '' not null;
+
+INSERT INTO `cake_i18nfields` (`name`, `savetodb`, `translate`, `type`, `model`, `locale`, `length`, `sort`, `allowadd`, `allowedit`, `selectmodel`, `selectvaluefield`, `selecttxtfield`, `selectparentid`, `selectautoload`, `selectvalues`, `associateflag`, `associateelement`, `associatefield`, `associatetype`, `formtype`, `default`, `allownull`, `validationregular`, `description`, `onchange`, `explodeimplode`, `explain`, `deleted`, `created`, `updated`, `conditions`)
+VALUES ('manage_notes',1,'管理备注','string','Brand','zh_cn','400','17',1,1,NUll,NULL,NULl,NULL,1,'','0',NULL,NULL,'equal','textarea','',1,NULL,'',NULL,NULL,'用于管理员记录商家备忘信息',0,'0000-00-00 00:00:00','0000-00-00 00:00:00',NULL)
