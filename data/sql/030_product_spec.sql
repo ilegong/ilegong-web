a2b24cc64bcdfbfa899e36b45717b244482c3302
shichaopeng <shichaopeng@ilegong.com>
2015-03-10 10:34:06 +0800

    添加数据库更新脚本

--- data/sql/030_product_spec.sql
+++ data/sql/030_product_spec.sql
@@ -1,28 +1,34 @@
-CREATE TABLE cake_product_attributes (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(100) NOT NULL,
-  `deleted` INT NULL DEFAULT 0,
-  PRIMARY KEY (`id`));
+CREATE TABLE `cake_product_attributes` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) NOT NULL,
+  `deleted` int(11) DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
-  CREATE TABLE cake_product_specs (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(100) NULL,
-  `attr_id` INT NULL,
-  `deleted` INT NULL DEFAULT 0,
-  PRIMARY KEY (`id`));
+CREATE TABLE `cake_product_specs` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) DEFAULT NULL,
+  `attr_id` int(11) DEFAULT NULL,
+  `deleted` int(11) DEFAULT '0',
+  `product_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=87 DEFAULT CHARSET=utf8;
 
-CREATE TABLE cake_product_spec_groups (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `spec_ids` VARCHAR(100) NULL,
-  `price` FLOAT NULL,
-  `stock` INT NULL,
-  `deleted` INT NULL DEFAULT 0,
-  PRIMARY KEY (`id`));
 
-ALTER TABLE `cake_product_spec_groups`
-ADD COLUMN `spec_names` VARCHAR(500) NULL AFTER `deleted`;
+CREATE TABLE `cake_product_spec_groups` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `spec_ids` varchar(100) DEFAULT NULL,
+  `price` float DEFAULT NULL,
+  `stock` int(11) DEFAULT NULL,
+  `deleted` int(11) DEFAULT '0',
+  `spec_names` varchar(500) DEFAULT NULL,
+  `product_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=95 DEFAULT CHARSET=utf8;
 
-ALTER TABLE `cake_product_spec_groups`
-ADD COLUMN `product_id` INT NULL AFTER `spec_names`;
+INSERT INTO `cake_product_attributes` (`id`,`name`,`deleted`) VALUES (1,'口味',0);
+INSERT INTO `cake_product_attributes` (`id`,`name`,`deleted`) VALUES (2,'尺寸',0);
+INSERT INTO `cake_product_attributes` (`id`,`name`,`deleted`) VALUES (3,'蛋糕口味',0);
+
 
 
