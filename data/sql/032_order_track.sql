6cefce3f2d0e03da7c2aad36be213df3d4b8486a
shichaopeng <shichaopeng@ilegong.com>
2015-03-16 19:47:23 +0800

    track log crud

--- data/sql/032_order_track.sql
+++ data/sql/032_order_track.sql
@@ -1,14 +1,13 @@
 CREATE TABLE `cake_order_tracks` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `date` datetime NOT NULL,
-  `brand_id` int(11) NOT NULL,
   `product_id` int(11) NOT NULL,
   `deleted` int(11) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`)
 );
 
 CREATE TABLE `cake_track_order_maps` (
-  `id` INT NOT NULL,
+  `id` INT NOT NULL AUTO_INCREMENT,
   `track_id` INT NOT NULL,
   `order_id` INT NOT NULL,
   PRIMARY KEY (`id`));
