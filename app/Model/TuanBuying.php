2a026bbc4bc3770c672d1f4a1ef9081ec9874573
ldy <ldy@ilegong.com>
2015-03-20 18:27:09 +0800

    tuan: add user into tuan when buying tuan good

--- app/Model/TuanBuying.php
+++ app/Model/TuanBuying.php
@@ -10,10 +10,24 @@ class TuanBuying extends AppModel {
         $CartM = ClassRegistry::init('Cart');
         $cart_info = $CartM->find('first', array(
             'conditions' => array('order_id' => $order_id),
-            'fields' => array('num')
+            'fields' => array('num','creator')
         ));
         $sold_num = $cart_info['Cart']['num'];
         $this->updateAll(array('join_num' => 'join_num + 1', 'sold_num' => 'sold_num + ' . $sold_num), array('id' => $memberId));
+        $tuan_buying = $this->find('first', array(
+            'conditions' => array('id' => $memberId),
+            'fields' => array('tuan_id')
+        ));
+        $tuan_id = $tuan_buying['TuanBuying']['tuan_id'];
+        $uid = $cart_info['Cart']['creator'];
+        $TuanMemberM = ClassRegistry::init('TuanMember');
+        $has_joined_tuan = $TuanMemberM->hasAny(array('tuan_id' => $tuan_id, 'uid' => $uid));
+        if(!$has_joined_tuan){
+            $data['tuan_id'] = $tuan_id;
+            $data['uid'] = $uid;
+            $data['join_time'] = date('Y-m-d H:i:s');
+            $TuanMemberM->save($data);
+        }
 //        $tuan_info = $this->find('first', array(
 //            'conditions' => array('id' => $memberId),
 //            'fields' => array('sold_num')
