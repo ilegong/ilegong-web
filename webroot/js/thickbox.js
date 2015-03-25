c3ee6721af666968c521c3a6f30350eeaacd7074
ldy <ldy@ilegong.com>
2015-03-25 18:46:26 +0800

    tuan:fix balance css

--- webroot/js/thickbox.js
+++ webroot/js/thickbox.js
@@ -255,7 +255,7 @@ function tb_showIframe(){
 function tb_remove() {
     $("#TB_imageOff").unbind("click");
     $("#TB_closeWindowButton").unbind("click");
-    $("#TB_window").fadeOut("fast",function(){$('#TB_window,#TB_overlay,#TB_HideSelect').trigger("unload").unbind().remove();});
+    $("#TB_window").fadeOut("slow",function(){$('#TB_window,#TB_overlay,#TB_HideSelect').trigger("unload").unbind().remove();});
     $("#TB_load").remove();
     if (typeof document.body.style.maxHeight == "undefined") {//if IE 6
         $("body","html").css({height: "auto", width: "auto"});
