ade83104de802a7cfaa617b4e4064593648605b8
aqingsao <vinci.zhang@gmail.com>
2015-03-26 15:43:26 +0800

    add debug log for uploadfiles

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -323,6 +323,8 @@ class AppController extends Controller {
         			),
                 'order'=> array('sortorder DESC')
         ));
+        $this->log('Upload files for ' + ${$modelClass}[$modelClass]['id']);
+        $this->log(${$modelClass}['Uploadfile']);
         if(Configure::read($modelClass.'.view_nums')){// 记录访问次数
         	$this->{$modelClass}->updateAll(
         			array('views_count' => 'views_count+1'), 
