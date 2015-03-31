53376b2b3c0c0e635e7dd8a92559e93ba7d1af89
ldy <ldy@ilegong.com>
2015-03-31 19:43:09 +0800

    add haohao cake coupon url

--- app/Controller/ShortmessagesController.php
+++ app/Controller/ShortmessagesController.php
@@ -3,7 +3,7 @@
 class ShortmessagesController extends AppController {
 
     var $name = 'Shortmessages';
-	
+
 	function admin_add()
 	{
 		if (!empty($this->data) ) {
@@ -36,6 +36,29 @@ class ShortmessagesController extends AppController {
 		}
 		parent::admin_add();
 	}
+
+    public function get_haohao_cake_coupon(){
+        $coupon_id = 19220;
+        if (empty($this->currentUser['id']) && $this->is_weixin()) {
+            $ref = Router::url($_SERVER['REQUEST_URI']);
+            $this->redirect('/users/login.html?force_login=1&auto_weixin=' . $this->is_weixin() . '&referer=' . urlencode($ref));
+            exit();
+        }
+        $this->loadModel('Coupon');
+        $this->loadModel('CouponItem');
+        $user_id = $this->currentUser['id'];
+        $already_get = $this->CouponItem->hasAny(array('coupon_id' => $coupon_id, 'bind_user' => $user_id));
+        $this->set('noFlash', false);
+        if(!$already_get){
+            $this->CouponItem->addCoupon($user_id, $coupon_id, $user_id, 'haohao_cake');
+            $this->Session->setFlash(__('领取成功'));
+            $this->redirect('/users/my_coupons');
+        } else {
+            $this->log("already got for ".$user_id." of ". $coupon_id);
+            $this->Session->setFlash(__('你已经领过啦'));
+            $this->redirect('/users/my_coupons');
+        }
+    }
 
 }
 ?>
