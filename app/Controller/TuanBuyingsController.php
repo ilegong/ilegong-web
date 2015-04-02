b4b3d253b6bbb5ae4210461535d0058a56418d6c
shichaopeng <shichaopeng@ilegong.com>
2015-04-02 12:17:52 +0800

    列出团购产品 过滤掉已经停止的团

--- app/Controller/TuanBuyingsController.php
+++ app/Controller/TuanBuyingsController.php
@@ -334,7 +334,7 @@ class TuanBuyingsController extends AppController{
     }
     public function goods(){
         $this->pageTitle = '团购商品';
-        $tuan_products = $this->TuanBuying->find('all',array('conditions' => array("pid != " => 863),'group' => array('pid')));
+        $tuan_products = $this->TuanBuying->find('all',array('conditions' => array("pid != " => 863,'status'=>0),'group' => array('pid')));
         $tuan_product_ids = Hash::extract($tuan_products,'{n}.TuanBuying.pid');
         $this->loadModel('Product');
         $tuan_products_info = array();
