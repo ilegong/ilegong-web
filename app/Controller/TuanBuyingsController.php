2a861e2988ea22cb76ce0ce24bfbe1d28729ab5d
aqingsao <vinci.zhang@gmail.com>
2015-04-04 17:20:50 +0800

    update tips when consign time is null

--- app/Controller/TuanBuyingsController.php
+++ app/Controller/TuanBuyingsController.php
@@ -41,7 +41,7 @@ class TuanBuyingsController extends AppController{
         $pid=$tuan_b['TuanBuying']['pid'];
         //$end_time =  friendlyDateFromStr($tuan_b['TuanBuying']['end_time'], FORMAT_DATETIME);
         $end_time = $tuan_b['TuanBuying']['end_time'];
-        $consign_time = empty($tuan_b['TuanBuying']['consign_time'])? '成团后发货' : friendlyDateFromStr($tuan_b['TuanBuying']['consign_time'], FFDATE_CH_MD);
+        $consign_time = empty($tuan_b['TuanBuying']['consign_time'])? '成团后现摘' : friendlyDateFromStr($tuan_b['TuanBuying']['consign_time'], FFDATE_CH_MD);
         $this->set(compact('pid', 'consign_time', 'tuan_buy_id', 'tuan_team', 'end_time'));
         $sold_num = $tuan_b['TuanBuying']['sold_num'];
         $max_num = $tuan_b['TuanBuying']['max_num'];
