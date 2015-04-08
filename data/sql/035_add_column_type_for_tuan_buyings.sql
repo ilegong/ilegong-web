cd19bd03790f658a77260277627a625ce4ad4349
aqingsao <vinci.zhang@gmail.com>
2015-04-08 21:08:23 +0800

    rename column type to consignment_type

--- data/sql/035_add_column_type_for_tuan_buyings.sql
+++ data/sql/035_add_column_type_for_tuan_buyings.sql
@@ -1,3 +1,3 @@
-alter table cake_tuan_buyings add column `type` TINYINT(1) default '0' not null;
+alter table cake_tuan_buyings add column `consignment_type` TINYINT(1) default '0' not null;
 
-update cake_tuan_buyings set type = 1 where pid in (851, 381, 868, 874, 876, 879, 883, 884);
+update cake_tuan_buyings set consignment_type = 1 where pid in (851, 381, 868, 874, 876, 879, 883, 884);
--- data/sql/036_add_column_type_for_tuan_products.sql
+++ data/sql/036_add_column_type_for_tuan_products.sql
@@ -1,3 +1,3 @@
-alter table cake_tuan_products add column `type` TINYINT(1) default '0' not null;
+alter table cake_tuan_products add column `consignment_type` TINYINT(1) default '0' not null;
 
-update cake_tuan_products set type = 1 where product_id in (851, 381, 868, 874, 876, 879, 883, 884);
+update cake_tuan_products set consignment_type = 1 where product_id in (851, 381, 868, 874, 876, 879, 883, 884);
