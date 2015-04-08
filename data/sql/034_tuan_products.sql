dfbe21e106b45475192c5d04f5c03a15b9dbdecc
aqingsao <vinci.zhang@gmail.com>
2015-04-08 12:11:18 +0800

    insert existing data to tuan_products

--- data/sql/034_tuan_products.sql
+++ data/sql/034_tuan_products.sql
@@ -20,17 +20,17 @@ ALTER TABLE cake_tuan_products
         REFERENCES cake_products(id);
 
 -- '838'=>'草莓', '851' => '芒果', '862'=>'好好蛋糕', '863' => '草莓863', '230' => '蛋糕230','381'=>'牛肉干','868'=>'建平小米','873'=>'好好蛋糕大团','874'=>'海南出口金菠萝','876'=>'蔬菜单次试吃','879'=>'烟台苹果','883'=>'海南椰子冻奶酪','884'=>'释迦果'
-INSERT INTO `cake_tuan_products`(`product_id`, `tuan_price`) VALUES(838, 19);
-INSERT INTO `cake_tuan_products`(`product_id`, `tuan_price`) VALUES(851, 19);
-INSERT INTO `cake_tuan_products`(`product_id`, `tuan_price`) VALUES(862, 19);
-INSERT INTO `cake_tuan_products`(`product_id`, `tuan_price`) VALUES(863, 19);
-INSERT INTO `cake_tuan_products`(`product_id`, `tuan_price`) VALUES(230, 19);
-INSERT INTO `cake_tuan_products`(`product_id`, `tuan_price`) VALUES(381, 19);
-INSERT INTO `cake_tuan_products`(`product_id`, `tuan_price`) VALUES(868, 19);
-INSERT INTO `cake_tuan_products`(`product_id`, `tuan_price`) VALUES(873, 19);
-INSERT INTO `cake_tuan_products`(`product_id`, `tuan_price`) VALUES(874, 19);
-INSERT INTO `cake_tuan_products`(`product_id`, `tuan_price`) VALUES(876, 19);
-INSERT INTO `cake_tuan_products`(`product_id`, `tuan_price`) VALUES(879, 19);
-INSERT INTO `cake_tuan_products`(`product_id`, `tuan_price`) VALUES(883, 19);
-INSERT INTO `cake_tuan_products`(`product_id`, `tuan_price`) VALUES(884, 19);
+INSERT INTO `cake_tuan_products`(`product_id`, `alias`, `tuan_price`) VALUES(838, '兴寿草莓', 19);
+INSERT INTO `cake_tuan_products`(`product_id`, `alias`, `tuan_price`) VALUES(851, '海南芒果', 19);
+INSERT INTO `cake_tuan_products`(`product_id`, `alias`, `tuan_price`) VALUES(862, '好好蛋糕', 19);
+INSERT INTO `cake_tuan_products`(`product_id`, `alias`, `tuan_price`) VALUES(863, '16元草莓' 19);
+INSERT INTO `cake_tuan_products`(`product_id`, `alias`, `tuan_price`) VALUES(230, '薇薇安蛋糕', 19);
+INSERT INTO `cake_tuan_products`(`product_id`, `alias`, `tuan_price`) VALUES(381, '饭二牛肉干' 19);
+INSERT INTO `cake_tuan_products`(`product_id`, `alias`, `tuan_price`) VALUES(868, '建平小米', 19);
+INSERT INTO `cake_tuan_products`(`product_id`, `alias`, `tuan_price`) VALUES(873, '好好蛋糕大团', 19);
+INSERT INTO `cake_tuan_products`(`product_id`, `alias`, `tuan_price`) VALUES(874, '海南出口金菠萝', 19);
+INSERT INTO `cake_tuan_products`(`product_id`, `alias`, `tuan_price`) VALUES(876, '蔬菜单次试吃', 19);
+INSERT INTO `cake_tuan_products`(`product_id`, `alias`, `tuan_price`) VALUES(879, '烟台苹果', 19);
+INSERT INTO `cake_tuan_products`(`product_id`, `alias`, `tuan_price`) VALUES(883, '海南椰子冻奶酪', 19);
+INSERT INTO `cake_tuan_products`(`product_id`, `alias`, `tuan_price`) VALUES(884, '海南释迦果', 19);
 
