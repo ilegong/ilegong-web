3a83677a82f541feef1c9896c9faaf9b3d8f303a
shichaopeng <shichaopeng@ilegong.com>
2015-04-08 14:46:32 +0800

    团购查询 产品名字显示采用查询团购产品 替代写死

--- manage/Controller/TuanController.php
+++ manage/Controller/TuanController.php
@@ -213,7 +213,8 @@ class TuanController extends AppController{
          $tuan_teams = $this->TuanTeam->find('all', array('conditions' => array('id' => $tuan_ids), 'fields' => array('id', 'tuan_name','type')));
          $tuan_teams = Hash::combine($tuan_teams, '{n}.TuanTeam.id', '{n}.TuanTeam');
          $this->log('tuan_team'.json_encode($tuan_teams));
-         $tuan_products = array('838'=>'草莓', '851' => '芒果', '862'=>'好好蛋糕', '863' => '草莓863', '230' => '蛋糕230','381'=>'牛肉干','868'=>'建平小米','873'=>'好好蛋糕大团','874'=>'海南出口金菠萝','876'=>'蔬菜单次试吃','879'=>'烟台苹果','883'=>'海南椰子冻奶酪','884'=>'释迦果');;
+         $tuan_products = getTuanProducts();
+         $tuan_products = Hash::combine($tuan_products,'{n}.TuanProduct.product_id','{n}.TuanProduct.alias');
          foreach($tuan_buyings as &$tuan_buying){
              $tuanBuying = $tuan_buying['TuanBuying'];
              $tb_id = $tuanBuying['id'];
