beb47d074f97a353d0683d8c5a0de872432ca32a
aqingsao <vinci.zhang@gmail.com>
2015-04-09 19:12:20 +0800

    change url to tuan_teams/edit and tuan_teams/new

--- manage/Controller/TuanTeamsController.php
+++ manage/Controller/TuanTeamsController.php
@@ -31,36 +31,31 @@ class TuanTeamsController extends AppController{
         $this->set('tuan_teams',$tuan_teams);
         $this->set('team_id',$team_id);
     }
-    /**
-     * create a new tuan_team
-     */
-    public function admin_tuan_team_create(){
 
-        if(!empty($this->data)){
-            if($this->TuanTeam->save($this->data)){
-                $this->redirect(array('controller' => 'tuanTeams','action' => 'admin_tuan_teams'));
-            }
+    public function admin_new(){
+    }
+
+    public function admin_create(){
+        if($this->TuanTeam->save($this->data)){
+            $this->redirect(array('controller' => 'tuan_teams','action' => 'index'));
         }
     }
 
-    /**
-     * edit a tuan_team
-     */
-    public function admin_tuan_team_edit($id){
-
+    public function admin_edit($id){
         $data_Info = $this->TuanTeam->find('first',array('conditions' => array('id' => $id)));
         $this->log('data_info'.json_encode($data_Info));
         if (empty($data_Info)) {
             throw new ForbiddenException(__('该团队不存在！'));
         }
-        if(!empty($this->data)){
-            $this->data['TuanTeam']['id'] = $id;
-            $this->autoRender = false;
-            if($this->TuanTeam->save($this->data)){
-                $this->redirect(array('controller' => 'tuanTeams','action' => 'admin_tuan_teams'));
-            }
-        }else{
-            $this->data = $data_Info;
+        $this->data = $data_Info;
+        $this->set('id',$id);
+    }
+
+    public function admin_update($id){
+        $this->log('update tuan team '.$id.': '.json_encode($this->data));
+        $this->autoRender = false;
+        if($this->TuanTeam->save($this->data)){
+            $this->redirect(array('controller' => 'tuan_teams','action' => 'index'));
         }
         $this->set('id',$id);
     }
