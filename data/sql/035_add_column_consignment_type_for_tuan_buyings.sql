786ae61a9e248f06072be1ab831f78e71cede73a
aqingsao <vinci.zhang@gmail.com>
2015-04-09 10:41:55 +0800

    change column type from tiny int to small int for consignment_type

--- data/sql/035_add_column_consignment_type_for_tuan_buyings.sql
+++ data/sql/035_add_column_consignment_type_for_tuan_buyings.sql
@@ -1,3 +1,5 @@
-alter table cake_tuan_buyings add column `consignment_type` TINYINT(1) default '0' not null;
+alter table cake_tuan_buyings drop column `consignment_type`;
+
+alter table cake_tuan_buyings add column `consignment_type` SMALLINT(1) default '0' not null;
 
 update cake_tuan_buyings set consignment_type = 1 where pid in (851, 381, 868, 874, 876, 879, 883, 884);
--- data/sql/036_add_column_consignment_type_for_tuan_products.sql
+++ data/sql/036_add_column_consignment_type_for_tuan_products.sql
@@ -1,3 +1,5 @@
-alter table cake_tuan_products add column `consignment_type` TINYINT(1) default '0' not null;
+alter table cake_tuan_products drop column `consignment_type`;
+
+alter table cake_tuan_products add column `consignment_type` SMALLINT(1) default '0' not null;
 
 update cake_tuan_products set consignment_type = 1 where product_id in (851, 381, 868, 874, 876, 879, 883, 884);
