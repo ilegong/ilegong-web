ad3170008ce724d4c1e4f9fc9339b25505aa6f69
aqingsao <vinci.zhang@gmail.com>
2015-04-23 11:52:17 +0800

    update send_date of carts for seckill products

--- app/Controller/CartsController.php
+++ app/Controller/CartsController.php
@@ -53,6 +53,7 @@ class CartsController extends AppController{
             $product_id = $this->data['Cart']['product_id'];
             $num = $this->data['Cart']['num'];
             $specId = $this->data['Cart']['spec'];
+            $send_date = $this->data['Cart']['send_date'];
             $type = $buyingCom->convert_cart_type($this->data['Cart']['type']);
             $tryId = intval($_POST['try_id']);
             $uid = $this->currentUser['id'];
@@ -66,15 +67,11 @@ class CartsController extends AppController{
             }
 
             $returnInfo = $buyingCom->check_and_add($cartM, $type, $tryId, $uid, $num, $product_id, $specId, $sessionId);
-            $this->log('check_and_add:specId='.$specId.', type='.$type.', tryId='.$tryId.', uid='.$uid.',num='.$num.', product_id='.$product_id.', customized_price='.$customized_price.', returnInfo='.json_encode($returnInfo));
+            $this->log('check_and_add:specId='.$specId.', type='.$type.', tryId='.$tryId.', uid='.$uid.',num='.$num.', product_id='.$product_id.', customized_price='.$customized_price.', send_date='.$send_date.', returnInfo='.json_encode($returnInfo));
             if (!empty($returnInfo) && $returnInfo['success']) {
                 $cart_id = $returnInfo['id'];
-                if ($this->data['Cart']['send_date'] && $cart_id) {
-                    $send_date = trim($this->data['Cart']['send_date']);
-                    if ($send_date) {
-                        $this->log('update send_date to '.$send_date.' for cart '.$cart_id);
-                        $cartM->updateAll(array('send_date' => $send_date), array('id' => $cart_id));
-                    }
+                if ($send_date && $cart_id) {
+                    $cartM->updateAll(array('send_date' => "'".$send_date."'"), array('id' => $cart_id));
                 }
 
                 if (accept_user_price($product_id, $customized_price)) {
