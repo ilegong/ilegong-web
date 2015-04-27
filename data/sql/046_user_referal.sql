5e80d438897550b852d6d80d7edabf85c6009345
jeffliu <pengsi.liu@gmail.com>
2015-04-28 00:22:49 +0800

    add new sql files for referral

---
+++ data/sql/046_user_referal.sql
@@ -0,0 +1,15 @@
+CREATE TABLE `cake_user_refers` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `step` int(11) NOT NULL DEFAULT '0',
+  `from` bigint(20) NOT NULL,
+  `to` bigint(20) NOT NULL,
+  `last_ip` varchar(30) DEFAULT NULL,
+  `status` tinyint(1) DEFAULT '0',
+  `deleted` tinyint(1) DEFAULT '0',
+  `updated` datetime DEFAULT NULL,
+  `created` datetime DEFAULT NULL,
+  `latest_click_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  KEY `idx_from` (`from`),
+  KEY `idx_to` (`to`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
