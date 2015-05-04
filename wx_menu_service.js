82ea3fc830fb01da3be801e1c328de9e94fd1ad3
jeffliu <pengsi.liu@gmail.com>
2015-05-04 15:40:45 +0800

    add menu service

--- wx_menu_service.js
+++ wx_menu_service.js
@@ -30,13 +30,13 @@
             },
             {
                 "type":"view",
-                "name":"加入试吃",
-                "url":"http://www.pyshuo.com/users/wx_menu_point?referer_key=CLICK_URL_SHICHI_APPLY&do_wx_auth_if_not_log_in=1&from=service&_sl=wx.sv.sc"
+                "name":"售后服务",
+                "url":"http://www.pyshuo.com/users/wx_menu_point?referer_key=CLICK_URL_SALE_AFTER_SAIL&from=service&_sl=wx.sv.sv"
             },
             {
                 "type":"view",
-                "name":"售后服务",
-                "url":"http://www.pyshuo.com/users/wx_menu_point?referer_key=CLICK_URL_SALE_AFTER_SAIL&from=service&_sl=wx.sv.sv"
+                "name":"个人中心",
+                "url":"http://www.pyshuo.com/users/me.html?from=service&_sl=wx.sv.uc"
             }
         ]
     }
