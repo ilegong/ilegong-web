0ee08efb71025adc9155067e4585ffd824723a92
aqingsao <vinci.zhang@gmail.com>
2015-05-05 14:22:15 +0800

    could view all offline stores

--- manage/Controller/OfflineStoresController.php
+++ manage/Controller/OfflineStoresController.php
@@ -3,15 +3,24 @@ class OfflineStoresController extends AppController{
 
     var $name = 'OfflineStores';
 
-    var $uses = array('OfflineStore');
+    var $uses = array('OfflineStore', 'Location');
 
     public function admin_index(){
-        $offline_stores = $this->OfflineScore->find('all', array(
+        $offline_stores = $this->OfflineStore->find('all', array(
             'conditions' => array(
-                'published' => PUBLISH_YES
+                'deleted' => 0
             )
         ));
 
+        $beijing_id = 110100;
+        $locations = $this->Location->find('all', array(
+            'conditions' => array(
+                'parent_id' => $beijing_id,
+            )
+        ));
+        $locations = Hash::combine($locations, '{n}.Location.id', '{n}');
+
         $this->set('offline_stores',$offline_stores);
+        $this->set('locations',$locations);
     }
 }
