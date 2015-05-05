5dfb03eb531f421335255188ea250c56e1a233e4
aqingsao <vinci.zhang@gmail.com>
2015-05-05 14:45:33 +0800

    show tuan teams count in offline stores page

--- data/sql/049_add_column_published_to_tuan_teams.sql
+++ data/sql/049_add_column_published_to_tuan_teams.sql
@@ -2,4 +2,4 @@ ALTER TABLE `cake_tuan_teams`
   ADD COLUMN `published` TINYINT(1) DEFAULT '1' NOT NULL;
 
 ALTER TABLE `cake_tuan_teams`
-  ADD COLUMN `offline_score_id` INT(11) UNSIGNED NULL AFTER `leader_weixin`;
+  ADD COLUMN `offline_store_id` INT(11) UNSIGNED NULL AFTER `leader_weixin`;
--- manage/Controller/OfflineStoresController.php
+++ manage/Controller/OfflineStoresController.php
@@ -3,7 +3,7 @@ class OfflineStoresController extends AppController{
 
     var $name = 'OfflineStores';
 
-    var $uses = array('OfflineStore', 'Location');
+    var $uses = array('OfflineStore', 'Location', 'TuanTeam');
 
     public function admin_index(){
         $offline_stores = $this->OfflineStore->find('all', array(
@@ -11,6 +11,17 @@ class OfflineStoresController extends AppController{
                 'deleted' => 0
             )
         ));
+
+        $tuan_teams_count = $this->TuanTeam->query('select offline_store_id as id, count(offline_store_id) as c from cake_tuan_teams WHERE published = 1 group by offline_store_id;');
+        $tuan_teams_count = Hash::combine($tuan_teams_count, '{n}.cake_tuan_teams.id', '{n}.0.c');
+        foreach($offline_stores as &$offline_store){
+            $tuan_team_count = $tuan_teams_count[$offline_store['OfflineStore']['id']];
+            if(!isset($tuan_team_count) || is_null($tuan_team_count)){
+                $tuan_team_count = 0;
+            }
+
+            $offline_store['OfflineStore']['tuan_team_count'] = $tuan_team_count;
+        }
 
         $beijing_id = 110100;
         $locations = $this->Location->find('all', array(
