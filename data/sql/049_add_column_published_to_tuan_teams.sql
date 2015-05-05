5a60d2e5f5524387148ceaeda1147c66d4cdd347
aqingsao <vinci.zhang@gmail.com>
2015-05-05 12:18:15 +0800

    add published and offline_store_id to tuan_teams

---
+++ data/sql/049_add_column_published_to_tuan_teams.sql
@@ -0,0 +1,5 @@
+ALTER TABLE `cake_tuan_teams`
+  ADD COLUMN `published` TINYINT(1) DEFAULT '1' NOT NULL;
+
+ALTER TABLE `cake_tuan_teams`
+  ADD COLUMN `offline_score_id` INT(11) UNSIGNED NULL AFTER `leader_weixin`;
