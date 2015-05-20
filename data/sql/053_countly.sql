3ae75190f28ce232c2a7901936a233ba348155b1
shichaopeng <shichaopeng@ilegong.com>
2015-05-21 00:47:36 +0800

    add countly sql

---
+++ data/sql/053_countly.sql
@@ -0,0 +1,33 @@
+CREATE TABLE `cake_statistics_order_datas` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `new_user_buy_count` int(11) NOT NULL DEFAULT '0',
+  `all_order_count` int(11) NOT NULL DEFAULT '0',
+  `ziti_order_count` int(11) NOT NULL DEFAULT '0',
+  `tuan_order_count` int(11) NOT NULL DEFAULT '0',
+  `max_order_count` int(11) NOT NULL DEFAULT '0',
+  `created` datetime NOT NULL,
+  `all_new_user_count` int(11) NOT NULL DEFAULT '0',
+  `max_order_date` date NOT NULL,
+  `start_date` date NOT NULL,
+  `end_date` date NOT NULL,
+  `repeat_buy_count` int(11) NOT NULL DEFAULT '0',
+  `all_buy_user_count` int(11) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
+
+CREATE TABLE `cake_statistics_ziti_datas` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `new_user_buy_count` int(11) NOT NULL DEFAULT '0',
+  `repeat_buy_user` int(11) NOT NULL DEFAULT '0',
+  `all_buy_user_count` int(11) NOT NULL DEFAULT '0',
+  `order_max_count` int(11) NOT NULL DEFAULT '0',
+  `all_order_count` int(11) NOT NULL DEFAULT '0',
+  `area_id` int(11) NOT NULL DEFAULT '0',
+  `offline_store_id` int(11) NOT NULL DEFAULT '0',
+  `created` datetime NOT NULL,
+  `all_new_user_count` int(11) NOT NULL DEFAULT '0',
+  `start_date` date NOT NULL,
+  `end_date` date NOT NULL,
+  `max_order_date` date NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8;
