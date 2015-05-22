d41e66c7b9ab50f44106c198b483cf9f4041ce2d
shichaopeng <shichaopeng@ilegong.com>
2015-05-22 19:42:42 +0800

    fix order export excel filed null issue

--- webroot/js/manage-lib/tableExport.jquery.plugin/tablesToExcel.js
+++ webroot/js/manage-lib/tableExport.jquery.plugin/tablesToExcel.js
@@ -29,6 +29,7 @@ var tablesToExcel = (function() {
                     var dataStyle = tables[i].rows[j].cells[k].getAttribute("data-style");
                     var dataValue = tables[i].rows[j].cells[k].getAttribute("data-value");
                     dataValue = (dataValue)?dataValue:tables[i].rows[j].cells[k].textContent;
+                    dataValue = dataValue.replace(/\s+/g,"");
                     var dataFormula = tables[i].rows[j].cells[k].getAttribute("data-formula");
                     dataFormula = (dataFormula)?dataFormula:(appname=='Calc' && dataType=='DateTime')?dataValue:null;
                     ctx = {  attributeStyleID: (dataStyle=='Currency' || dataStyle=='Date')?' ss:StyleID="'+dataStyle+'"':''
