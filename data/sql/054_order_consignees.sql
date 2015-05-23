ff7013f8a5989eef3c81aa2377dc3b8a320d0818
shichaopeng <shichaopeng@ilegong.com>
2015-05-23 14:48:39 +0800

    add sql patch form custom remark address

--- data/sql/054_order_consignees.sql
+++ data/sql/054_order_consignees.sql
@@ -1,2 +1,4 @@
+ALTER TABLE `cake_offline_stores`
+ADD COLUMN `can_remark_address` TINYINT(1) NOT NULL DEFAULT 0 AFTER `deleted`;
 ALTER TABLE `cake_order_consignees`
 ADD COLUMN `remark_address` VARCHAR(100) NULL DEFAULT NULL AFTER `ziti_type`;
