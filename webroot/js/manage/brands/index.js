e5cb0e83965fd3b44f339e04bc321e249a40b70c
shichaopeng <shichaopeng@ilegong.com>
2015-05-27 10:57:01 +0800

    add manage table filter

---
+++ webroot/js/manage/brands/index.js
@@ -0,0 +1,10 @@
+/**
+ * Created by shichaopeng on 5/27/15.
+ */
+
+$(document).ready(function(){
+    var options = {
+        valueNames: [ 'id', 'name' ]
+    };
+    var userList = new List('brands', options);
+});
--- webroot/js/manage/tuan_products/index.js
+++ webroot/js/manage/tuan_products/index.js
@@ -1,6 +1,6 @@
 $(function(){
     var options = {
-        valueNames: [ 'id', 'name' ]
+        valueNames: [ 'id', 'name', 'alias' ]
     };
     var userList = new List('tuan_products', options);
 });
