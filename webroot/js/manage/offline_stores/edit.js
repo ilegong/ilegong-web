db9ceffab176dd641061be2ad8d9d2d3048bf436
coderbyr <lipengyu@bupt.edu.cn>
2015-06-01 15:38:25 +0800

    add child area for changping in offlinestore manage

--- webroot/js/manage/offline_stores/edit.js
+++ webroot/js/manage/offline_stores/edit.js
@@ -2,8 +2,12 @@ $(function () {
     var areaId = $('.area-id');
     var type = $('.type');
     var shopNo = $('.shop-no');
+    var childArea = $('.child-area-id');
     var initShopNo = function(){
         shopNo.toggleClass('hidden', type.val() != 0);
+    };
+    var initChildArea = function(){
+        childArea.toggleClass('hidden',areaId.data('value') !=110114);
     };
 //    var zitiAddress = zitiAddress('');
 //    var areas = zitiAddress.getBeijingAreas;
@@ -11,11 +15,19 @@ $(function () {
     $.each(beijingArea, function (index, item) {
         $('<option value="' + index + '">' + item + '</option>').appendTo(areaId);
     });
+    $.each(changpingArea, function (index, item) {
+        $('<option value="' + index + '">' + item + '</option>').appendTo(childArea);
+    });
     type.on('change', function(){
         initShopNo();
     });
-
+    areaId.on('change',function(){
+       var parentId = $(this).val();
+         childArea.toggleClass('hidden',parentId != 110114);
+    });
     iUtils.initSelectBox(areaId);
     iUtils.initSelectBox(type);
+    iUtils.initSelectBox(childArea);
     initShopNo();
+    initChildArea();
 });
