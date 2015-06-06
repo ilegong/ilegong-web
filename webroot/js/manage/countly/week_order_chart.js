434195b51f87d42f16b7ad8a0fead630bb44a76a
shichaopeng <shichaopeng@ilegong.com>
2015-06-06 19:34:30 +0800

    add chart for week order

--- webroot/js/manage/countly/week_order_chart.js
+++ webroot/js/manage/countly/week_order_chart.js
@@ -55,7 +55,7 @@ $(function () {
             }]
         },
         tooltip: {
-            valueSuffix: '°C'
+            valueSuffix: '份'
         },
         legend: {
             layout: 'vertical',
