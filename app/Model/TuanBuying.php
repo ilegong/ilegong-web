a484b41fb499737ebd4c1d40d1a537b945010b3e
shichaopeng <shichaopeng@ilegong.com>
2015-06-08 21:14:19 +0800

    fix index view sec kill num issue

--- app/Model/TuanBuying.php
+++ app/Model/TuanBuying.php
@@ -32,31 +32,6 @@ class TuanBuying extends AppModel {
             $TuanMemberM->save($data);
             $TuanTeamM->update(array('member_num' => 'member_num + 1'), array('id' => $tuan_id));
         }
-
-        //send join tuan buy success msg
-//        $tuan = $TuanTeamM->find('first',array(
-//            'conditions' => array(
-//                'id' => $tuan_id
-//            )
-//        ));
-//        $product_id = $tuan_buying['TuanBuying']['pid'];
-//        $product = $productM->find('first',array(
-//            'conditions' => array(
-//                'id'=>$product_id
-//            )
-//        ));
-//
-//        $product_name = $product['Product']['name'];
-//        $tuan_name = $tuan['TuanTeam']['tuan_name'];
-//        $title='您已参加'.$tuan_name.'发起的一个团购';
-//        $tuan_leader = $tuan['TuanTeam']['leader_weixin'];
-//        $consign_time = $tuan_buying['TuanBuying']['consign_time'];
-//        $consign_time = friendlyDateFromStr($consign_time,FFDATE_CH_MD);
-//        $product_name = $product_name.', '.$consign_time.'发货';
-//        $remark = '目标'.$tuan_buying['TuanBuying']['target_num'].'份，现在'.$tuan_buying['TuanBuying']['sold_num'].'份，点击详情，赶紧邀请小伙伴们加入，享受成团优惠价！';
-//        $deatil_url = WX_HOST.'/tuan_buyings/detail/'.$memberId;;
-//        //$this->Weixin->send_tuan_tip_msg($uid,$title,$product_name,$tuan_leader,$remark,$deatil_url);
-//        send_tuan_tip_msg($uid,$title,$product_name,$tuan_leader,$remark,$deatil_url);
-        //send sold num complete msg
+        Cache::write('tag-products23','[]');
     }
 }
