3e38660dfbe95b65fe6defb842215f3623e1b912
shichaopeng <shichaopeng@ilegong.com>
2015-06-08 09:54:21 +0800

    when sec buy many update product try sold num

--- app/Model/Order.php
+++ app/Model/Order.php
@@ -179,6 +179,11 @@ class Order extends AppModel {
                         }else{
                             $add_num = rand(1,$buy_multiple);
                         }
+                        //sec buy many
+                        $sec_num = $cartItems[0]['Cart']['num'];
+                        if($sec_num>1){
+                            $add_num = $sec_num;
+                        }
                         $tryM->updateAll(array('sold_num' => 'sold_num + '.$add_num), array('id' => $isTry, 'modified' => $pTry['ProductTry']['modified']));
                     }
                 } else if ($type == ORDER_TYPE_GROUP || $type == ORDER_TYPE_GROUP_FILL) {
