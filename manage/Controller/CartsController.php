6d48116d29f5965860c80ede2b260c1da7223d28
aqingsao <vinci.zhang@gmail.com>
2015-06-17 13:12:37 +0800

    could edit a cart

--- manage/Controller/CartsController.php
+++ manage/Controller/CartsController.php
@@ -31,4 +31,87 @@ class CartsController extends AppController{
         $this->set('order', $order);
         $this->set('cart', $cart);
     }
+
+    public function admin_update2($id)
+    {
+        $this->autoRender = false;
+
+        $username = $this->currentUser['username'];
+        $user_agent = $this->request->header('User-Agent');
+        $user_ip = $this->request->clientIp(true);
+        $this->log('user ' . $username . ' is to update cart ' . $id . ', request ip ' . $user_ip . ', user_agent ' . $user_agent);
+
+        $this->loadModel('Order');
+        $this->loadModel('Cart');
+        $cart = $this->Cart->findById($id);
+        $order = $this->Order->find('first', array(
+            'conditions' => array(
+                'id' => $cart['Cart']['order_id']
+            )
+        ));
+        if (empty($cart)) {
+            echo json_encode(array('success' => false, 'reason' => 'cart_not_exists'));
+            return;
+        }
+        if (empty($order)) {
+            echo json_encode(array('success' => false, 'reason' => 'order_not_exists'));
+            return;
+        }
+
+        if(!in_array($this->data['modify_user'], array('miaoyue', 'xiaoguang', 'xiaoqing', 'xinyu', 'jingge'))){
+            echo json_encode(array('success' => false, 'reason' => 'no_permission'));
+            return;
+        }
+
+        $remark = (empty($order['Order']['remark']) ? '' : $order['Order']['remark'] . ', ') . $this->data['modify_reason'] . '(' . $this->data['modify_user'] . ')';
+        unset($this->data['modify_reason']);
+        unset($this->data['modify_user']);
+
+        if(empty($this->data)){
+            echo json_encode(array('success' => false, 'reason' => 'fields_are_empty'));
+            return;
+        }
+
+        $new_cart_status = $this->data['status'];
+        if(!empty($new_cart_status) && !in_array($new_cart_status, array(ORDER_STATUS_SHIPPED, ORDER_STATUS_RETURNING_MONEY, ORDER_STATUS_RETURN_MONEY))){
+            echo json_encode(array('success' => false, 'reason' => 'invalid_status'));
+            return;
+        }
+
+        foreach($this->data as $key => $value){
+            $this->data[$key] = "'".$value."'";
+        }
+
+        $this->log('update cart ' . $id . ': '.json_encode($this->data));
+        if(!$this->Cart->updateAll($this->data, array('id' => $id))){
+            echo json_encode(array('success' => false, 'reason' => 'failed_to_save_cart'));
+            return;
+        }
+
+        $order_data = array('remark' => "'".$remark."'");
+        if($this->_need_to_update_order_status($order['Order']['id'], $new_cart_status)){
+            $order_data['status'] = "'".$new_cart_status."'";
+        }
+        $this->log('update order ' . $order['Order']['id'] . ': '.json_encode($order_data));
+        $this->Order->updateAll($order_data, array('id' => $order['Order']['id']));
+
+        echo json_encode(array('success' => true));
+    }
+
+    private function _need_to_update_order_status($order_id, $new_cart_status){
+        if(empty($new_cart_status)){
+            return false;
+        }
+        $carts = $this->Cart->find('all', array(
+            'conditions'=>array(
+                'order_id' => $order_id
+            )
+        ));
+        foreach($carts as &$cart){
+            if($cart['Cart']['status'] != $new_cart_status){
+                return false;
+            }
+        }
+        return true;
+    }
 }
--- webroot/js/manage/carts/edit.js
+++ webroot/js/manage/carts/edit.js
@@ -1,11 +1,5 @@
 $(document).ready(function () {
     var $saveBtn = $('.save-btn');
-    var $shipMark = $('.ship-mark');
-    var $consigneeId = $('.consignee-id');
-    var $consigneeName = $('.consignee-name');
-    var $consigneeMobilephone = $('.consignee-mobilephone');
-    var $consigneeAddress = $('.consignee-address');
-    var $remarkAddress = $('.remark-address');
     var $sendDate = $('.send-date');
     var $modifyReason = $('.modify-reason');
     var $modifyUser = $('.modify-user');
@@ -15,29 +9,10 @@ $(document).ready(function () {
         return date ;
     }
 
-    $shipMark.on('change', function () {
-        $consigneeId.parents('.form-group').toggleClass('hidden', $shipMark.val() != 'ziti');
-        $remarkAddress.parents('.form-group').toggleClass('hidden', $shipMark.val() != 'ziti');
-    });
-
     $saveBtn.on('click', function(){
         var invalidFields = [];
         if($sendDate.data('value') != $sendDate.val() && new Date($sendDate.val()) <= yesterday()){
             invalidFields.push($sendDate);
-        }
-        if(_.isEmpty($consigneeName.val())){
-            invalidFields.push($consigneeName);
-        }
-        if(!iUtils.isMobileValid($consigneeMobilephone.val())){
-            invalidFields.push($consigneeMobilephone);
-        }
-        if(_.isEmpty($consigneeAddress.val())){
-            invalidFields.push($consigneeAddress);
-        }
-        if($shipMark.val() == 'ziti'){
-            if(_.isEmpty($consigneeId.val())){
-                invalidFields.push($consigneeId);
-            }
         }
         if(_.isEmpty($modifyReason.val())){
             invalidFields.push($modifyReason);
@@ -64,24 +39,23 @@ $(document).ready(function () {
 
         var reasons = {
             'order_not_exists': '订单不存在',
+            'cart_not_exists': '发货单不存在',
             'fields_are_empty': '然而你并没有修改任何订单',
             'no_permission': '您没有权限',
             'invalid_send_date': '发货时间有误',
+            'invalid_status': '发货单状态只能修改为已发货、退款中、已退款',
             'failed_to_save_send_date': '保存发货时间失败',
             'missed_consignee_id': '请输入自提点',
-            'failed_to_save_order': '保存订单失败'
+            'failed_to_save_cart': '保存发货失败'
         }
-        var orderId = $('.order-id').val();
-        $.post('/manage/admin/orders/update2/' + orderId + ".json", json, function(data){
+        var cartId = $('.cart-id').val();
+        $.post('/manage/admin/carts/update2/' + cartId + ".json", json, function(data){
             if(data.success){
-                alert('修改成功: 订单号' + orderId);
+                alert('修改成功: 发货单号' + cartId);
             }
             else{
                 alert('修改失败: ' + reasons[data.reason]);
             }
         }, 'json');
     });
-
-    $consigneeId.parents('.form-group').toggleClass('hidden', $shipMark.val() != 'ziti');
-    $remarkAddress.parents('.form-group').toggleClass('hidden', $shipMark.val()  != 'ziti');
 });
