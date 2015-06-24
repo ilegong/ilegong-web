b4c862d1d1a1d3f8f3bddcf95018882ac10c865a
shichaopeng <shichaopeng@ilegong.com>
2015-06-24 16:11:40 +0800

    fix group buy style and add spec

--- app/Model/GroupBuy.php
+++ app/Model/GroupBuy.php
@@ -18,14 +18,15 @@ class GroupBuy extends AppModel{
     var $allGroupBuyProducts = array(
         '1069' => array(
             'id' => 1069,
-            'name' => '顺平水蜜桃-顺丰包邮 果汁四溢 绿色无农药 国家地理标志保护产品 4.5斤装',
-            'group_price' => 76.6,
+            'name' => '顺平水蜜桃-顺丰包邮 果汁四溢 绿色无农药 国家地理标志保护产品',
+            'group_price' => 88,
             'market_price' => 128,
             'spec' => 0,
+            'spec_name' => '2.4kg 左右',
             'closing_date' => '2015-06-25 18:00:00',
             'send_date' => '2015-06-26',
             'group_buy_num' => 5,
-            'desc' => '满3人团购价76.6'
+            'desc' => '满5人团购价88'
         )
     );
 
