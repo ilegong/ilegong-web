6a6f2c5bf367c8a6d6c9d13ec2469bcf9ab8c21c
shichaopeng <shichaopeng@ilegong.com>
2015-06-24 14:58:15 +0800

    group buy title use product alias

--- app/Model/GroupBuy.php
+++ app/Model/GroupBuy.php
@@ -18,7 +18,7 @@ class GroupBuy extends AppModel{
     var $allGroupBuyProducts = array(
         '1069' => array(
             'id' => 1069,
-            'name' => '顺平水蜜桃-顺丰包邮 4.5斤装',
+            'name' => '顺平水蜜桃-顺丰包邮 果汁四溢 绿色无农药 国家地理标志保护产品 4.5斤装',
             'group_price' => 76.6,
             'market_price' => 128,
             'closing_date' => '2015-06-25 18:00:00',
