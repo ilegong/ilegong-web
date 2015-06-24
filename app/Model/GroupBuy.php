130a7b8ee79123dbfbe73ad6c110552c61528ff2
shichaopeng <shichaopeng@ilegong.com>
2015-06-24 15:48:20 +0800

    fix group buy style

--- app/Model/GroupBuy.php
+++ app/Model/GroupBuy.php
@@ -21,15 +21,20 @@ class GroupBuy extends AppModel{
             'name' => '顺平水蜜桃-顺丰包邮 果汁四溢 绿色无农药 国家地理标志保护产品 4.5斤装',
             'group_price' => 76.6,
             'market_price' => 128,
+            'spec' => 0,
             'closing_date' => '2015-06-25 18:00:00',
             'send_date' => '2015-06-26',
-            'group_buy_num' => 3,
+            'group_buy_num' => 5,
             'desc' => '满3人团购价76.6'
         )
     );
 
     public function getGroupBuyProductInfo($pid){
         return $this->allGroupBuyProducts[$pid];
+    }
+
+    public function getProductGroupBuyNum($pid){
+        return $this->allGroupBuyProducts[$pid]['group_buy_num'];
     }
 
 }
