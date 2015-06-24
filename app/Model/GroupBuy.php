e9c35583cc1c2c341a15eaa6dc6936448e283834
shichaopeng <shichaopeng@ilegong.com>
2015-06-24 13:37:26 +0800

    add limit date time for group buy

--- app/Model/GroupBuy.php
+++ app/Model/GroupBuy.php
@@ -10,14 +10,18 @@ class GroupBuy extends AppModel{
 
     public $useTable = false;
 
-
+    /**
+     * @var array
+     *
+     * custom group by product
+     */
     var $allGroupBuyProducts = array(
         '883' => array(
             'id' => 883,
             'name' => '海南椰子冻',
             'group_price' => 10,
             'market_price' => 30,
-            'closing_date' => '2015-06-25',
+            'closing_date' => '2015-06-24 18:00:00',
             'send_date' => '2015-06-26',
             'group_buy_num' => 5,
             'desc' => '满5人购买享受10元团购价'
