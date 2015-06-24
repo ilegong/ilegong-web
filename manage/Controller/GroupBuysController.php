1a72b145adf6e84fe38ea66af790a6e5c552e5df
shichaopeng <shichaopeng@ilegong.com>
2015-06-24 14:52:27 +0800

    fix group buy product name

---
+++ manage/Controller/GroupBuysController.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: shichaopeng
+ * Date: 6/24/15
+ * Time: 14:42
+ */
+class GroupBuysController extends AppController{
+
+    var $name = 'GroupBuys';
+
+    var $uses = array();
+
+
+
+
+}
