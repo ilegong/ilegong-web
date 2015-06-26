41a6f6cd604b5705160e0fc116ea9d7eb6dd3406
aqingsao <vinci.zhang@gmail.com>
2015-06-26 19:52:43 +0800

    improve plan helper

--- manage/Controller/PlanHelperController.php
+++ manage/Controller/PlanHelperController.php
@@ -180,14 +180,15 @@ class PlanHelperController extends AppController
         $users = $this->User->find('all', array(
             "conditions" => array(
                 "id NOT IN (SELECT DISTINCT creator FROM cake_orders WHERE creator IS NOT NULL AND status > 0)",
-                "nickname IS NOT NULL and nickname != ''"
+                "nickname IS NOT NULL and nickname != ''",
+                "mobilephone IS NOT NULL and mobilephone != ''"
             ),
-            'limit' => count($orders)
+            'limit' => max(count($orders) / 3, 1),
         ));
 
-        foreach ($orders as $index => &$order) {
-            $user = $users[$index];
-            $this->Order->updateAll(array("creator" => "'" . $user['User']['id'] . "'", 'consignee_name' => "'" . $user['User']['nickname'] . "'", 'status' => "'" . ORDER_STATUS_SHIPPED . "'", 'published' => "'" . PUBLISH_NO . "'"), array('id' => $order['Order']['id']));
+        foreach ($orders as &$order) {
+            $user = $users[array_rand($users)];
+            $this->Order->updateAll(array("creator" => "'" . $user['User']['id'] . "'", 'consignee_name' => "'" . $user['User']['nickname'] . "'", 'consignee_mobilephone' => "'" . $user['User']['nickname'] . "'", 'status' => "'" . ORDER_STATUS_SHIPPED . "'", 'published' => "'" . PUBLISH_NO . "'"), array('id' => $order['Order']['id']));
             $this->Cart->updateAll(array('status' => "'" . ORDER_STATUS_SHIPPED . "'"), array('order_id' => $order['Order']['id']));
         }
         echo json_encode(array("order_ids" => $order_ids));
