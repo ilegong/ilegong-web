e989107ec1d5fb375d58b39d74e988302d38bf1e
ldy <ldy@ilegong.com>
2015-06-30 20:33:57 +0800

    baby vote function

--- app/Controller/VoteController.php
+++ app/Controller/VoteController.php
@@ -35,7 +35,39 @@ class VoteController extends AppController {
      * 给特定人和特定投票活动 进行投票
      */
     public function vote($candidateId, $eventId) {
-
+        $this->autoRender = false;
+        $uid = $this->currentUser['id'];
+        if(empty($uid)){
+            echo json_encode(array('success' => false, 'reason' => 'Not logged'));
+            return;
+        }
+        if(user_subscribed_pys($uid) != WX_STATUS_SUBSCRIBED ){
+            echo json_encode(array('success' => false, 'reason' => 'Not subscribed'));
+            return;
+        }
+        $uvote = $this->Vote->find('all', array(
+            'conditions' => array(
+                'user_id' => $uid,
+                'event_id' => $eventId,
+                'created >'=> date('Y-m-d', time()),
+                'created <'=> date('Y-m-d', strtotime('+1 day')),
+            )
+        ));
+        $already_vote_candidate = Hash::extract($uvote, '{n}.Vote.candidate_id');
+        if(count($uvote) >= 5){
+            echo json_encode(array('success' => false, 'reason' => 'more than five'));
+            return;
+        }
+        if(in_array($candidateId, $already_vote_candidate)){
+            echo json_encode(array('success' => false, 'reason' => 'already vote'));
+            return;
+        }
+        $vote = $this->Vote->save(array('candidate_id' => $candidateId, 'user_id'=>$uid, 'event_id'=>$eventId));
+        if(empty($vote)) {
+            echo json_encode(array('success' => false, 'reason' => 'save wrong'));
+            return;
+        }
+        echo json_encode(array('success' => true));
     }
 
     /**
