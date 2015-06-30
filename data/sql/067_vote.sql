0d5e51bfde823d172c13f8a075268e1df0bf4481
shichaopeng <shichaopeng@ilegong.com>
2015-06-30 16:13:33 +0800

    add vote sql

--- data/sql/067_vote.sql
+++ data/sql/067_vote.sql
@@ -1,7 +1,16 @@
+CREATE TABLE cake_vote_events(
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  mark VARCHAR (100) UNIQUE,
+  start_time DATETIME NOT NULL,
+  end_time DATETIME NOT NULL,
+  place VARCHAR (300),
+  published tinyint(4) NOT NULL DEFAULT '0',
+	deleted tinyint(4) NOT NULL DEFAULT '0',
+);
 
 CREATE TABLE cake_candidates (
 	id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-	tag VARCHAR (50),
+	tag INT (11),
 	title VARCHAR(50),
 	description TEXT DEFAULT NULL,
 	images VARCHAR (300),
