c71564fdf12b481d05bfff13dba46426f0466cce
shichaopeng <shichaopeng@ilegong.com>
2015-07-08 14:35:17 +0800

    add tiny buy structure

---
+++ app/Controller/TinyBuyController.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: shichaopeng
+ * Date: 7/8/15
+ * Time: 14:12
+ */
+class TinyBuyController extends AppController {
+
+
+    public function add() {
+        
+    }
+
+    public function create() {
+
+    }
+
+    public function detail() {
+
+    }
+
+    public function buy() {
+
+    }
+
+    private function saveTinyBuyProducts() {
+
+    }
+
+    private function saveTinyBuyAddresses() {
+
+    }
+}
