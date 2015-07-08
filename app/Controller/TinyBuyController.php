991166ffa38a7c458c7791313016e8c495694147
shichaopeng <shichaopeng@ilegong.com>
2015-07-08 16:52:16 +0800

    add tiny buy struct

--- app/Controller/TinyBuyController.php
+++ app/Controller/TinyBuyController.php
@@ -33,15 +33,58 @@ class TinyBuyController extends AppController {
     }
 
     public function detail($tinyBuyId) {
-
+        
     }
 
     public function buy() {
 
     }
 
+    public function pay($orderId){
+
+    }
+
     public function makeOrder(){
-        
+        $uid = $this->currentUser['id'];
+        $postData = $_POST['postData'];
+        $postDataArray = json_decode($postData, true);
+        $products = $postDataArray['products'];
+        $tinyBuyId = $postDataArray['tinyBuyId'];
+        $addressId = $postDataArray['addressId'];
+        $cart = array();
+        $tinyBuyProductIds = Hash::extract($products, '{n}.id');
+        $productIdNumMap = Hash::combine($products, '{n}.id','{n}.num');
+        $tinyProducts = $this->TinyBuyProduct->find('all', array(
+            'conditions' => array(
+                'id' => $tinyBuyProductIds,
+                'tiny_buy_id' => $tinyBuyId
+            )
+        ));
+        $order = $this->Order->save(array('creator' => $uid ,'member_id' => $tinyBuyId, 'type' => ORDER_TYPE_TINY_BUY, 'created' => date('Y-m-d H:i:s'), 'updated' => date('Y-m-d H:i:s'), 'consignee_id' => $addressId));
+        $orderId = $order['Order']['id'];
+        $totalPrice = 0;
+        foreach ($tinyProducts as $p) {
+            $item = array();
+            $pid = $p['TinyBuyProduct']['id'];
+            $num = $productIdNumMap[$pid];
+            $price = $p['TinyBuyProduct']['price'];
+            $item['name'] = $p['TinyBuyProduct']['name'];
+            $item['num'] = $num;
+            $item['price'] = $price;
+            $item['type'] = ORDER_TYPE_TINY_BUY;
+            $item['product_id'] = $p['TinyBuyProduct']['id'];
+            $item['created'] = date('Y-m-d H:i:s');
+            $item['updated'] = date('Y-m-d H:i:s');
+            $item['creator'] = $uid;
+            $item['order_id'] = $orderId;
+            $cart[] = $item;
+            $totalPrice+= $num*$price;
+        }
+        $this->Cart->saveAll($cart);
+        $this->Order->updateAll(array('total_all_price' => $totalPrice/100, 'total_price' => $totalPrice/100, 'ship_fee' => 0), array('id' => $orderId));
+
+        echo json_encode(array('success' => true, 'orderId' => $orderId));
+        return;
     }
 
     private function saveTinyBuyProducts($tinyBuyId,$tinyBuyProductData) {
