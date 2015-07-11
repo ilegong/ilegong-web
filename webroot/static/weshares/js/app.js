e2603c5e70a58a3069d7eb5f8b436df70f6e6107
aqingsao <vinci.zhang@gmail.com>
2015-07-11 12:25:21 +0800

    work on uploading images

--- webroot/static/weshares/js/app.js
+++ webroot/static/weshares/js/app.js
@@ -1,6 +1,7 @@
-(function (window, angular) {
+(function (window, angular, wx) {
 	angular.module('weshares', ['ui.router'])
 		.constant('_', window._)
+		.constant('wx', wx)
 		.config(configCompileProvider)
 		.config(configHttpProvider)
 		.config(configStates)
@@ -18,54 +19,75 @@
 		$httpProvider.defaults.headers.common['Content-Type'] = 'application/json';
 		$httpProvider.defaults.headers.post['Content-Type'] = 'application/json';
 	}
+
 	/* @ngInject */
 	function configStates($stateProvider, $urlRouterProvider, $locationProvider) {
 		$stateProvider
 			.state('add', {url: '/add', templateUrl: '/static/weshares/templates/add.html',controller: 'WesharesAddCtrl as vm'})
-            .state('view', {url: '/view', templateUrl: '/static/weshares/templates/view.html',controller: 'WesharesViewCtrl as vm'});
+			.state('view', {url: '/view', templateUrl: '/static/weshares/templates/view.html',controller: 'WesharesViewCtrl as vm'});
 
 		$urlRouterProvider.otherwise('/add');
 		$locationProvider.hashPrefix('!').html5Mode(false);
 	}
+
 	/* @ngInject */
-	function extendLog($provide){
-		$provide.decorator('$log', function($delegate, $injector){
+	function extendLog($provide) {
+		$provide.decorator('$log', function ($delegate, $injector) {
 			var _log = $delegate.log;
 			var _warn = $delegate.warn;
 			var _info = $delegate.info;
 			var _debug = $delegate.debug;
 			var _error = $delegate.error;
-			var addMessage = function(message, forceLog){
+			var addMessage = function (message, forceLog) {
 				var $rootScope = $injector.get("$rootScope");
 				$rootScope.config = $rootScope.config || {logMode: false};
-				if($rootScope.config.logMode || forceLog){
+				if ($rootScope.config.logMode || forceLog) {
 					$rootScope.messages = $rootScope.messages || [];
 					$rootScope.messages.push(message);
 				}
 				return message;
 			}
 
-			$delegate.log = function(msg, forceLog){_log(addMessage(msg, forceLog || false)); return this;};
-			$delegate.warn = function(msg, forceLog){_warn(addMessage(msg, forceLog || false)); return this;};
-			$delegate.info = function(msg, forceLog){_info(addMessage(msg, forceLog || false)); return this;};
-			$delegate.debug = function(msg, forceLog){_debug(addMessage(msg, forceLog || false)); return this;};
-			$delegate.error = function(msg, forceLog){_error(addMessage(msg, forceLog || false)); return this;};
+			$delegate.log = function (msg, forceLog) {
+				_log(addMessage(msg, forceLog || false));
+				return this;
+			};
+			$delegate.warn = function (msg, forceLog) {
+				_warn(addMessage(msg, forceLog || false));
+				return this;
+			};
+			$delegate.info = function (msg, forceLog) {
+				_info(addMessage(msg, forceLog || false));
+				return this;
+			};
+			$delegate.debug = function (msg, forceLog) {
+				_debug(addMessage(msg, forceLog || false));
+				return this;
+			};
+			$delegate.error = function (msg, forceLog) {
+				_error(addMessage(msg, forceLog || false));
+				return this;
+			};
 
 			return $delegate;
 		});
 	}
 
-	function initApp($rootScope){
+	function initApp($rootScope) {
 		$rootScope._ = _;
 	}
 
-	function WesharesAddCtrl($scope, $rootScope, $log, $http) {
+	function WesharesAddCtrl($scope, $rootScope, $log, $http, $timeout, wx) {
 		var vm = this;
-		vm.nextStep = nextStep;
-		vm.submit = submit;
+		vm.chooseAndUploadImage = chooseAndUploadImage;
+		vm.uploadImage = uploadImage;
+		vm.uploadImageToWXSucceeded = uploadImageToWXSucceeded;
+
 		vm.toggleProduct = toggleProduct;
 		vm.toggleAddress = toggleAddress;
-		vm.getImageSource = getImageSource;
+
+		vm.nextStep = nextStep;
+		vm.submit = submit;
 
 		activate();
 
@@ -75,54 +97,91 @@
 				title: '',
 				description: '',
 				images: [],
-				products: [{name: '', price: 0}],
+				products: [
+					{name: '', price: 0}
+				],
 				send_date: '',
-				addresses: [{address: ''}]
+				addresses: [
+					{address: ''}
+				]
 			}
 		}
 
-		function toggleProduct(product, isLast){
-			if(isLast){
+		function chooseAndUploadImage() {
+			wx.chooseImage({
+				success: function (res) {
+					$log.log('choose image succeeded: ').log(res);
+					$timeout(function(){
+						_.each(res.localIds, function(localId){
+							var image = {localId: localId};
+							vm.weshare.images.push(localId);
+							vm.uploadImage(image);
+						})
+					}, 30);
+				}
+			});
+		}
+		function uploadImage(image){
+			wx.uploadImage({
+				localId: image.localId,
+				isShowProgressTips:1,
+				success : function(res){
+					image.serverId = res.serverId;
+					$http.get('/downloads/download_wx_img?media_id='+image.serverId).success(function(data, status, headers, config){
+						var imageUrl = data['download_url'];
+						if(!imageUrl || imageUrl=='false'){
+							return;
+						}
+						image.url = imageUrl;
+					}).error(function(data, status, headers, config){
+						//TODO: 怎么处理?
+					});
+				},
+				fail: function(){
+					vm.weshare.images = _.without(vm.weshare.images, image);
+				}
+			});
+		}
+
+		function toggleProduct(product, isLast) {
+			if (isLast) {
 				vm.weshare.products.push({name: 'fdaf', price: 100});
 			}
-			else{
+			else {
 				vm.weshare.products = _.without(vm.weshare.products, product);
 			}
 		}
-		function toggleAddress(address, isLast){
-			if(isLast){
+
+		function toggleAddress(address, isLast) {
+			if (isLast) {
 				vm.weshare.addresses.push({name: ''});
 			}
-			else{
+			else {
 				vm.weshare.addresses = _.without(vm.weshare.addresses, address);
 			}
 		}
-		function getImageSource(image){
-			if(image.src == ''){
-				return '/weshares/images/good.jpg';
-			}
-			return image.src;
-		}
-		function nextStep(){
-			if(_.isEmpty(vm.weshare.title)){
+
+		function nextStep() {
+			if (_.isEmpty(vm.weshare.title)) {
 
 				return false;
 			}
 
-			vm.showShippmentInfo=true;
+			vm.showShippmentInfo = true;
 		}
-		function submit(){
+
+		function submit() {
 			$log.log('submitted').log(vm.weshare);
-			$http.post('/weshares/create', vm.weshare).success(function(data, status, headers, config){
-				if(status == 200){
+			$http.post('/weshares/create', vm.weshare).success(function (data, status, headers, config) {
+				if (status == 200) {
 					$log.log('post succeeded, data: ').log(data);
 				}
-				else{
-					$log.log("failed with status: " + status +", data: ").log(data);
+				else {
+					$log.log("failed with status: " + status + ", data: ").log(data);
 				}
-			}).error(function(data, status, headers, config){
+			}).error(function (data, status, headers, config) {
 					$log.log("failed with status :" + status + ", data: ").log(data).log(', and config').log(config);
 				});
 		}
 	}
-})(window, window.angular);
+})(window, window.angular, window.wx);
