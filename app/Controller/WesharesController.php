d9ead7c9a2658c7a11b0acfb0d41b933c737a76b
shichaopeng <shichaopeng@ilegong.com>
2015-07-17 16:06:49 +0800

    weshare add view add share param s

--- app/Controller/WesharesController.php
+++ app/Controller/WesharesController.php
@@ -21,7 +21,10 @@ class WesharesController extends AppController {
             $wexin_params = $this->set_weixin_share_data($currentUser['id'],0);
             $this->set($wexin_params);
             $title = $currentUser['nickname'].'邀请你一起来加入分享';
-            $image = $currentUser['image'] || 'http://dev.tongshijia.com/img/logo_footer.jpg';
+            $image = $currentUser['image'];
+            if(!$image){
+                $image = 'http://dev.tongshijia.com/img/logo_footer.jpg';
+            }
             $desc = '朋友说是一个有人情味的分享社区，这里你不但可以吃到各地的特产，还能认识有趣的人。';
             $this->set('title', $title);
             $this->set('image', $image);
