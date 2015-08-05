0b0b027bbdd2a2a8ffc14ac77a7fb4daa7a06f2a
shichaopeng <shichaopeng@ilegong.com>
2015-08-05 16:39:47 +0800

    when add new share send msg for fans

--- app/Controller/WesharesController.php
+++ app/Controller/WesharesController.php
@@ -141,6 +141,7 @@ class WesharesController extends AppController {
         $this->saveWeshareAddresses($weshare['Weshare']['id'], $addressesData);
         $this->saevWeshareShipType($weshare['Weshare']['id'], $shipSetData);
         if ($saveBuyFlag) {
+            $this->WeshareBuy->send_new_share_msg($weshare['Weshare']['id']);
             echo json_encode(array('success' => true, 'id' => $weshare['Weshare']['id']));
             return;
         } else {
