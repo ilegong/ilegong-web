ad7501a9addbcbe7f955b9d6a4895e34ee711273
shichaopeng <shichaopeng@ilegong.com>
2015-08-05 16:29:57 +0800

    fix weshare view js error

--- webroot/static/weshares/js/view-controller.js
+++ webroot/static/weshares/js/view-controller.js
@@ -153,7 +153,7 @@
           vm.consignee = data['consignee'];
           vm.myCoupons = data['my_coupons'];
           vm.weshareSettings = data['weshare_ship_settings'];
-          if(vm.consignee.offlineStore){
+          if(vm.consignee&&vm.consignee.offlineStore){
             vm.checkedOfflineStore = vm.consignee.offlineStore;
           }
           vm.selectShipType = -1;
