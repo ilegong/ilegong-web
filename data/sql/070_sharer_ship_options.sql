1037a9efdb9acc20957506e0b2b02bc271de8e6f
shichaopeng <shichaopeng@ilegong.com>
2015-08-05 11:15:51 +0800

    update share ship setting patch sql

--- data/sql/070_sharer_ship_options.sql
+++ data/sql/070_sharer_ship_options.sql
@@ -1,13 +1,15 @@
 CREATE TABLE `cake_sharer_ship_options` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `sharer_id` INT NOT NULL DEFAULT 0,
-  `type` INT(2) NOT NULL DEFAULT 0,
-  PRIMARY KEY (`id`));
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `sharer_id` int(11) NOT NULL DEFAULT '0',
+  `type` int(2) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-  CREATE TABLE `cake_weshare_ship_settings` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `weshare_id` INT NOT NULL DEFAULT 0,
-  `type` INT NOT NULL DEFAULT 0,
-  `ship_fee` INT NOT NULL DEFAULT 0,
-  PRIMARY KEY (`id`));
-
+CREATE TABLE `cake_weshare_ship_settings` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `weshare_id` int(11) NOT NULL DEFAULT '0',
+  `status` int(2) NOT NULL DEFAULT '0',
+  `ship_fee` int(11) NOT NULL DEFAULT '0',
+  `tag` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=61 DEFAULT CHARSET=utf8;
