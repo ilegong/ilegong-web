87b0ef9cbd43beb3a1c4af552c646da4a8ab7013
shichaopeng <shichaopeng@ilegong.com>
2015-08-28 11:35:18 +0800

    index css compress

--- compress.sh
+++ compress.sh
@@ -3,3 +3,5 @@
 uglifyjs ./webroot/static/weshares/js/angular.min.js  ./webroot/static/weshares/js/angular-ui-router.min.js ./webroot/static/weshares/js/underscore-min.js  ./webroot/static/weshares/js/app.js ./webroot/static/weshares/js/edit-controller.js ./webroot/static/weshares/js/view-controller.js -o ./webroot/static/weshares/js/weshare.min.js
 
 cat ./webroot/static/weshares/css/main.css ./webroot/static/weshares/css/site-common.css ./webroot/css/font-awesome-4.4.0/css/font-awesome.min.css | cleancss -o ./webroot/static/weshares/css/weshare.min.css
+
+cat ./webroot/static/weshares/css/common.css ./webroot/static/weshares/css/index-view.css ./webroot/static/weshares/css/site-common.css | cleancss -o ./webroot/static/weshares/css/index.min.css
