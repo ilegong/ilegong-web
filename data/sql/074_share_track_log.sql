3dca5aea83b3c0c0ea5a82da2ee541787c524cc3
shichaopeng <shichaopeng@ilegong.com>
2015-08-31 15:11:56 +0800

    update share track log fileld

--- data/sql/074_share_track_log.sql
+++ data/sql/074_share_track_log.sql
@@ -1,7 +1,7 @@
 CREATE TABLE `cake_rebate_track_logs` (
   `id` INT NOT NULL AUTO_INCREMENT,
-  `from` INT NOT NULL,
-  `to` INT NOT NULL,
+  `sharer` INT NOT NULL,
+  `clicker` INT NOT NULL,
   `is_paid` TINYINT NOT NULL DEFAULT 0,
   `order_id` INT NOT NULL,
   `created` DATETIME NOT NULL,
