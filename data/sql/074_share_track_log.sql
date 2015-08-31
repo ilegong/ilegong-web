c0f593bbe614a7a3272d967b1196316cfe69c36f
shichaopeng <shichaopeng@ilegong.com>
2015-08-31 17:04:25 +0800

    update rebate track log

--- data/sql/074_share_track_log.sql
+++ data/sql/074_share_track_log.sql
@@ -1,8 +1,11 @@
 CREATE TABLE `cake_rebate_track_logs` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `sharer` INT NOT NULL,
-  `clicker` INT NOT NULL,
-  `is_paid` TINYINT NOT NULL DEFAULT 0,
-  `order_id` INT NOT NULL,
-  `created` DATETIME NOT NULL,
-  PRIMARY KEY (`id`));
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `sharer` int(11) NOT NULL,
+  `clicker` int(11) NOT NULL,
+  `is_paid` tinyint(4) NOT NULL DEFAULT '0',
+  `is_rebate` tinyint(4) NOT NULL DEFAULT '0',
+  `order_id` int(11) NOT NULL DEFAULT '0',
+  `created` datetime NOT NULL,
+  `updated` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
