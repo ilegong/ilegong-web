20e7c82ea7e41c20d509f62b45a1c1cb38e4759f
shichaopeng <shichaopeng@ilegong.com>
2015-09-12 16:44:52 +0800

    fix vip bug

--- webroot/static/opt/js/opt.js
+++ webroot/static/opt/js/opt.js
@@ -252,7 +252,7 @@ $(document).ready(function () {
   var optLogTemplate = '<div class="postinfo" style="border-bottom-width: 1px; border-bottom-style: solid; border-bottom-color: rgb(223, 223, 221);" data-show="0" data-infoid="<%this.id%>" data-timestamp="<%this.timestamp%>" data-myzan="0" id="info_<%this.id%>">' +
     '<a class="heada" <%if(this.user_info){%>href="/weshares/user_share_info/<%this.user_info.id%>"<%}else{%>href="javascript:void(0)"<%}%>>' +
     '<img class="headimg" src="/static/opt/images/default.png" <%if(this.user_info){%>data-original="<%this.user_info.image%>"<%}else{%>data-original="/static/opt/images/default.png"<%}%>>' +
-    '<%if(this.user_info&&this.user_info.fans_count > 0){%><img src="/static/weshares/images/v.png" class="user-is-vip-user-tag"><%}%>'+
+    '<%if(this.user_info&&this.user_info.fans_count > 100){%><img src="/static/weshares/images/v.png" class="user-is-vip-user-tag"><%}%>'+
     '<%if(this.user_info&&this.user_info.is_proxy==1){%><span class="user-is-proxy-tag">团长</span><%}%>'+
     '</a>' +
     '<a href="javascript:void(0)" class="nickname"><%if(this.user_info){%><%this.user_info.nickname%><%}else{%>匿名用户<%}%></a>' +
