a9dbd6f34a0502a85292c7920c820b07884ae7cf
shichaopeng <shichaopeng@ilegong.com>
2015-09-21 16:31:15 +0800

    add user bind

--- app/Model/ShareUserBind.php
+++ app/Model/ShareUserBind.php
@@ -12,12 +12,14 @@ class ShareUserBind extends AppModel {
      */
     var $shareBindData = array(
         446 => array(544307, 633345),
-        542 => array(544307, 633345)
+        542 => array(544307, 633345),
+        22 => array(633345),
+        484 => array(633345, 141)
     );
 
     public function checkUserCanManageShare($share_id, $user_id) {
         $shareBindInfo = $this->shareBindData[$share_id];
-        if(empty($shareBindInfo)){
+        if (empty($shareBindInfo)) {
             return false;
         }
         return in_array($user_id, $shareBindInfo);
