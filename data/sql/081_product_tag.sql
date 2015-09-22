e3bf402d3f85499c18e2951cb9209697d6ae328a
shichaopeng <shichaopeng@ilegong.com>
2015-09-22 20:26:39 +0800

    add patch sql

--- data/sql/081_product_tag.sql
+++ data/sql/081_product_tag.sql
@@ -4,3 +4,7 @@ ADD COLUMN `tag_id` INT NOT NULL DEFAULT 0 AFTER `tbd`;
 ALTER TABLE `cake_carts`
 ADD COLUMN `tag_id` INT NOT NULL DEFAULT 0 AFTER `confirm_price`;
 
+ALTER TABLE `cake_weshare_products`
+ADD COLUMN `deleted` TINYINT NOT NULL DEFAULT 0 AFTER `tag_id`;
+
+
