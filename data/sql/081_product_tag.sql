156db6307f775ea8665dc2e99c40c23288e95952
shichaopeng <shichaopeng@ilegong.com>
2015-09-23 17:57:54 +0800

    add refresh css js

--- data/sql/081_product_tag.sql
+++ data/sql/081_product_tag.sql
@@ -8,3 +8,14 @@ ALTER TABLE `cake_weshare_products`
 ADD COLUMN `deleted` TINYINT NOT NULL DEFAULT 0 AFTER `tag_id`;
 
 
+CREATE TABLE `cake_weshare_product_tags` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(128) NOT NULL,
+  `created` datetime NOT NULL,
+  `deleted` tinyint(4) NOT NULL DEFAULT '0',
+  `user_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8;
+
+
+
