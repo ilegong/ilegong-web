fb0296b0feba41eac788848a8b37c340bb6f5044
shichaopeng <shichaopeng@ilegong.com>
2015-10-09 16:28:43 +0800

    add patch sql

--- data/sql/082_share_group_buy.sql
+++ data/sql/082_share_group_buy.sql
@@ -14,3 +14,16 @@ ADD COLUMN `relate_type` INT(2) NOT NULL DEFAULT 0 AFTER `process_prepaid_status
 ALTER TABLE `cake_rebate_track_logs`
 ADD COLUMN `type` INT NOT NULL DEFAULT 0 AFTER `updated`;
 
+CREATE TABLE `cake_weshare_offline_addresses` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `creator` int(11) NOT NULL,
+  `share_id` int(11) NOT NULL,
+  `refer_share_id` int(11) NOT NULL DEFAULT '0',
+  `address` varchar(128) NOT NULL,
+  `created` datetime NOT NULL,
+  `remarks` varchar(256) DEFAULT NULL,
+  `deleted` tinyint(4) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+
+
