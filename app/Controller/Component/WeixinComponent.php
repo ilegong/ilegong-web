9c1ad31699b5a7bdf244abe1202a33e84ba95718
shichaopeng <shichaopeng@ilegong.com>
2015-10-10 18:08:44 +0800

    fix clear cache issue

--- app/Controller/Component/WeixinComponent.php
+++ app/Controller/Component/WeixinComponent.php
@@ -479,6 +479,8 @@ class WeixinComponent extends Component {
         Cache::write(SHARE_ORDER_DATA_CACHE_KEY . '_' . $share_id . '_0_1', '');
         Cache::write(SHARE_ORDER_DATA_CACHE_KEY . '_' . $share_id . '_1_0', '');
         Cache::write(SHARE_ORDER_DATA_CACHE_KEY . '_' . $share_id . '_0_0', '');
+        Cache::write(SHARE_OFFLINE_ADDRESS_SUMMERY_DATA_CACHE_KEY . '_' . $share_id, '');
+        Cache::write(SHARE_OFFLINE_ADDRESS_BUY_DATA_CACHE_KEY . '_' . $share_id, '');
         //check should clear child share cache
         if ($is_pin_tuan) {
             $refer_share_id = $this->ShareUtil->get_share_refer_id($share_id);
