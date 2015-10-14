21bc5f4b58079494bdf93526e539b7b4bd8ca12d
shichaopeng <shichaopeng@ilegong.com>
2015-10-14 17:42:07 +0800

    fix share for pay ship fee issue

--- app/Controller/ShareFaqController.php
+++ app/Controller/ShareFaqController.php
@@ -4,7 +4,9 @@ class ShareFaqController extends AppController {
 
     var $name = 'share_faq';
 
-    var $uses = array('ShareFaq', 'Weshares', 'User');
+    var $uses = array('ShareFaq', 'Weshare', 'User');
+
+    var $components = array('Weixin', 'WeshareBuy', 'ShareUtil');
 
 
     public function beforeFilter() {
@@ -12,16 +14,33 @@ class ShareFaqController extends AppController {
         $this->layout = 'weshare_bootstrap';
     }
 
-    public function faq_list() {
+    public function faq_list($shareId) {
 
     }
 
-    public function faq() {
+    public function faq($shareId, $userId) {
+        $share_info = $this->get_share_info($shareId);
+        $share_creator = $share_info['Weshare']['creator'];
+        $user_infos = $this->User->find('all', array(
+            'conditions' => array(
+                'id' => array($userId, $share_creator)
+            ),
+            'fields' => array('id', 'nickname', 'image')
+        ));
 
     }
 
     public function create_faq() {
 
+    }
+
+    private function get_share_info($share_id) {
+        $share_info = $this->Weshare->find('first', array(
+            'conditions' => array(
+                'id' => $share_id
+            )
+        ));
+        return $share_info;
     }
 
 }
