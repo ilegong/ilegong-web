3630dc4b4eb93f9d6aa089bff3631adbc4ee8329
shichaopeng <shichaopeng@ilegong.com>
2015-10-27 18:11:17 +0800

    app js use consts var

--- webroot/static/weshares/js/app.js
+++ webroot/static/weshares/js/app.js
@@ -1,66 +1,68 @@
 (function (window, angular) {
-	angular.module('module.filters', []);
+  angular.module('module.filters', []);
 })(window, window.angular);
 
 (function (window, angular) {
-	angular.module('module.directives', []);
+  angular.module('module.directives', []);
 })(window, window.angular);
 
 (function (window, angular) {
-	angular.module('module.services', [])
-		.service('Utils', Utils);
+  angular.module('module.services', [])
+    .service('Utils', Utils);
 
-	function Utils(){
-		return {
-			isBlank: isBlank,
-			isMobileValid: isMobileValid,
-			isNumber: isNumber,
-			toPercent: toPercent
-		};
-		function isMobileValid(mobile){
-			return /^1\d{10}$/.test(mobile);
-		}
+  function Utils() {
+    return {
+      isBlank: isBlank,
+      isMobileValid: isMobileValid,
+      isNumber: isNumber,
+      toPercent: toPercent
+    };
+    function isMobileValid(mobile) {
+      return /^1\d{10}$/.test(mobile);
+    }
 
-		function isBlank(str){
-			return (!str || /^\s*$/.test(str));
-		}
-		function isNumber(n){
+    function isBlank(str) {
+      return (!str || /^\s*$/.test(str));
+    }
+
+    function isNumber(n) {
       return !isNaN(n);
-		}
-		function toPercent(value){
-			return Math.min(Math.round(value * 10000) / 100, 100);
-		}
-	}
+    }
+
+    function toPercent(value) {
+      return Math.min(Math.round(value * 10000) / 100, 100);
+    }
+  }
 })(window, window.angular);
 
 (function (window, angular) {
-	var app = angular.module('weshares', ['ui.router', 'module.services', 'module.filters', 'module.directives', 'me-lazyload'])
-		.constant('_', window._)
-		.config(configCompileProvider)
-		.config(configHttpProvider)
-		.config(extendLog)
-    .config(['$sceDelegateProvider', function($sceDelegateProvider){
+  var app = angular.module('weshares', ['ui.router', 'module.services', 'module.filters', 'module.directives', 'me-lazyload'])
+    .constant('_', window._)
+    .config(configCompileProvider)
+    .config(configHttpProvider)
+    .config(extendLog)
+    .config(['$sceDelegateProvider', function ($sceDelegateProvider) {
       $sceDelegateProvider.resourceUrlWhitelist(
-        ['self', 'http://51daifan-assets.stor.sinaapp.com/**']
+        ['self', PYS.staticFilePath + '/**']
       )
     }])
-		.run(initApp);
+    .run(initApp);
 
   //define static file path
   app.constant('staticFilePath', PYS.staticFilePath);
 
-  app.filter('unsafe', function($sce) {
-    return function(val) {
+  app.filter('unsafe', function ($sce) {
+    return function (val) {
       return $sce.trustAsHtml(val);
     };
   });
 
-  app.directive('elemReady', function( $parse ) {
+  app.directive('elemReady', function ($parse) {
     return {
       restrict: 'A',
-      link: function( $scope, elem, attrs ) {
-        elem.ready(function(){
-          $scope.$apply(function(){
+      link: function ($scope, elem, attrs) {
+        elem.ready(function () {
+          $scope.$apply(function () {
             var func = $parse(attrs.elemReady);
             func($scope);
           })
@@ -71,60 +73,61 @@
 
 
   /* @ngInject */
-	function configCompileProvider($compileProvider) {
-		$compileProvider.imgSrcSanitizationWhitelist(/^\s*(https|file|blob|cdvfile|http|chrome-extension|blob:chrome-extension):|data:image\//);
-	}
+  function configCompileProvider($compileProvider) {
+    $compileProvider.imgSrcSanitizationWhitelist(/^\s*(https|file|blob|cdvfile|http|chrome-extension|blob:chrome-extension):|data:image\//);
+  }
 
-	/* @ngInject */
-	function configHttpProvider($httpProvider) {
-		$httpProvider.defaults.headers.common['Content-Type'] = 'application/json';
-		$httpProvider.defaults.headers.post['Content-Type'] = 'application/json';
-	}
+  /* @ngInject */
+  function configHttpProvider($httpProvider) {
+    $httpProvider.defaults.headers.common['Content-Type'] = 'application/json';
+    $httpProvider.defaults.headers.post['Content-Type'] = 'application/json';
+  }
 
-	/* @ngInject */
-	function extendLog($provide) {
-		$provide.decorator('$log', function ($delegate, $injector) {
-			var _log = $delegate.log;
-			var _warn = $delegate.warn;
-			var _info = $delegate.info;
-			var _debug = $delegate.debug;
-			var _error = $delegate.error;
-			var addMessage = function (message, forceLog) {
-				var $rootScope = $injector.get("$rootScope");
-				$rootScope.config = $rootScope.config || {logMode: false};
-				if ($rootScope.config.logMode || forceLog) {
-					$rootScope.messages = $rootScope.messages || [];
-					$rootScope.messages.push(message);
-				}
-				return message;
-			};
+  /* @ngInject */
+  function extendLog($provide) {
+    $provide.decorator('$log', function ($delegate, $injector) {
+      var _log = $delegate.log;
+      var _warn = $delegate.warn;
+      var _info = $delegate.info;
+      var _debug = $delegate.debug;
+      var _error = $delegate.error;
+      var addMessage = function (message, forceLog) {
+        var $rootScope = $injector.get("$rootScope");
+        $rootScope.config = $rootScope.config || {logMode: false};
+        if ($rootScope.config.logMode || forceLog) {
+          $rootScope.messages = $rootScope.messages || [];
+          $rootScope.messages.push(message);
+        }
+        return message;
+      };
 
-			$delegate.log = function (msg, forceLog) {
-				_log(addMessage(msg, forceLog || false));
-				return this;
-			};
-			$delegate.warn = function (msg, forceLog) {
-				_warn(addMessage(msg, forceLog || false));
-				return this;
-			};
-			$delegate.info = function (msg, forceLog) {
-				_info(addMessage(msg, forceLog || false));
-				return this;
-			};
-			$delegate.debug = function (msg, forceLog) {
-				_debug(addMessage(msg, forceLog || false));
-				return this;
-			};
-			$delegate.error = function (msg, forceLog) {
-				_error(addMessage(msg, forceLog || false));
-				return this;
-			};
+      $delegate.log = function (msg, forceLog) {
+        _log(addMessage(msg, forceLog || false));
+        return this;
+      };
+      $delegate.warn = function (msg, forceLog) {
+        _warn(addMessage(msg, forceLog || false));
+        return this;
+      };
+      $delegate.info = function (msg, forceLog) {
+        _info(addMessage(msg, forceLog || false));
+        return this;
+      };
+      $delegate.debug = function (msg, forceLog) {
+        _debug(addMessage(msg, forceLog || false));
+        return this;
+      };
+      $delegate.error = function (msg, forceLog) {
+        _error(addMessage(msg, forceLog || false));
+        return this;
+      };
 
-			return $delegate;
-		});
-	}
-	function initApp($rootScope) {
-		$rootScope._ = _;
-    $rootScope.loadingPage=true;
-	}
+      return $delegate;
+    });
+  }
+
+  function initApp($rootScope) {
+    $rootScope._ = _;
+    $rootScope.loadingPage = true;
+  }
 })(window, window.angular);
