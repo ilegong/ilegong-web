a7dad79ae6259bbe60f08b45b206faeafad0d905
shichaopeng <shichaopeng@ilegong.com>
2015-10-30 15:20:17 +0800

    add share order manage view

--- app/Controller/ShareManageController.php
+++ app/Controller/ShareManageController.php
@@ -4,18 +4,22 @@
  * Class ShareManageController
  * 分享者管理系统
  */
-class ShareManageController extends AppController{
+class ShareManageController extends AppController {
 
     public function beforeFilter() {
         parent::beforeFilter();
         $this->layout = 'sharer';
     }
 
-    public function index(){
+    public function index() {
 
     }
 
-    public function login(){
+    public function login() {
         $this->layout = null;
+    }
+
+    public function share_order() {
+        $share_id = $_REQUEST['share_id'];
     }
 }
