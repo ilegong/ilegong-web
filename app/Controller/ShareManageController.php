971d126e92d0f01154d2a64074c1a1aa82d0401c
shichaopeng <shichaopeng@ilegong.com>
2015-11-09 10:31:02 +0800

    add login logout for sharer manage

--- app/Controller/ShareManageController.php
+++ app/Controller/ShareManageController.php
@@ -9,13 +9,37 @@ class ShareManageController extends AppController {
 
     public $components = array('ShareUtil', 'WeshareBuy', 'ShareManage');
 
+    public $uses = array('User');
+
     public function beforeFilter() {
         parent::beforeFilter();
+        $this->Auth->authenticate = array('Mobile');
+        $this->Auth->allowedActions = array('login', 'forgot', 'captcha', 'reset');
+        $this->set('op_cate', 'me');
         $this->layout = 'sharer';
     }
 
     public function index() {
 
+    }
+
+    public function logout(){
+        $this->logoutCurrUser();
+        $this->redirect(array('action' => 'login'));
+    }
+
+    public function do_login(){
+        if ($this->Auth->login()) {
+            $this->User->id = $this->Auth->user('id');
+            $this->User->updateAll(array(
+                'last_login' => "'" . date('Y-m-d H:i:s') . "'",
+                'last_ip' => "'" . $this->request->clientIp(false) . "'"
+            ), array('id' => $this->User->id,));
+            $this->Session->setFlash('登录成功' . $this->Session->read('Auth.User.session_flash'));
+            $this->redirect(array('action' => 'index'));
+        }
+        $this->Session->setFlash('登录失败,手机号或者密码错误');
+        $this->redirect(array('action' => 'login'));
     }
 
     public function login() {
