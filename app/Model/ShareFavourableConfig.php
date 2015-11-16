7de72d5f3ce74f9328a45e1084fcd4097aa9b139
shichaopeng <shichaopeng@ilegong.com>
2015-11-16 20:34:31 +0800

    add favouable config

--- app/Model/ShareFavourableConfig.php
+++ app/Model/ShareFavourableConfig.php
@@ -13,6 +13,9 @@ class ShareFavourableConfig extends AppModel {
 //        71 => array(
 //            'discount' => 0.8
 //        )
+        1216 => array(
+            'discount' => 0.8
+        )
     );
 
     public function get_favourable_config($share_id) {
