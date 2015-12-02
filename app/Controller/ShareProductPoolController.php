97d1a2262a20b6ab7e5e16dc581c8354b4aa1ccc
shichaopeng <shichaopeng@ilegong.com>
2015-12-02 16:11:02 +0800

    init share product detail view

--- app/Controller/ShareProductPoolController.php
+++ app/Controller/ShareProductPoolController.php
@@ -9,6 +9,8 @@ class ShareProductPoolController extends AppController {
 
     var $name = 'share_product_pool';
 
+    var $components = array('ShareUtil');
+
     public function beforeFilter() {
         parent::beforeFilter();
         $this->layout = 'weshare';
@@ -20,6 +22,8 @@ class ShareProductPoolController extends AppController {
      */
     public function share_products_index() {
         $this->layout = null;
+        $uid = $this->currentUser['id'];
+        $this->set('uid', $uid);
         $share_products = $this->get_share_products();
         $this->set('share_products', $share_products);
     }
@@ -34,6 +38,21 @@ class ShareProductPoolController extends AppController {
 
     /**
      * @param $share_id
+     * 团长从产品库开启分享
+     */
+    public function clone_share($share_id) {
+        $user_id = $this->currentUser['id'];
+        if (empty($user_id)) {
+            echo json_encode(array('success' => false, 'reason' => 'not_login'));
+            return;
+        }
+        $result = $this->ShareUtil->cloneShare($share_id, $user_id);
+        echo json_encode($result);
+        return;
+    }
+
+    /**
+     * @param $share_id
      * ajax 获取产品池的详情
      */
     public function get_share_product_detail($share_id) {
@@ -43,13 +62,11 @@ class ShareProductPoolController extends AppController {
         return;
     }
 
-
     private function get_share_product_info($share_id) {
         $key = 'pool_product_info_cache_key_' . $share_id;
         $cacheData = Cache::read($key);
         if (empty($cacheData)) {
             $weshareM = ClassRegistry::init('Weshare');
-            $weshareProductM = ClassRegistry::init('WeshareProduct');
             //get share basic info
             $weshare_info = $weshareM->find('first', array(
                 'conditions' => array('id' => $share_id)
@@ -57,21 +74,15 @@ class ShareProductPoolController extends AppController {
             $weshare_info = $weshare_info['Weshare'];
             $weshare_info['description'] = str_replace(array("\r\n", "\n", "\r"), '<br />', $weshare_info['description']);
             $weshare_info['images'] = array_filter(explode('|', $weshare_info['images']));
-            //get share products
-            $weshare_products = $weshareProductM->find('all', array(
-                'conditions' => array(
-                    'weshare_id' => $share_id,
-                    'deleted' => DELETED_NO
-                )
-            ));
-            $weshare_products = Hash::extract($weshare_products, '{n}.WeshareProduct');
+            $weshare_products = $this->ShareUtil->get_product_tag_map($share_id);
+            $sharer_tags = $this->ShareUtil->get_tags($weshare_info['creator'], $weshare_info['refer_share_id']);
             $weshare_info['products'] = $weshare_products;
+            $weshare_info['tags'] = $sharer_tags;
             Cache::write($key, json_encode($weshare_info));
             return $weshare_info;
         }
         return json_decode($cacheData, true);
     }
-
 
     /**
      * @return array
--- webroot/static/weshares/js/view-product-info.js
+++ webroot/static/weshares/js/view-product-info.js
@@ -6,21 +6,77 @@
     var vm = this;
     vm.pageLoaded = pageLoaded;
     vm.initProductInfo = initProductInfo;
+    vm.initViewFieldName = initViewFieldName;
+    vm.toggleTag = toggleTag;
+    vm.cloneShare = cloneShare;
+    vm.viewImage = viewImage;
+    vm.foretaste = foretaste;
+    vm.staticFilePath = staticFilePath;
     activate();
     function activate() {
       vm.initProductInfo();
     }
+
     function initProductInfo() {
       var weshareId = angular.element(document.getElementById('shareProductView')).attr('data-weshare-id');
+      vm.shareId = weshareId;
       vm.weshare = {};
       $http({
         method: 'GET',
         url: '/share_product_pool/get_share_product_detail/' + weshareId + '.json',
         cache: $templateCache
       }).success(function (data) {
+        $log.log(data);
         vm.weshare = data;
+        vm.initViewFieldName();
       }).error(function () {
         $log.log('get share product info error');
+      });
+    }
+
+    function foretaste() {
+
+    }
+
+    function toggleTag(tag) {
+      var currentToggleState = vm.toggleState[tag];
+      currentToggleState['open'] = !currentToggleState['open'];
+      currentToggleState['statusText'] = currentToggleState['open'] ? '收起' : '展开';
+    }
+
+    function cloneShare() {
+      $http({method: 'GET', url: '/share_product_pool/clone_share/' + vm.shareId}).success(function (data) {
+        if (data['success']) {
+          var newShareId = data['shareId'];
+          window.location.href = '/weshares/view/' + newShareId;
+        } else {
+          if (data['reason']) {
+            alert('您还没有登录，请登录后进行操作。');
+            window.location.href = '/users/login'
+          } else {
+            alert('分享失败请联系管理员');
+          }
+        }
+      }).error(function () {
+        $log.log('clone share from product pool error')
+      });
+    }
+
+    function viewImage(url) {
+      wx.previewImage({
+        current: url,
+        urls: vm.weshare.images
+      });
+    }
+
+    function initViewFieldName() {
+      vm.shareStatusText = vm.weshare.status == 2 ? '无效' : '可用';
+      vm.toggleState = {0: {open: true, statusText: '收起'}};
+      _.each(vm.weshare.tags, function (value, key) {
+        vm.toggleState[key] = {
+          open: true,
+          statusText: '收起'
+        };
       });
     }
 
