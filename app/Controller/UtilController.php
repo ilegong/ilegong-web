68e71bb814991d148c7ad9602be87fa58406587a
shichaopeng <shichaopeng@ilegong.com>
2015-12-11 18:37:18 +0800

    update notify tip msg

--- app/Controller/UtilController.php
+++ app/Controller/UtilController.php
@@ -86,14 +86,14 @@ class UtilController extends AppController {
 
     public function send_tip_msg($shareId) {
         $this->autoRender = false;
-        $msg = '冰糖橙昨天已摘完，正在发往北京的途中，周四陆续安排快递，预计周日前后能送达，感谢您的耐心等待，感恩您对朋友说的支持。';
-        $uids = $this->WeshareBuy->get_has_buy_user($shareId);
-        //$uids = array();
+        $msg = '报告小主们一个好消息，等待已久的橙橙预计明日到京，到京后会火速发出，感谢您的耐心等待，您的等待一定是值得的，感谢您对朋友说的支持。';
+        //$uids = $this->WeshareBuy->get_has_buy_user($shareId);
+        $uids = array();
         $uids[] = '633345';
-        //$uids_[] = '544307';
+        $uids_[] = '544307';
         $openIds = $this->Oauthbind->findWxServiceBindsByUids($uids);
         foreach ($openIds as $openId) {
-            $this->Weixin->send_faq_notify_template_msg($openId, 'www.tongshijia.com/weshares/view/' . $shareId, '冰糖橙延迟发货提醒', $msg, '麻阳冰糖橙');
+            $this->Weixin->send_faq_notify_template_msg($openId, 'www.tongshijia.com/weshares/view/' . $shareId, '橙妾坐到了', $msg, '麻阳冰糖橙');
         }
         echo json_encode(array('success' => true));
         return;
