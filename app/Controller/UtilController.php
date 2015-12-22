b0d50d324d3f585d3d92f83a20c8f0a98ea44fdd
shichaopeng <shichaopeng@ilegong.com>
2015-12-22 11:35:21 +0800

    add kiwi tip msg

--- app/Controller/UtilController.php
+++ app/Controller/UtilController.php
@@ -84,6 +84,21 @@ class UtilController extends AppController {
         return;
     }
 
+    public function send_kiwi_tip_msg($shareId){
+        $this->autoRender = false;
+        $msg = '大家好，收到猕猴桃后请大家及时查看，软了的就可以立即食用，如果还硬的，就先放着，经常要查看一下，软了就立即食用，以免放坏。';
+        $uids = array();
+        //$uids = $this->WeshareBuy->get_has_buy_user($shareId);
+        $uids[] = 633345;
+        $uids[] = 874821;
+        $openIds = $this->Oauthbind->findWxServiceBindsByUids($uids);
+        foreach ($openIds as $openId) {
+            $this->Weixin->send_faq_notify_template_msg($openId, 'www.tongshijia.com/weshares/view/' . $shareId, '猕猴桃收到了吧！', $msg, '影子家的徐香猕猴桃');
+        }
+        echo json_encode(array('success' => true));
+        return;
+    }
+
     public function send_tip_msg($shareId) {
         $this->autoRender = false;
         $msg = '报告小主们一个好消息，等待已久的橙橙明日到京，到京后会火速发出，感谢您的耐心等待，您的等待一定是值得的，感谢您对朋友说的支持。';
