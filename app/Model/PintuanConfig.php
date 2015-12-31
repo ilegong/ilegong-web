dd0b479f24c78c83e595a25108bcce0192e6c5e1
shichaopeng <shichaopeng@ilegong.com>
2015-12-31 15:05:02 +0800

    update pintuan tip info

--- app/Model/PintuanConfig.php
+++ app/Model/PintuanConfig.php
@@ -33,7 +33,7 @@ class PintuanConfig extends AppModel {
             'banner_img' => 'http://static.tongshijia.com/static/pintuan/images/banner.jpg',
             'sharer_id' => '141',
             'sharer_avatar' => 'http://51daifan-avatar.stor.sinaapp.com/wx_head_f88cfd957b22b112058e340d508423a7.jpg',
-            'share_title' => '【限北京】越南红心火龙果 4个装 4.5斤左右',
+            'share_title' => '越南红心火龙果 4个装 4.5斤左右',
             'wx_title' => '[和你一起立省5元] 越南红心火龙果 4个49元，杨晓光邀你吃',
             'wx_desc' => '品质棒棒嗒，好吃的要分享给大家一起吃--朋友说',
             'share_label' => '来自［杨晓光］的分享',
