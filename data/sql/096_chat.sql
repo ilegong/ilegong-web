d67fa792ddb6e87170a68d8f7b0166b073e03b59
shichaopeng <shichaopeng@ilegong.com>
2016-01-14 17:59:55 +0800

    add im sql

--- data/sql/096_chat.sql
+++ data/sql/096_chat.sql
@@ -1,26 +1,38 @@
 ALTER TABLE `51daifan`.`cake_users`
 ADD COLUMN `hx_password` VARCHAR(128) NULL AFTER `is_proxy`;
 
+CREATE TABLE `cake_chat_groups` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `hx_group_id` varchar(128) NOT NULL,
+  `created` datetime NOT NULL,
+  `creator` int(11) NOT NULL,
+  `approval` tinyint(4) NOT NULL DEFAULT '0',
+  `public` tinyint(4) NOT NULL DEFAULT '0',
+  `maxusers` int(11) NOT NULL DEFAULT '300',
+  `description` varchar(256) NOT NULL,
+  `status` int(11) DEFAULT NULL,
+  `deleted` tinyint(4) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `cake_user_friends` (
-  `id` INT NOT NULL,
-  `user_id` INT NOT NULL,
-  `friend_id` INT NOT NULL,
-  `status` INT NOT NULL DEFAULT 0,
-  `deleted` TINYINT NOT NULL DEFAULT 0,
-  `created` DATETIME NOT NULL,
-  `updated` DATETIME NOT NULL,
-  PRIMARY KEY (`id`));
+  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `friend_id` int(11) NOT NULL,
+  `status` int(11) NOT NULL DEFAULT '0',
+  `deleted` tinyint(4) NOT NULL DEFAULT '0',
+  `created` datetime NOT NULL,
+  `updated` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 CREATE TABLE `cake_user_groups` (
-  `id` INT NOT NULL,
-  `hx_group_id` VARCHAR(128) NOT NULL,
-  `created` DATETIME NOT NULL,
-  `creator` INT NOT NULL,
-  `approval` TINYINT NOT NULL DEFAULT 0,
-  `public` TINYINT NOT NULL DEFAULT 0,
-  `maxusers` INT NOT NULL DEFAULT 300,
-  `description` VARCHAR(256) NOT NULL,
-  `status` INT NULL,
-  `deleted` TINYINT NOT NULL DEFAULT 0,
-  PRIMARY KEY (`id`));
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `group_id` int(11) NOT NULL,
+  `created` datetime NOT NULL,
+  `updated` datetime NOT NULL,
+  `status` int(11) NOT NULL DEFAULT '0',
+  `deleted` tinyint(4) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
