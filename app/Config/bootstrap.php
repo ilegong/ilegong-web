fbf97447bdb9709c3f193ec115c55cd0790a440a
shichaopeng <shichaopeng@ilegong.com>
2016-01-20 11:12:54 +0800

    debug aliyun create avatar

--- app/Config/bootstrap.php
+++ app/Config/bootstrap.php
@@ -1705,7 +1705,6 @@ function create_avatar_in_aliyun($url)
     curl_setopt($ch, CURLOPT_POSTFIELDS, 'url='.$url);
     $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
     $response = curl_exec($ch);
-    log($response);
     $ali_avatar = '';
     if($httpCode == 200){
         $result = json_decode($response, true);
