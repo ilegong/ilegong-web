ffe199cfe6ba0f409e2604d4c9d97e5d1290c526
shichaopeng <shichaopeng@ilegong.com>
2016-02-22 15:46:44 +0800

    change sql

--- data/sql/02/003_create_weshare_delivery_templates.sql
+++ data/sql/02/003_create_weshare_delivery_templates.sql
@@ -1,5 +1,5 @@
 CREATE TABLE `cake_weshare_delivery_templates` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `user_id` bigint(11) NOT NULL,
   `weshare_id` int(11) NOT NULL,
   `unit_type` smallint(4) NOT NULL DEFAULT '0',
@@ -10,7 +10,8 @@ CREATE TABLE `cake_weshare_delivery_templates` (
   `is_default` tinyint(4) NOT NULL DEFAULT '0',
   `created` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=20 DEFAULT CHARSET=utf8;
+
 
 
 CREATE TABLE `cake_weshare_template_regions` (
