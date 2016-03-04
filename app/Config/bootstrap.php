a75d6a5de6039be79fce9520505e6272c366c620
shichaopeng <shichaopeng@ilegong.com>
2016-03-04 10:07:25 +0800

    add test config

--- app/Config/bootstrap.php
+++ app/Config/bootstrap.php
@@ -165,9 +165,11 @@ const SUB_SHARER_REASON_TYPE_FROM_USER_CENTER = 'ShareUserInfo'; // 标示用户
 const SUB_SHARER_REASON_TYPE_FROM_SHARE_INFO = 'ShareInfo'; // 标示用户关注朋友说的原因 来向
 const SUB_SHARER_REASON_TYPE_FROM_SPREAD = 'Spread';
 //const SAE_STATIC_FILE_PATH = 'http://51daifan-assets.stor.sinaapp.com';
-if (getenv('TONGSHIJIA_ENV') == 'test' || getenv('TONGSHIJIA_ENV') == 'product') {
+if (getenv('TONGSHIJIA_ENV') == 'product') {
     //const SAE_STATIC_FILE_PATH = 'http://static.tongshijia.com';
     define('SAE_STATIC_FILE_PATH', 'http://static.tongshijia.com');
+} elseif (getenv('TONGSHIJIA_ENV') == 'test') {
+    define('SAE_STATIC_FILE_PATH', 'http://test.tongshijia.com');
 } else {
     define('SAE_STATIC_FILE_PATH', 'http://dev.tongshijia.com');
 }
