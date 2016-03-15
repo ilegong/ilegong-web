77bdbb3f1e82d1d4ad0ef0b54d1a7bd4523e119b
shichaopeng <shichaopeng@ilegong.com>
2016-03-15 15:09:49 +0800

    improve user fans view

--- app/Controller/Component/UserFansComponent.php
+++ app/Controller/Component/UserFansComponent.php
@@ -4,17 +4,17 @@ class UserFansComponent extends Component{
 
     public function get_fans($uid){
         $queryCond = ['conditions' => ['user_id' => $uid, 'deleted' => DELETED_NO], 'limit' => 100];
-        $users = $this->process_query_data($queryCond, 'follow_id');
-        return $users;
+        $data = $this->process_query_data($queryCond, 'follow_id', $uid);
+        return $data;
     }
 
     public function get_subs($uid){
         $queryCond = ['conditions' => ['follow_id' => $uid, 'deleted' => DELETED_NO], 'limit' => 100];
-        $users = $this->process_query_data($queryCond, 'user_id');
+        $users = $this->process_query_data($queryCond, 'user_id', $uid);
         return $users;
     }
 
-    private function process_query_data($queryCond, $extract_field){
+    private function process_query_data($queryCond, $extract_field, $uid){
         $userRelationM = ClassRegistry::init('UserRelation');
         $userM = ClassRegistry::init('User');
         $relations = $userRelationM->find('all', $queryCond);
@@ -27,7 +27,17 @@ class UserFansComponent extends Component{
             'order' => array('id DESC')
         ));
         $levels_data = $this->get_user_level_map($user_ids);
-        return ['users' => $users_data, 'level_map' => $levels_data];
+        $sub_user_ids = $this->get_user_subs($uid, $user_ids);
+        return ['users' => $users_data, 'level_map' => $levels_data, 'sub_user_ids' => $sub_user_ids];
+    }
+
+    private function get_user_subs($uid, $user_ids){
+        $userRelationM = ClassRegistry::init('UserRelation');
+        $relations = $userRelationM->find('all', [
+            'conditions' => ['follow_id' => $uid, 'user_id' => $user_ids]
+        ]);
+        $sub_user_ids = Hash::extract($relations, '{n}.UserRelation.user_id');
+        return $sub_user_ids == null ? [] : $sub_user_ids;
     }
 
     private function get_user_level_map($user_ids){
