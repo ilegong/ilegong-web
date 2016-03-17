2d507ae63a9bcb1f68616634cbba49148da33155
shichaopeng <shichaopeng@ilegong.com>
2016-03-17 13:56:46 +0800

    app controller remove not use code

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -65,16 +65,7 @@ class AppController extends Controller {
     	$site_info = Configure::read('Site');
     	$this->set('site', $site_info);
     	$GLOBALS['site_cate_id'] = Configure::read('Site.default_site_cate_id');
-
-        if ($this->RequestHandler->isMobile()) {
-            //TODO: cache the result
-            $this->loadModel('ProductTag');
-            $productTags = $this->ProductTag->find('all', array(
-                'conditions' => array('published' => 1),
-                'order' => 'priority desc'
-            ));
-            $this->set('productTags', $productTags);
-        }
+        
 
     	// 无Session，且有Cookie登录信息时，解析cookie生成信息。否则忽略cookie，防止每次都要消耗性能解密cookie
     	// 其余时间使用session。
