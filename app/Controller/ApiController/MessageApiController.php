f2ebb117f443033027078178d6c0390d76efd2cd
shichaopeng <shichaopeng@ilegong.com>
2016-03-25 09:57:30 +0800

    add message api

--- app/Controller/ApiController/MessageApiController.php
+++ app/Controller/ApiController/MessageApiController.php
@@ -5,6 +5,8 @@ class MessageApiController extends AppController
 
     public $components = array('OAuth.OAuth', 'Session');
 
+    public $uses = array('OptLog');
+
     public function beforeFilter()
     {
         $allow_action = [];
@@ -16,10 +18,43 @@ class MessageApiController extends AppController
     }
 
     /**
+     * 获取最近购买的信息概要
+     */
+    public function buy_resume()
+    {
+        //query from opt log
+        $uid = $this->currentUser['id'];
+        $last_buy_log = $this->OptLog->find('first', [
+            'conditions' => [
+                'obj_creator' => $uid,
+                'obj_type' => OPT_LOG_SHARE_BUY
+            ],
+            'order' => ['id DESC']
+        ]);
+        
+    }
+
+    /**
+     * 最近评论的信息概要
+     */
+    public function comment_resume(){
+        //query from opt log
+    }
+
+    /**
+     * 私信列表[用户列表] 消息首页显示
+     */
+    public function faq_msg_list()
+    {
+        //query from faq
+    }
+
+    /**
      * 获取购买列表
      */
     public function get_buy_list()
     {
+        $uid = $this->currentUser['id'];
 
     }
 
@@ -43,14 +78,6 @@ class MessageApiController extends AppController
      * 评论的详情
      */
     public function comment_detail()
-    {
-
-    }
-
-    /**
-     * 私信列表[用户列表]
-     */
-    public function faq_msg_list()
     {
 
     }
