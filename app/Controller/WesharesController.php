c9869fad0914fa5629fccf1959feec2abbc94d6f
Zhigang Song <1005411480a@gmail.com>
2016-03-31 19:01:04 +0800

    bug fixed

--- app/Controller/WesharesController.php
+++ app/Controller/WesharesController.php
@@ -1954,6 +1954,7 @@ class WesharesController extends AppController
                 $desc = $shareUser['nickname'] . '是我的朋友，很靠谱。朋友说是一个有人情味的分享社区，这里你不但可以吃到各地的特产，还能认识有趣的人。';
             }
             if (!$image) {
+                // 这里有问题吧? 为啥是dev?
                 $image = 'http://dev.tongshijia.com/img/logo_footer.jpg';
             }
             $detail_url = WX_HOST . '/weshares/user_share_info/' . $uid;
