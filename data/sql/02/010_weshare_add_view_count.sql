4daedfc0c37253a5bab3122d17733b14e30828a5
shichaopeng <shichaopeng@ilegong.com>
2016-04-08 10:52:35 +0800

    fix sql error

--- data/sql/02/010_weshare_add_view_count.sql
+++ data/sql/02/010_weshare_add_view_count.sql
@@ -1,3 +1,2 @@
 ALTER TABLE `cake_weshares`
-ADD COLUMN `cake_wesharescol` VARCHAR(45) NULL AFTER `offline_address_id`,
-ADD COLUMN `view_count` INT NULL DEFAULT 0 AFTER `cake_wesharescol`;
+ADD COLUMN `view_count` INT NULL DEFAULT 0;
