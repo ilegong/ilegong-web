705447c1abb4cafde952a8c99a07738eab97186f
Zhigang Song <1005411480a@gmail.com>
2016-04-12 10:16:25 +0800

    我本地开发用小宝妈的帐号, 临时让她成为超管.

--- app/Config/bootstrap.php
+++ app/Config/bootstrap.php
@@ -2513,7 +2513,19 @@ function is_blacklist_user($uid) {
  * 超级管理员
  */
 function is_super_share_manager($uid) {
-    $super_manager = array(633345, 701166, 544307, 141, 802852, 801447,895096,711503, 897195, 897247,5081, 559795, 902131, 884183);
+    $super_manager = [
+        902185, 633345, 701166, 544307,
+        141, 802852, 801447, 895096,
+        711503, 897195, 897247, 5081,
+        559795, 902131, 884183
+    ];
+
+    // just for development perpose.
+    if ($_SERVER['HTTP_HOST'] === 'dev.tongshijia.com') {
+        // 811917 == 小宝妈
+        $super_manager[] = 811917;
+    }
+
     return in_array($uid, $super_manager);
 }
 
