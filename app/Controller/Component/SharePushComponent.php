58931ebfe771bb0d1ee029bc0ecf596d82f6f791
shichaopeng <shichaopeng@ilegong.com>
2016-04-12 12:13:41 +0800

    reconstruct

--- app/Controller/Component/SharePushComponent.php
+++ app/Controller/Component/SharePushComponent.php
@@ -35,14 +35,11 @@ class SharePushComponent extends Component
 
     public function push_faq_msg($faqData)
     {
+        $this->log('push faq');
         $msg = $faqData['msg'];
-        $share_id = $faqData['share_id'];
-        $share = $this->WeshareBuy->get_weshare_info($share_id);
-        $title = $share['title'];
-        $thumbnail = explode('|', $share['images']);
-        $thumbnail = $thumbnail[0];
-        $users = $this->get_users([$faqData['sender'], $faqData['receiver']]);
-        $this->JPush->push($faqData['receiver'], $title, $msg, self::$MSG_FAQ_TYPE, ['share_thumb' => $thumbnail, 'users' => json_encode($users)]);
+        $users = $this->get_users([$faqData['receiver'], $faqData['sender']]);
+        $title = $users[$faqData['sender']]['nickname'] . ' : ' . $msg;
+        $this->JPush->push(strval($faqData['receiver']), $title, $msg, self::$MSG_FAQ_TYPE, ['$msg' => json_encode($msg), 'users' => json_encode($users)]);
     }
 
 
