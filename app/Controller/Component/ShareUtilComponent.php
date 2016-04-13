517439ef88e436a88abe1a6f5912922628b12bd9
aqingsao <vinci.zhang@gmail.com>
2016-04-13 12:20:12 +0800

    add user 635627 to proxy list

--- app/Controller/Component/ShareUtilComponent.php
+++ app/Controller/Component/ShareUtilComponent.php
@@ -260,7 +260,7 @@ class ShareUtilComponent extends Component
     public function is_proxy_user($uid)
     {
         $userLevel = $this->get_user_level($uid);
-        return $userLevel['data_value'] >= PROXY_USER_LEVEL_VALUE || in_array($uid, array(5081));
+        return $userLevel['data_value'] >= PROXY_USER_LEVEL_VALUE || in_array($uid, array(635627));
     }
 
     /**
