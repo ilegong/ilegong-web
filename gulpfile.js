4703efdd1d48ee34a89fc923ab523939cdeb34d3
Zhigang Song <1005411480a@gmail.com>
2016-04-19 15:22:07 +0800

    gulp a bit bitter

--- gulpfile.js
+++ gulpfile.js
@@ -3,219 +3,159 @@ var uglify = require('gulp-uglify');
 var concat = require('gulp-concat');
 var cleancss = require('gulp-cleancss');
 
-var all_js_task = [
-  'weshare.min.js',
-  'avatar.min.js',
-  'opt.min.js',
-  'share-order-list.min.js',
-  'user-share-info.min.js',
-  'faq.min.js',
-  'user-info.min.js'
-];
+var all_js = [{
+  name: 'weshare.min.js',
+  sources: [
+    'webroot/static/weshares/js/me-lazyload.js',
+    'webroot/static/weshares/js/app.js',
+    'webroot/static/weshares/js/offline-store.js',
+    'webroot/static/weshares/js/edit-controller.js',
+    'webroot/static/weshares/js/view-consignee-controller.js',
+    'webroot/static/weshares/js/edit-consignee-controller.js',
+    'webroot/static/weshares/js/view-controller.js',
+    'webroot/static/weshares/js/pool-product-factory.js',
+    'webroot/static/weshares/js/view-product-info.js',
+  ],
+  dist: 'webroot/static/weshares/js/'
+}, {
+  name: 'avatar.min.js',
+  sources: [
+    'webroot/static/user/layer.m.js',
+    'webroot/static/user/jquery.crop.js',
+  ],
+  dist: 'webroot/static/user/'
+}, {
+  name: 'opt.min.js',
+  sources: [
+    'webroot/static/opt/js/lazyload.min.js',
+    'webroot/static/opt/js/opt.js'
+  ],
+  dist: 'webroot/static/opt/js/'
+}, {
+  name: 'share-order-list.min.js',
+  sources: [
+    'webroot/static/weshares/js/share-order-list.js',
+  ],
+  dist: 'webroot/static/weshares/js/'
+}, {
+  name: 'user-share-info.min.js',
+  sources: [
+    'webroot/static/opt/js/lazyload.min.js',
+    'webroot/static/weshares/js/user-share-info.js'
+  ],
+  dist: 'webroot/static/weshares/js/'
+}, {
+  name: 'faq.min.js',
+  sources: [
+    'webroot/static/share_faq/custom/word-limit.js',
+    'webroot/static/share_faq/custom/faq.js'
+  ],
+  dist: 'webroot/static/share_faq/custom/'
+}, {
+  name: 'user-info.min.js',
+  sources: [
+    'webroot/static/weshares/js/me-lazyload.js',
+    'webroot/static/weshares/js/user-info-app.js',
+    'webroot/static/weshares/js/user-list-data.js'
+  ],
+  dist: 'webroot/static/weshares/js/'
+}];
 
-var all_css_task = [
-  'weshare.min.css',
-  'index.min.css',
-  'opt.min.css',
-  'product_pool.min.css',
-  'share-info.min.css',
-  'user-info.min.css'
-];
-
-var src_weshare_min_js = [
-  'webroot/static/weshares/js/me-lazyload.js',
-  'webroot/static/weshares/js/app.js',
-  'webroot/static/weshares/js/offline-store.js',
-  'webroot/static/weshares/js/edit-controller.js',
-  'webroot/static/weshares/js/view-consignee-controller.js',
-  'webroot/static/weshares/js/edit-consignee-controller.js',
-  'webroot/static/weshares/js/view-controller.js',
-  'webroot/static/weshares/js/pool-product-factory.js',
-  'webroot/static/weshares/js/view-product-info.js',
-];
-
-var src_avatar_min_js = [
-  'webroot/static/user/layer.m.js',
-  'webroot/static/user/jquery.crop.js',
-];
-
-var src_opt_min_js = [
-  'webroot/static/opt/js/lazyload.min.js',
-  'webroot/static/opt/js/opt.js'
-];
-
-var src_share_order_list_min_js = [
-  'webroot/static/weshares/js/share-order-list.js',
-];
-
-var src_user_share_info_min_js = [
-  'webroot/static/opt/js/lazyload.min.js',
-  'webroot/static/weshares/js/user-share-info.js'
-];
-
-var src_faq_min_js = [
-  'webroot/static/share_faq/custom/word-limit.js',
-  'webroot/static/share_faq/custom/faq.js'
-];
-
-var src_user_info_min_js = [
-  'webroot/static/weshares/js/me-lazyload.js',
-  'webroot/static/weshares/js/user-info-app.js',
-  'webroot/static/weshares/js/user-list-data.js'
-];
-
-var src_weshare_min_css = [
-  'webroot/static/weshares/css/main.css',
-  'webroot/static/weshares/css/site-common.css',
-  'webroot/static/weshares/css/share-balance-view.css',
-  'webroot/static/weshares/css/share.css',
-];
-
-var src_index_min_css = [
-  'webroot/static/weshares/css/common.css',
-  'webroot/static/weshares/css/index-view.css',
-  'webroot/static/weshares/css/site-common.css',
-  'webroot/static/weshares/css/tab.css',
-];
-
-var src_opt_min_css = [
-  'webroot/static/opt/css/postinfo.css',
-  'webroot/static/opt/css/opt.css',
-];
-
-var src_product_pool_min_css = [
-  'webroot/static/weshares/css/site-common.css',
-  'webroot/static/product_pool/css/product_pool.css',
-];
-
-var src_share_info_min_css = [
-  'webroot/static/weshares/css/share-info.css',
-];
-
-var src_user_info_min_css = [
-  'webroot/static/weshares/css/user-info.css',
-];
-
-var all_task = all_js_task.concat(all_css_task);
-var all_js_src = src_weshare_min_js
-.concat(src_avatar_min_js)
-.concat(src_opt_min_js)
-.concat(src_share_order_list_min_js)
-.concat(src_user_share_info_min_js)
-.concat(src_faq_min_js)
-.concat(src_user_info_min_js);
-
-var all_css_src = src_weshare_min_css
-.concat(src_index_min_css)
-.concat(src_opt_min_css)
-.concat(src_product_pool_min_css)
-.concat(src_share_info_min_css)
-.concat(src_user_info_min_css);
-
-
-var all_src = all_js_src.concat(all_css_src);
+var all_css = [{
+  name: 'weshare.min.css',
+  sources: [
+    'webroot/static/weshares/css/main.css',
+    'webroot/static/weshares/css/site-common.css',
+    'webroot/static/weshares/css/share-balance-view.css',
+    'webroot/static/weshares/css/share.css',
+  ],
+  dist: 'webroot/static/weshares/css/'
+}, {
+  name: 'index.min.css',
+  sources: [
+    'webroot/static/weshares/css/common.css',
+    'webroot/static/weshares/css/index-view.css',
+    'webroot/static/weshares/css/site-common.css',
+    'webroot/static/weshares/css/tab.css',
+  ],
+  dist: 'webroot/static/weshares/css/'
+}, {
+  name: 'opt.min.css',
+  sources: [
+    'webroot/static/opt/css/postinfo.css',
+    'webroot/static/opt/css/opt.css',
+  ],
+  dist: 'webroot/static/opt/css/'
+}, {
+  name: 'product_pool.min.css',
+  sources: [
+    'webroot/static/weshares/css/site-common.css',
+    'webroot/static/product_pool/css/product_pool.css',
+  ],
+  dist: 'webroot/static/product_pool/css/'
+}, {
+  name: 'share-info.min.css',
+  sources: [
+    'webroot/static/weshares/css/share-info.css',
+  ],
+  dist: 'webroot/static/weshares/css/'
+}, {
+  name: 'user-info.min.css',
+  sources: [
+    'webroot/static/weshares/css/user-info.css',
+  ],
+  dist: 'webroot/static/weshares/css/'
+}];
 
 gulp.task('default', ['js_task', 'css_task'], function() {
 });
 
-gulp.task('dev', ['js_task', 'css_task'], function (){
-  // 查看是属于那个任务的依赖的改动, 然后编译相应的任务就可以了, 写在回调里面
-  gulp.watch(all_src, ['js_task', 'css_task']);
+gulp.task('dev', ['js_task', 'css_task'], function() {
+  var watchers = [];
+  for (var i = 0; i < all_js.length; i++) {
+    watchers.push({
+      watcher: gulp.watch(all_js[i].sources),
+      type: 'js',
+      'ball': all_js[i]
+    });
+  }
+
+  for (var i = 0; i < all_css.length; i++) {
+    watchers.push({
+      watcher: gulp.watch(all_css[i].sources),
+      type: 'css',
+      'ball': all_css[i]
+    });
+  }
+
+  watchers.map(function (item) {
+    item.watcher.on('change', function(event){
+      console.log("File changed: " + event.path);
+      var pipe = gulp.src(item.ball.sources).pipe(concat(item.ball.name));
+      if (item.type == 'js') {
+        pipe = pipe.pipe(uglify({mangle: false}));
+      } else {
+        pipe = pipe.pipe(cleancss({keepBreaks: false}));
+      }
+      pipe = pipe.pipe(gulp.dest(item.ball.dist));
+    });
+  });
 });
 
-gulp.task('js_task', all_js_task, function() {
+gulp.task('js_task', function() {
+  for (var i = 0; i < all_js.length; i++) {
+    gulp.src(all_js[i].sources)
+    .pipe(concat(all_js[i].name))
+    .pipe(uglify({mangle: false}))
+    .pipe(gulp.dest(all_js[i].dist));
+  }
 });
 
-gulp.task('css_task', all_css_task, function() {
-});
-
-gulp.task('weshare.min.js', function() {
-  gulp.src(src_weshare_min_js)
-  .pipe(concat('weshare.min.js'))
-  .pipe(uglify({mangle: false}))
-  .pipe(gulp.dest('webroot/static/weshares/js/'));
-});
-
-gulp.task('avatar.min.js', function() {
-  gulp.src(src_avatar_min_js)
-  .pipe(concat('avatar.min.js'))
-  .pipe(uglify({mangle: false}))
-  .pipe(gulp.dest('webroot/static/user/'));
-});
-
-gulp.task('opt.min.js', function() {
-  gulp.src(src_opt_min_js)
-  .pipe(concat('opt.min.js'))
-  .pipe(uglify({mangle: false}))
-  .pipe(gulp.dest('webroot/static/opt/js/'));
-});
-
-gulp.task('share-order-list.min.js', function() {
-  gulp.src(src_share_order_list_min_js)
-  .pipe(concat('share-order-list.min.js'))
-  .pipe(uglify({mangle: false}))
-  .pipe(gulp.dest('webroot/static/weshares/js/'));
-});
-
-gulp.task('user-share-info.min.js', function() {
-  gulp.src(src_user_share_info_min_js)
-  .pipe(concat('user-share-info.min.js'))
-  .pipe(uglify({mangle: false}))
-  .pipe(gulp.dest('webroot/static/weshares/js/'));
-});
-
-gulp.task('faq.min.js', function() {
-  gulp.src(src_faq_min_js)
-  .pipe(concat('faq.min.js'))
-  .pipe(uglify({mangle: false}))
-  .pipe(gulp.dest('webroot/static/share_faq/custom/'));
-});
-
-gulp.task('user-info.min.js', function() {
-  gulp.src(src_user_info_min_js)
-  .pipe(concat('user-info.min.js'))
-  .pipe(uglify({mangle: false}))
-  .pipe(gulp.dest('webroot/static/weshares/js/'));
-});
-
-gulp.task('weshare.min.css', function() {
-  gulp.src(src_weshare_min_css)
-  .pipe(concat('weshare.min.css'))
-  .pipe(cleancss({keepBreaks: false}))
-  .pipe(gulp.dest('webroot/static/weshares/css/'));
-});
-
-gulp.task('index.min.css', function() {
-  gulp.src(src_index_min_css)
-  .pipe(concat('index.min.css'))
-  .pipe(cleancss({keepBreaks: false}))
-  .pipe(gulp.dest('webroot/static/weshares/css/'));
-});
-
-gulp.task('opt.min.css', function() {
-  gulp.src(src_opt_min_css)
-  .pipe(concat('opt.min.css'))
-  .pipe(cleancss({keepBreaks: false}))
-  .pipe(gulp.dest('webroot/static/opt/css/'));
-});
-
-gulp.task('product_pool.min.css', function() {
-  gulp.src(src_product_pool_min_css)
-  .pipe(concat('product_pool.min.css'))
-  .pipe(cleancss({keepBreaks: false}))
-  .pipe(gulp.dest('webroot/static/product_pool/css/'));
-});
-
-gulp.task('share-info.min.css', function() {
-  gulp.src(src_share_info_min_css)
-  .pipe(concat('share-info.min.css'))
-  .pipe(cleancss({keepBreaks: false}))
-  .pipe(gulp.dest('webroot/static/weshares/css/'));
-});
-
-gulp.task('user-info.min.css', function() {
-  gulp.src(src_user_info_min_css)
-  .pipe(concat('user-info.min.css'))
-  .pipe(cleancss({keepBreaks: false}))
-  .pipe(gulp.dest('webroot/static/weshares/css/'));
+gulp.task('css_task', function() {
+  for (var i = 0; i < all_css.length; i++) {
+    gulp.src(all_css[i].sources)
+    .pipe(concat(all_css[i].name))
+    .pipe(cleancss({keepBreaks: false}))
+    .pipe(gulp.dest(all_css[i].dist));
+  }
 });
