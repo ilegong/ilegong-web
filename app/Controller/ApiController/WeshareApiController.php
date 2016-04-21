eee2630282b7d33743a3866208113da77eb39900
shichaopeng <shichaopeng@ilegong.com>
2016-04-21 17:10:31 +0800

    add share api

--- app/Controller/ApiController/WeshareApiController.php
+++ app/Controller/ApiController/WeshareApiController.php
@@ -21,10 +21,23 @@ class WeshareApiController extends BaseApiController
         echo json_encode($summery);
         exit();
     }
-
+    
+    //获取某个分享自己的订单
     public function get_current_user_order($weshare_id)
     {
+        $uid = $this->currentUser['id'];
+        $ordersDetail = $this->WeshareBuy->get_current_user_share_order_data($weshare_id, $uid);
+        echo json_encode($ordersDetail);
+        exit();
+    }
 
+    //获取某个分享分页查询订单
+    public function get_share_order_data($weshare_id, $page)
+    {
+        $uid = $this->currentUser['id'];
+        $ordersDetail = $this->WeshareBuy->get_share_detail_view_orders($weshare_id, $page, $uid, 1);
+        echo json_encode($ordersDetail);
+        exit();
     }
 
 }
