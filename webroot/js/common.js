58352fcbc4ce97047a11296e8e0376bacc9640fa
shichaopeng <shichaopeng@ilegong.com>
2016-04-29 13:59:27 +0800

    remove not use code

--- webroot/js/common.js
+++ webroot/js/common.js
@@ -1097,31 +1097,31 @@ var utils = {
   },
 
   wx_to_friend : function(appid, imgUrl, lineLink, descContent, shareTitle) {
-    WeixinJSBridge.invoke('sendAppMessage',{
-      "appid": appid,
-      "img_url": imgUrl,
-      "img_width": "640",
-      "img_height": "640",
-      "link": lineLink,
-      "desc": descContent,
-      "title": shareTitle
-    }, function(res) {
-      //_report('send_msg', res.err_msg);
-    });
+    //WeixinJSBridge.invoke('sendAppMessage',{
+    //  "appid": appid,
+    //  "img_url": imgUrl,
+    //  "img_width": "640",
+    //  "img_height": "640",
+    //  "link": lineLink,
+    //  "desc": descContent,
+    //  "title": shareTitle
+    //}, function(res) {
+    //  //_report('send_msg', res.err_msg);
+    //});
   },
 
   wx_to_timeline : function(appid, imgUrl, lineLink, descContent, shareTitle){
-    WeixinJSBridge.invoke('shareTimeline',{
-      "appid": appid,
-      "img_url": imgUrl,
-      "img_width": "640",
-      "img_height": "640",
-      "link": lineLink,
-      "desc": descContent,
-      "title": shareTitle
-    }, function(res) {
-      //_report('timeline', res.err_msg);
-    });
+    //WeixinJSBridge.invoke('shareTimeline',{
+    //  "appid": appid,
+    //  "img_url": imgUrl,
+    //  "img_width": "640",
+    //  "img_height": "640",
+    //  "link": lineLink,
+    //  "desc": descContent,
+    //  "title": shareTitle
+    //}, function(res) {
+    //  //_report('timeline', res.err_msg);
+    //});
   },
 
   is_weixin: function(){
