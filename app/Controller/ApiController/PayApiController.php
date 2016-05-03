8a763e94243700a65e108e7efe46acc619f8ea73
shichaopeng <shichaopeng@ilegong.com>
2016-05-03 15:42:24 +0800

    add save pay log

---
+++ app/Controller/ApiController/PayApiController.php
@@ -0,0 +1,29 @@
+<?php
+
+class PayApiController extends Controller
+{
+
+    public $components = array('OAuth.OAuth', 'WxPayment');
+
+    public function beforeFilter()
+    {
+        $allow_action = array('test');
+        $this->OAuth->allow($allow_action);
+        if (array_search($this->request->params['action'], $allow_action) == false) {
+            $this->currentUser = $this->OAuth->user();
+        }
+        $this->autoRender = false;
+    }
+
+    public function save_ali_pay_log($order_id)
+    {
+        $uid = $this->currentUser['id'];
+        $order = $this->findOrderAndCheckStatus($order_id, $uid);
+        $totalFee = $order['Order']['total_all_price'];
+        list($subject, $body) = $this->getProductDesc($order_id);
+        $out_trade_no = $this->out_trade_no(TRADE_ALI_TYPE, $order_id);
+        $this->savePayLog($order_id, $out_trade_no, $body, TRADE_ALI_TYPE, $totalFee * 100, '', '');
+        echo json_encode(['success' => true, 'out_trade_no' => $out_trade_no, 'subject' => $subject, 'body' => $body]);
+    }
+
+}
