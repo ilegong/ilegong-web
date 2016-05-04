e1dea4bf55c3f3a476ee27227eef49c522f0b26c
倪海宝 <nihaibao@126.com>
2016-05-04 10:23:29 +0800

    update xhprof

--- lib/Xhprof_lib/display/xhprof.php
+++ lib/Xhprof_lib/display/xhprof.php
@@ -30,7 +30,7 @@
 
 if (!isset($GLOBALS['XHPROF_LIB_ROOT'])) {
   // by default, the parent directory is XHPROF lib root
-  $GLOBALS['XHPROF_LIB_ROOT'] = realpath(dirname(__FILE__) . '/..');
+  $GLOBALS['XHPROF_LIB_ROOT'] = dirname(__FILE__) . '/../../lib/Xhprof_lib';
 }
 
 require_once $GLOBALS['XHPROF_LIB_ROOT'].'/utils/xhprof_lib.php';
