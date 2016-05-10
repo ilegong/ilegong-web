a8b30bb8beb96f7ae5cab9d87cf3667809870130
shichaopeng <shichaopeng@ilegong.com>
2016-05-10 13:36:55 +0800

    custom wx config by different host

--- webroot/index.php
+++ webroot/index.php
@@ -26,6 +26,7 @@ if ($_SERVER['HTTP_HOST'] == SH_SITE_HOST) {
     define('WX_SERVICE_ID_GOTO', 'http://mp.weixin.qq.com/s?__biz=MjM5MjY5ODAyOA==&mid=200757804&idx=1&sn=90b121983525298a4ac26ee8d6c0bc1c#rd');
 }
 define('ALI_HOST', WX_HOST);
+
 define('WX_JS_API_CALL_URL', 'http://' . WX_HOST . '/wxPay/jsApiPay');
 define('WX_JS_LOGISTICS_API_CALL_URL', 'http://' . WX_HOST . '/wxPay/logistics_order_pay');
 define('WX_NOTIFY_URL', 'http://' . WX_HOST . '/wxPay/notify.html');
