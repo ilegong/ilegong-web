b093f92880d059bd31da65efc6fa1246e0603cce
shichaopeng <shichaopeng@ilegong.com>
2016-05-17 15:37:16 +0800

    clean routes code

--- app/Config/routes.php
+++ app/Config/routes.php
@@ -1,65 +1,10 @@
 <?php
 App::uses('CustomRouter', 'Lib');
 
-// When making custom routes, a common pitfall is that using named parameters will break your custom routes. In order to solve this
-	//Router::connectNamed(array('page'),array('separator'=>'|')); 	
+
 	Router::parseExtensions('html', 'rss','json','xml','css');
 
 
-//    Router::redirect('/products/19700101/heizhu_tuangou', '/techan/18');
-//
-//    CustomRouter::connect('/t/kuerlexl_ten',
-//        array('controller'=>'products','action' => 'view', '2yuan_shi_chi_xin_jiang_ku_er_le_xiang_li'));
-//
-//    CustomRouter::connect('/t/rice_product',
-//        array('controller'=>'products','action' => 'view', 'wu_chang_dao_hua_xiang_ti_qian_yu_shou_500jin_zhi_xian_bei_jing'));
-//
-//
-//    CustomRouter::connect('/t/special_nov',
-//        array('controller'=>'categories','action' => 'special_list', 'special_for_rice_coupons'));
-//
-//    //store
-//    CustomRouter::connect('/s', array('controller' => 'stores', 'action' => 'index'));
-//    CustomRouter::connect('/s/:action', array('controller' => 'stores'), array('action' => '[\w_]+') );
-//    CustomRouter::connect('/b/index', array('controller' => 'brands', 'action' => 'index'));
-//    CustomRouter::connect('/b/:slug', array('controller' => 'brands', 'action' => 'view'), array('pass' => array('slug'), 'slug' => '[\w_]+'));
-//    CustomRouter::connect('/b/:slug/:action', array('controller' => 'brands'), array('pass' => array('slug',), 'action' => '[\w_]+' , 'slug' => '[\w_]+'));
-//
-//    CustomRouter::connect('/t/ag',
-//        array('controller'=>'game_xiyang','action' => 'award', 'xiyang', '?' => array('from' => 'init-pys')));
-//    CustomRouter::connect('/t/ag/jiujiu',
-//        array('controller'=>'game_jiujiu','action' => 'award', 'jiujiu'));
-//    CustomRouter::connect('/t/ag/xiyang',
-//        array('controller'=>'game_xiyang','action' => 'award', 'xiyang'));
-//    CustomRouter::connect('/t/ag/:type',
-//        array('controller'=>'apple201410','action' => 'award'),
-//        array('pass' => array('type'), 'type'=>'[\w_]+'));
-//
-//    CustomRouter::connect('/:controller/:yearmonth/:slug',
-//	array('action' => 'view'),
-//	array('pass' => array('slug'),'controller'=>'[\w_]+','yearmonth'=>"[0-9]{8}")
-//	);
-//	CustomRouter::connect('/products/lists',
-//			array('controller'=>'products','action' => 'lists')
-//	);
-//	CustomRouter::connect('/products/add',
-//		array('controller'=>'products','action' => 'add')
-//	);
-// 	CustomRouter::connect('/products/:slug',
-// 			array('controller'=>'products','action' => 'view'),
-// 			array('pass' => array('slug'))
-// 	);
-//	CustomRouter::connect('/tag/:model/:id',
-//			array('controller'=>'keywords','action' => 'lists'),
-//			array('pass' => array('model','id'))
-//	);
-//
-//	CustomRouter::connect(
-//	'/regions/:regionid/*',
-//	array('controller' => 'regions', 'action' => 'index'),
-//	array('pass' => array('regionid'),'regionid'=>"[0-9]+")
-//	);
-	// 	CustomRouter::connect('/products', array('controller' => 'products','action'=>'index'));
 	
 	Configure::write('Install.installed',
 		file_exists(APP . 'Config' . DS . 'database.php') && file_exists(DATA_PATH . 'install.lock')
@@ -74,31 +19,13 @@ App::uses('CustomRouter', 'Lib');
 		}
 		CakePlugin::load('Install', array( 'routes' => true));	
 	}
-	else{	
-		// CakePlugin::load('Oauth', array( 'bootstrap' => true));
-		// CakePlugin::load('Museum', array( 'routes' => true)); //'bootstrap' => true,
-		// CakePlugin::load('Taobao', array( 'routes' => true));
-		// CakePlugin::load('Communicate');	
+	else{
 		$GLOBALS['site_cate_id'] = 0;
 		$GLOBALS['site_info'] = array();
-		//try{CakePlugin::load('MultiSite', array('bootstrap'=>true));}catch(Exception $e){}
 	}
 	CakePlugin::routes();
-
 	CustomRouter::connect('/', array('controller' => 'pys', 'action' => 'index','/'));
-	//CustomRouter::connect('/users/51daifan.sinaapp.com', array('controller' => 'categories', 'action' => 'view','/'));
-
-	//CustomRouter::connect('/shichituan', array('controller' => 'articles', 'action' => 'shi_chi_tuan'));
 	CustomRouter::connect('/jserror', array('controller' => 'util', 'action' => 'log_js_error'));
-	//CustomRouter::connect('/tk/log', array('controller' => 'util', 'action' => 'log_trace'));
-
-	//CustomRouter::connect('/pt/:tag', array('controller' => 'categories', 'action' => 'tag'), array('pass' => array('tag')));
-
-	/*'/:slug' 这条放在插件之后，容易对其它路由产生冲突 */
-	//CustomRouter::connect('/:slug', array('controller' => 'categories', 'action' => 'view'), array('pass' => array('slug')) );
-	//CustomRouter::connect('/:slug/:brand', array('controller' => 'categories', 'action' => 'view'), array('pass' => array('slug', 'brand'), 'slug' => 'techan',  'brand'=>"[0-9]+") );
-
-// 	require CAKE . 'Config' . DS . 'routes.php';
 
 	$prefixes = Router::prefixes();
 	
