0553d7694a6315c0eeedd6d3da8f0e7ea50ebabe
倪海宝 <nihaibao@126.com>
2016-05-19 11:29:53 +0800

    修改获取用户头像方法bug

--- app/Config/bootstrap.php
+++ app/Config/bootstrap.php
@@ -1727,6 +1727,10 @@ function get_user_avatar($user)
         $user = $user['User'];
     }
     if ($user['avatar']) {
+        if(stripos($user['avatar'], ALIYUN_AVATAR_PREFIX) === 0)
+        {
+            return $user['avatar'];
+        }
         return ALIYUN_AVATAR_PREFIX . '/' . str_replace('avatar/', 'avatar/s/', $user['avatar']);
     }
     return $user['image'];
