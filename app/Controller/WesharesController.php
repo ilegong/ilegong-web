dc927dcf528f1869cc1ef9cf47a1e61dd6504faf
shichaopeng <shichaopeng@ilegong.com>
2016-05-19 16:40:25 +0800

    debug self get orders issue

--- app/Controller/WesharesController.php
+++ app/Controller/WesharesController.php
@@ -794,7 +794,7 @@ class WesharesController extends AppController
             $result_item['share_info'] = $order_item['Weshare'];
             $result[] = $result_item;
         }
-        $this->log('user get self order data '.json_encode($result), LOG_DEBUG);
+        $this->log('user get self order uid '.$uid.' data '.json_encode($result), LOG_DEBUG);
         echo json_encode($result);
         exit();
     }
