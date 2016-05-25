0353b5b921b6924a5794959506e079a8908a5dd0
shichaopeng <shichaopeng@ilegong.com>
2016-05-25 13:50:32 +0800

    when system error add log

--- lib/Error/CustomExceptionRender.php
+++ lib/Error/CustomExceptionRender.php
@@ -19,10 +19,11 @@ class CustomExceptionRender extends ExceptionRenderer {
     public function render() {
         if ($this->template) {
             if ($this->template == 'missingAction' || $this->template == 'missingController') {
+                $this->controller->log('system error uri ' . $_SERVER['REQUEST_URI'] . ' refer ' . $_SERVER['HTTP_REFERER'] . ' error ' . $this->error->getMessage());
                 $this->controller->redirect('/');
                 //$this->error400($this->error);
             } else {
-               //call_user_func_array(array($this, $this->method), array($this->error));
+                //call_user_func_array(array($this, $this->method), array($this->error));
                 $this->error500($this->error);
             }
         }  else {
