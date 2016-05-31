2c8caad383cbeffe93903e5f9c297e5a14b3c4b5
shichaopeng <shichaopeng@ilegong.com>
2016-05-31 15:01:04 +0800

    add custom service event click

--- app/Controller/WeixinController.php
+++ app/Controller/WeixinController.php
@@ -116,7 +116,7 @@ class WeixinController extends Controller {
                 } else if ($req['Event'] == 'CLICK') {
                     $input = $req['EventKey'];
                     if($input==WX_CLICK_CUSTOM_SERVICE){
-                        echo $this->newTextMsg($user, $me, "如果出现售后问题，可以直接可团长沟通，也可以加朋友说小妹微信：pyshuo2015 \n工作时间9:00-20:00。\n紧急情况请电话联系\n".CSAD_PHONE);
+                        echo $this->newTextMsg($user, $me, "如果出现售后问题，可以直接可团长沟通，也可以加朋友说小妹微信：pyshuo2015 \n工作时间9:00-19:00。\n紧急情况请电话联系\n".CSAD_PHONE);
                     }
                     exit;
                 } else if ($req['Event'] == 'unsubscribe') {
