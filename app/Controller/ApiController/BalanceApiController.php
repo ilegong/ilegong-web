e82fc6b721f871c7121092aac06db1e105687aab
shichaopeng <shichaopeng@ilegong.com>
2016-06-30 18:43:23 +0800

    update api

--- app/Controller/ApiController/BalanceApiController.php
+++ app/Controller/ApiController/BalanceApiController.php
@@ -45,73 +45,99 @@ class BalanceApiController extends Controller
         exit;
     }
 
-    /**
-     * 待结算的list
-     */
-    public function wait_balance_list($page, $limit)
-    {
+    public function get_balance_list($type, $page, $limit){
         $uid = $this->currentUser['id'];
-        $data = $this->Balance->get_wait_balance_share_list($uid, $page, $limit);
+        if($type==0){
+            $data = $this->Balance->get_wait_balance_share_list($uid, $page, $limit);
+        }
+        if($type==1){
+            $data = $this->Balance->get_already_balance_share_list($uid, $page, $limit);
+        }
+        if($type==2){
+            $data = $this->Balance->get_already_balance_share_list($uid, $page, $limit);
+        }
         echo json_encode($data);
         exit;
     }
 
-    /**
-     * 已经结算list
-     */
-    public function already_balance_list($page, $limit)
-    {
-        $uid = $this->currentUser['id'];
-        $data = $this->Balance->get_already_balance_share_list($uid, $page, $limit);
-        echo json_encode($data);
-        exit;
-    }
+//    /**
+//     * 待结算的list
+//     */
+//    public function wait_balance_list($page, $limit)
+//    {
+//        $uid = $this->currentUser['id'];
+//        $data = $this->Balance->get_wait_balance_share_list($uid, $page, $limit);
+//        echo json_encode($data);
+//        exit;
+//    }
+//
+//    /**
+//     * 已经结算list
+//     */
+//    public function already_balance_list($page, $limit)
+//    {
+//        $uid = $this->currentUser['id'];
+//        $data = $this->Balance->get_already_balance_share_list($uid, $page, $limit);
+//        echo json_encode($data);
+//        exit;
+//    }
+//
+//    /**
+//     * 进行中的分享list
+//     */
+//    public function going_balance_list($page, $limit)
+//    {
+//        $uid = $this->currentUser['id'];
+//        $data = $this->Balance->get_going_share_list($uid, $page, $limit);
+//        echo json_encode($data);
+//        exit;
+//    }
 
-    /**
-     * 进行中的分享list
-     */
-    public function going_balance_list($page, $limit)
-    {
-        $uid = $this->currentUser['id'];
-        $data = $this->Balance->get_going_share_list($uid, $page, $limit);
-        echo json_encode($data);
-        exit;
-    }
 
-    /**
-     * 自己分享的结算详情
-     */
-    public function self_share_balance_detail($balanceId)
-    {
+    public function share_balance_detail($type, $balanceId) {
         list($orders, $balanceLog) = $this->Balance->get_balance_detail_orders($balanceId);
         list($order_list, $order_ids) = $this->get_balance_order_list($orders);
+        if ($type == 2 || $type == 3) {
+            $order_list = $this->combine_channel_price_to_order($order_list, $order_ids);
+        }
         echo json_encode(['orders' => $order_list, 'balanceLog' => $balanceLog]);
         exit;
     }
 
-    /**
-     * 产品街分享的结算详情
-     */
-    public function pool_share_balance_detail($balanceId)
-    {
-        list($orders, $balanceLog) = $this->Balance->get_balance_detail_orders($balanceId);
-        list($order_list, $order_ids) = $this->get_balance_order_list($orders);
-        $order_list = $this->combine_channel_price_to_order($order_list, $order_ids);
-        echo json_encode(['orders' => $order_list, 'balanceLog' => $balanceLog]);
-        exit;
-    }
-
-    /**
-     * 产品街商家结算详情
-     */
-    public function brand_share_balance_detail($balanceId)
-    {
-        list($orders, $balanceLog) = $this->Balance->get_balance_detail_orders($balanceId);
-        list($order_list, $order_ids) = $this->get_balance_order_list($orders);
-        $order_list = $this->combine_channel_price_to_order($order_list, $order_ids);
-        echo json_encode(['orders' => $order_list, 'balanceLog' => $balanceLog]);
-        exit;
-    }
+//    /**
+//     * 自己分享的结算详情
+//     */
+//    public function self_share_balance_detail($balanceId)
+//    {
+//        list($orders, $balanceLog) = $this->Balance->get_balance_detail_orders($balanceId);
+//        list($order_list, $order_ids) = $this->get_balance_order_list($orders);
+//        echo json_encode(['orders' => $order_list, 'balanceLog' => $balanceLog]);
+//        exit;
+//    }
+//
+//    /**
+//     * 产品街分享的结算详情
+//     */
+//    public function pool_share_balance_detail($balanceId)
+//    {
+//        list($orders, $balanceLog) = $this->Balance->get_balance_detail_orders($balanceId);
+//        list($order_list, $order_ids) = $this->get_balance_order_list($orders);
+//        $order_list = $this->combine_channel_price_to_order($order_list, $order_ids);
+//        echo json_encode(['orders' => $order_list, 'balanceLog' => $balanceLog]);
+//        exit;
+//    }
+//
+//    /**
+//     * 产品街商家结算详情
+//     */
+//    public function brand_share_balance_detail($balanceId)
+//    {
+//        list($orders, $balanceLog) = $this->Balance->get_balance_detail_orders($balanceId);
+//        list($order_list, $order_ids) = $this->get_balance_order_list($orders);
+//        $order_list = $this->combine_channel_price_to_order($order_list, $order_ids);
+//        echo json_encode(['orders' => $order_list, 'balanceLog' => $balanceLog]);
+//        exit;
+//    }
 
     private function combine_channel_price_to_order($order_list, $order_ids){
         $order_channel_prices = $this->Balance->get_orders_channel_price($order_ids);
