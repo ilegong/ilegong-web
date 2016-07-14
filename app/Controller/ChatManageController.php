7b8931bd486916b366d425ed8911937b80778024
shichaopeng <shichaopeng@ilegong.com>
2016-07-14 14:32:26 +0800

    add chat group form

--- app/Controller/ChatManageController.php
+++ app/Controller/ChatManageController.php
@@ -1,7 +1,9 @@
 <?php
 
 
-class ChatManageController extends Controller{
+class ChatManageController extends AppController
+{
+    public $components = ['Auth'];
 
     public function beforeFilter()
     {
@@ -10,21 +12,40 @@ class ChatManageController extends Controller{
     }
 
 
-    public function group_list(){
+    public function group_list()
+    {
         $this->loadModel('ChatGroup');
-        $groups = $this->ChatGroup->find('all', []);
+        $groups = $this->ChatGroup->find('all', [
+            'joins' => [
+                [
+                    'table' => 'cake_users',
+                    'type' => 'left',
+                    'alias' => 'User',
+                    'conditions' => 'User.id = ChatGroup.creator'
+                ],
+            ],
+            'fields' => ['ChatGroup.*', 'User.nickname']
+        ]);
         $this->set('groups', $groups);
     }
 
-    public function group_form(){
+    public function group_form()
+    {
+        $id = $_REQUEST['id'];
+        if($id){
+            $this->loadModel('ChatGroup');
+            $group = $this->ChatGroup->findById($id);
+            $this->set('group', $group);
+        }
+    }
+
+    public function save_group()
+    {
 
     }
 
-    public function save_group(){
-
-    }
-
-    public function send_msg(){
+    public function send_msg()
+    {
 
     }
 
