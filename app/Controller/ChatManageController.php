804b9fc77329bfa8a49fbdef2652b4375c485a3e
shichaopeng <shichaopeng@ilegong.com>
2016-07-14 15:03:38 +0800

    save and update chat group

--- app/Controller/ChatManageController.php
+++ app/Controller/ChatManageController.php
@@ -32,16 +32,51 @@ class ChatManageController extends AppController
     public function group_form()
     {
         $id = $_REQUEST['id'];
-        if($id){
+        $title = '创建';
+        if ($id) {
             $this->loadModel('ChatGroup');
             $group = $this->ChatGroup->findById($id);
             $this->set('group', $group);
+            $title = '更新';
         }
+        $this->set('title', $title);
     }
 
     public function save_group()
     {
-
+        $this->Components->load('ChatUtil');
+        $this->loadModel('ChatGroup');
+        $data = $this->request->data;
+        if ($data['ChatGroup']['id']) {
+            //update
+            $hxGroupData = [
+                "groupname" => $data['ChatGroup']['group_name'],
+                "desc" => str_replace(" ", "+", $data['ChatGroup']['description']),
+                "maxusers" => $data['ChatGroup']['maxusers']
+            ];
+            $result = $this->ChatUtil->update_group($hxGroupData, $data['ChatGroup']['hx_group_id']);
+            if ($result) {
+                $this->ChatGroup->save($data);
+            }
+        } else {
+            //save
+            $hxGroupData = [
+                "groupname" => $data['ChatGroup']['group_name'],
+                "desc" => $data['ChatGroup']['description'],
+                "public" => true,
+                "maxusers" => $data['ChatGroup']['maxusers'],
+                "approval" => boolval($data['CatGroup']['approval']),
+                "owner" => $data['ChatGroup']['creator'],
+                "members" => []
+            ];
+            $result = $this->ChatUtil->create_group($hxGroupData);
+            if ($result) {
+                $data['ChatGroup']['hx_group_id'] = $result;
+                $data['ChatGroup']['is_public'] = 1;
+                $this->ChatGroup->save($data);
+            }
+        }
+        $this->redirect('/chatManage/group_list.html');
     }
 
     public function send_msg()
