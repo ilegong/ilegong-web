35d146f0cc956c0a04589abfcbeaa51c1cc744ec
shichaopeng <shichaopeng@ilegong.com>
2016-07-21 17:25:09 +0800

    fix get rebate money issue

--- app/Controller/RebateMoneyController.php
+++ app/Controller/RebateMoneyController.php
@@ -15,11 +15,55 @@ class RebateMoneyController extends AppController{
     }
 
     public function rules(){
-        $this->set('title', '余额常见问题');
     }
 
     public function detail(){
+        $this->loadModel('User');
+        $uid = $this->currentUser['id'];
+        $totalRebateMoney = $this->User->get_rebate_money($uid);
+        $this->set('totalRebateMoney', get_format_number($totalRebateMoney/100));
+    }
 
+    public function rebate_list($page){
+        $limit = 10;
+        $uid = $this->currentUser['id'];
+        $logs = $this->RebateLog->find('all', [
+            'conditions' => [
+                'RebateLog.user_id' => $uid
+            ],
+            'joins' => [
+                [
+                    'table' => 'cake_orders',
+                    'type' => 'left',
+                    'alias' => 'Order',
+                    'conditions' => 'Order.id = RebateLog.order_id'
+                ],
+                [
+                    'table' => 'cake_users',
+                    'type' => 'left',
+                    'alias' => 'User',
+                    'conditions' => 'User.id = Order.creator'
+                ],
+                [
+                    'table' => 'cake_weshares',
+                    'type' => 'left',
+                    'alias' => 'Weshare',
+                    'conditions' => 'Weshare.id = Order.member_id'
+                ]
+            ],
+            'fields' => ['RebateLog.reason', 'RebateLog.money', 'RebateLog.description', 'Weshare.title', 'Weshare.default_image', 'User.nickname', 'Order.created', 'Weshare.id', 'Order.id'],
+            'limit' => $limit,
+            'page' => $page,
+            'order' => 'RebateLog.id ASC'
+        ]);
+        $result = [];
+        foreach ($logs as $logItem) {
+            $item = array_merge([], $logItem['RebateLog'], $logItem['Weshare'], $logItem['User']);
+            $item['money'] = get_format_number($item['money']/100);
+            $result[] = $item;
+        }
+        echo json_encode($result);
+        exit;
     }
 
 }
