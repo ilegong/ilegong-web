40c2f2761828641b1e8b675801d845e47ecb97e3
shichaopeng <shichaopeng@ilegong.com>
2016-07-26 11:19:45 +0800

    when order paid save rebate log

--- app/Controller/Component/WeixinComponent.php
+++ app/Controller/Component/WeixinComponent.php
@@ -840,6 +840,7 @@ class WeixinComponent extends Component {
                 $this->ShareUtil->process_order_paid_rebate($cate_id, $order);
                 //$this->ShareUtil->update_rebate_log($cate_id, $order);
             }
+            $this->ShareUtil->add_use_rebate_log($order);
             //save buy opt log
             $this->ShareUtil->save_buy_opt_log($order['Order']['creator'], $order['Order']['member_id'], $order['Order']['id']);
             $this->ShareUtil->send_buy_msg_to_hx($order['Order']['member_id'], $order['Order']['chat_group_id'], $order['Order']['creator']);
