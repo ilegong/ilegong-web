c50a8fa2a745f153b282a717f30cf04f277d750b
shichaopeng <shichaopeng@ilegong.com>
2016-07-26 10:27:21 +0800

    strong recommender name

--- webroot/src/scripts/user-rebate-list-controller.js
+++ webroot/src/scripts/user-rebate-list-controller.js
@@ -17,7 +17,7 @@
 
         function getRebateDetail(data){
             if(data['reason'] == 1){
-                return '您推荐的' + data['nickname'] + '报名了[' + data['title'] + ']';
+                return '您推荐的<' + data['nickname'] + '>报名了[' + data['title'] + ']';
             }
             if(data['reason'] == 2){
                 return '您报名了[' + data['title'] + ']';
