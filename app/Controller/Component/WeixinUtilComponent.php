0b4b4fccc23e2a0ffc7169f4565131db682c17de
shichaopeng <shichaopeng@ilegong.com>
2016-08-12 11:56:33 +0800

    fix user sub tip info

--- app/Controller/Component/WeixinUtilComponent.php
+++ app/Controller/Component/WeixinUtilComponent.php
@@ -38,8 +38,8 @@ class WeixinUtilComponent extends Component {
     public function create_weixin_user($openId) {
         try {
             $oauth_wx_source = oauth_wx_source();
-            $WxOauthM = ClassRegistry::init('WxOauth');
             $OauthbindM = ClassRegistry::init('Oauthbind');
+            $WxOauthM = ClassRegistry::init('WxOauth');
             $uinfo = $WxOauthM->get_user_info_by_base_token($openId);
             $userId = createNewUserByWeixin($uinfo, $this->User);
             $oauth['Oauthbind']['oauth_openid'] = $openId;
@@ -53,6 +53,12 @@ class WeixinUtilComponent extends Component {
         }
     }
 
+    public function get_weixin_nickname_by_openid($openId){
+        $WxOauthM = ClassRegistry::init('WxOauth');
+        $uinfo = $WxOauthM->get_user_info_by_base_token($openId);
+        return $uinfo['nickname'];
+    }
+
     /**
      * @param $from
      * @param $uid
@@ -62,18 +68,13 @@ class WeixinUtilComponent extends Component {
     public function process_user_sub_weixin($from, $uid, $openId) {
         $replay_type = 1;
         $url = '';
-        $userM = ClassRegistry::init('User');
-        $sql = "SELECT nickname FROM cake_users WHERE deleted = 0 AND username = '{$openId}' LIMIT 1";
-
-        $user = $userM->query($sql);
-        $userName = $user[0]['cake_users']['nickname'];
-
         if ($from == FROM_WX_SERVICE) {
             /*$content = array(
                 array('title' => '朋友说是什么？看完你就懂了！', 'description' => '',
                     'picUrl' => 'https://mmbiz.qlogo.cn/mmbiz/qpxHrxLKdR0A6F8hWz04wVpntT9Jiao8XZn7as5FuHch5zFzFnvibjUGYU3J4ibxRyLicytfdd9qDQoqV1ODOp3Rjg/0',
                     'url' => 'http://mp.weixin.qq.com/s?__biz=MjM5MjY5ODAyOA==&mid=201694178&idx=1&sn=8dea494e02c96dc21e51931604771748#rd')
             );*/
+            $userName = $this->get_weixin_nickname_by_openid($openId);
             $content = "Hi，{$userName}\n终于等到您了，欢迎加入[ 朋友说]社区。\n[朋友说] 是一家为您提供精挑细选产品的平台，通过熟人间信任关系来为您提供优质的产品，节省您选择的时间，并通过这种方式来保证您买到产品的品质。\n".'<a href="http://www.tongshijia.com/weshares/index?from=weixin">'."进入逛逛>></a>\n如您有任何问题或建议，请加客服小妹微信咨询，微信号：pyshuo2015";
             $reason = $this->get_user_sub_reason($uid);
             if (!empty($uid) && !empty($reason)) {
@@ -130,7 +131,7 @@ class WeixinUtilComponent extends Component {
                     'picUrl' => 'https://mmbiz.qlogo.cn/mmbiz/qpxHrxLKdR0A6F8hWz04wVpntT9Jiao8XZn7as5FuHch5zFzFnvibjUGYU3J4ibxRyLicytfdd9qDQoqV1ODOp3Rjg/0',
                     'url' => 'http://mp.weixin.qq.com/s?__biz=MjM5NzQ3NTkxNA==&mid=203424483&idx=1&sn=e281fc56834fb0c2942f887d2edd8d48#rd')
             );*/
-            $content = "Hi，{$userName}\n终于等到您了，欢迎加入[ 朋友说]社区。\n[朋友说] 是一家为您提供精挑细选产品的平台，通过熟人间信任关系来为您提供优质的产品，节省您选择的时间，并通过这种方式来保证您买到产品的品质。\n".'<a href="http://www.tongshijia.com/weshares/index?from=weixin">'."进入逛逛>></a>\n如您有任何问题或建议，请加客服小妹微信咨询，微信号：pyshuo2015";
+            $content = "Hi，终于等到您了，欢迎加入[ 朋友说]社区。\n[朋友说] 是一家为您提供精挑细选产品的平台，通过熟人间信任关系来为您提供优质的产品，节省您选择的时间，并通过这种方式来保证您买到产品的品质。\n".'<a href="http://www.tongshijia.com/weshares/index?from=weixin">'."进入逛逛>></a>\n如您有任何问题或建议，请加客服小妹微信咨询，微信号：pyshuo2015";
         }
         return array('replay_type' => $replay_type, 'content' => $content, 'url' => $url);
     }
