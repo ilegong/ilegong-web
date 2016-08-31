1d2c487c78f5c43e022df1db07ea40ac39dd1f3d
shichaopeng <shichaopeng@ilegong.com>
2016-08-31 15:22:39 +0800

    fix get promotion data type

--- app/Controller/Component/ShareUtilComponent.php
+++ app/Controller/Component/ShareUtilComponent.php
@@ -2335,12 +2335,12 @@ class ShareUtilComponent extends Component
             [
                 'banner_img' => 'http://static.tongshijia.com/static/img/index/dzx.png',
                 'data' => '6951',
-                'type' => 1
+                'type' => "1"
             ],
             [
                 'banner_img' => 'http://static.tongshijia.com/static/img/index/yuebing.png',
                 'data' => 'http://www.tongshijia.com/articles/moon_cake.html',
-                'type' => 0
+                'type' => "0"
             ]
         ];
         return $promotions;
