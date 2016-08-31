e0389d6f84f3bd6aab949c78aadec307147100cf
shichaopeng <shichaopeng@ilegong.com>
2016-08-31 13:50:54 +0800

    add product manager

--- app/Config/bootstrap.php
+++ app/Config/bootstrap.php
@@ -2623,7 +2623,7 @@ function is_super_share_manager($uid) {
 
 function is_product_manager($uid)
 {
-    $m = [801447, 559795, 633345];
+    $m = [801447, 559795, 633345, 701166];
     return in_array($uid, $m);
 }
 
