2fd4c79a4b29d01c14fcd954afd084e7151f0224
shichaopeng <shichaopeng@ilegong.com>
2016-08-31 16:04:47 +0800

    add page click log

--- webroot/src/scripts/app.js
+++ webroot/src/scripts/app.js
@@ -1,58 +1,62 @@
 (function (window, angular) {
-  var app = angular.module('weshares', ['infinite-scroll','angular-carousel', 'module.services', 'module.filters', 'module.directives'])
-    .constant('_', window._)
-    .config(configCompileProvider)
-    .config(configHttpProvider)
-    .config(extendLog)
-    .config(['$sceDelegateProvider', function ($sceDelegateProvider) {
-      $sceDelegateProvider.resourceUrlWhitelist(
-        ['self', 'http://*.tongshijia.com/**']
-      )
-    }])
-    .run(initApp)
-    .controller('DefaultCtrl', DefaultCtrl);
+    var app = angular.module('weshares', ['infinite-scroll', 'angular-carousel', 'module.services', 'module.filters', 'module.directives'])
+        .constant('_', window._)
+        .config(configCompileProvider)
+        .config(configHttpProvider)
+        .config(extendLog)
+        .config(['$sceDelegateProvider', function ($sceDelegateProvider) {
+            $sceDelegateProvider.resourceUrlWhitelist(
+                ['self', 'http://*.tongshijia.com/**']
+            )
+        }])
+        .run(initApp)
+        .controller('DefaultCtrl', DefaultCtrl);
 
-  /* @ngInject */
-  function configCompileProvider($compileProvider) {
-    $compileProvider.imgSrcSanitizationWhitelist(/^\s*(https|file|blob|cdvfile|http|chrome-extension|blob:chrome-extension):|data:image\//);
-  }
-
-  /* @ngInject */
-  function configHttpProvider($httpProvider) {
-    $httpProvider.defaults.headers.common['Content-Type'] = 'application/json';
-    $httpProvider.defaults.headers.post['Content-Type'] = 'application/json';
-  }
-
-  /* @ngInject */
-  function extendLog($provide) {
-    $provide.decorator('$log', function ($delegate, $injector) {
-      var _log = $delegate.log;
-      $delegate.log = function (msg, forceLog) {
-        _log(msg);
-        return this;
-      };
-      return $delegate;
-    });
-  }
-
-  function initApp($rootScope, $http) {
-    $rootScope._ = _;
-    $rootScope.loadingPage = true;
-    $rootScope.clickPage = function(){
-      $rootScope.$broadcast('page_clicked', {});
-    }
-    $rootScope.checkHasUnRead = function(){
-      //$http.get('/share_opt/check_opt_has_new.json').success(function (data) {
-      //  if (data['has_new']) {
-      //    $rootScope.showUnReadMark = true;
-      //  }
-      //});
+    /* @ngInject */
+    function configCompileProvider($compileProvider) {
+        $compileProvider.imgSrcSanitizationWhitelist(/^\s*(https|file|blob|cdvfile|http|chrome-extension|blob:chrome-extension):|data:image\//);
     }
 
-  }
+    /* @ngInject */
+    function configHttpProvider($httpProvider) {
+        $httpProvider.defaults.headers.common['Content-Type'] = 'application/json';
+        $httpProvider.defaults.headers.post['Content-Type'] = 'application/json';
+    }
 
-  function DefaultCtrl($rootScope){
-    $rootScope.loadingPage = false;
-    $rootScope.checkHasUnRead();
-  }
+    /* @ngInject */
+    function extendLog($provide) {
+        $provide.decorator('$log', function ($delegate, $injector) {
+            var _log = $delegate.log;
+            $delegate.log = function (msg, forceLog) {
+                _log(msg);
+                return this;
+            };
+            return $delegate;
+        });
+    }
+
+    function initApp($rootScope, $http) {
+        $rootScope._ = _;
+        $rootScope.loadingPage = true;
+        $rootScope.clickPage = function () {
+            $rootScope.$broadcast('page_clicked', {});
+        };
+        $rootScope.checkHasUnRead = function () {
+            //$http.get('/share_opt/check_opt_has_new.json').success(function (data) {
+            //  if (data['has_new']) {
+            //    $rootScope.showUnReadMark = true;
+            //  }
+            //});
+        };
+        $rootScope.addPageClickLog = function (data) {
+            $http.post('/util/log_view_position_click.json', data).success(function (data) {
+            }).error(function () {
+            });
+        }
+    }
+
+    function DefaultCtrl($rootScope) {
+        $rootScope.loadingPage = false;
+        $rootScope.checkHasUnRead();
+    }
 })(window, window.angular);
--- webroot/src/scripts/index-products-controller.js
+++ webroot/src/scripts/index-products-controller.js
@@ -1,54 +1,63 @@
 (function (window, angular) {
 
-  angular.module('weshares')
-    .controller('IndexCtrl', IndexCtrl);
+    angular.module('weshares')
+        .controller('IndexCtrl', IndexCtrl);
 
 
-  function IndexCtrl($scope, $rootScope, $http, $log, $attrs, Utils) {
-    var vm = this;
-    vm.staticFilePath = Utils.staticFilePath();
-    vm.getSummary = getSummary;
+    function IndexCtrl($scope, $rootScope, $http, $log, $attrs, Utils) {
+        var vm = this;
+        vm.staticFilePath = Utils.staticFilePath();
+        vm.getSummary = getSummary;
+        vm.addPageClickLog = addPageClickLog;
 
-    activate();
-    function activate() {
-      $rootScope.showUnReadMark = false;
-      $rootScope.proxies = [];
-      $rootScope.loadingPage = false;
-      vm.uid = -1;
-      //$rootScope.checkHasUnRead();
-      //$http.get('/users/get_id_and_proxies').success(function (data) {
-      //  if (data.uid != null) {
-      //    $rootScope.uid = data.uid;
-      //    $rootScope.proxies = _.map(data.proxies, function (pid) {
-      //      return parseInt(pid);
-      //    })
-      //  }
-      //  else {
-      //  }
-      //}).error(function (data, e) {
-      //  $log.log('Failed to get proxies: ' + e);
-      //});
+        activate();
+        function activate() {
+            $rootScope.showUnReadMark = false;
+            $rootScope.proxies = [];
+            $rootScope.loadingPage = false;
+            vm.uid = -1;
+            //$rootScope.checkHasUnRead();
+            //$http.get('/users/get_id_and_proxies').success(function (data) {
+            //  if (data.uid != null) {
+            //    $rootScope.uid = data.uid;
+            //    $rootScope.proxies = _.map(data.proxies, function (pid) {
+            //      return parseInt(pid);
+            //    })
+            //  }
+            //  else {
+            //  }
+            //}).error(function (data, e) {
+            //  $log.log('Failed to get proxies: ' + e);
+            //});
 
-      var tag = $attrs.tag;
-      vm.indexProducts = [];
-      $http.get('/index_products/index_products/' + tag).success(function (data) {
-        vm.indexProducts = _.map(data, function (p) {
-          return {'id': p.IndexProduct.id, 'shareId': p.IndexProduct.share_id, 'summary': p.IndexProduct.summary};
-        });
-      }).error(function (data, e) {
-        $log.log('Failed to get index products: ' + e);
-      });
+            var tag = $attrs.tag;
+            vm.indexProducts = [];
+            $http.get('/index_products/index_products/' + tag).success(function (data) {
+                vm.indexProducts = _.map(data, function (p) {
+                    return {
+                        'id': p.IndexProduct.id,
+                        'shareId': p.IndexProduct.share_id,
+                        'summary': p.IndexProduct.summary
+                    };
+                });
+            }).error(function (data, e) {
+                $log.log('Failed to get index products: ' + e);
+            });
+        }
+
+        function addPageClickLog(data){
+            $rootScope.addPageClickLog(data);
+        }
+
+        function getSummary(shareId) {
+            var indexProduct = _.find(vm.indexProducts, function (p) {
+                return p.shareId == shareId;
+            });
+            if (_.isEmpty(indexProduct)) {
+                return {orders_count: 0, view_count: 0, orders_and_creators: []};
+            }
+            return indexProduct.summary;
+        }
     }
-
-    function getSummary(shareId) {
-      var indexProduct = _.find(vm.indexProducts, function (p) {
-        return p.shareId == shareId;
-      });
-      if (_.isEmpty(indexProduct)) {
-        return {orders_count: 0, view_count: 0, orders_and_creators: []};
-      }
-      return indexProduct.summary;
-    }
-  }
 })
 (window, window.angular);
